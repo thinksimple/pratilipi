fa53e791b30b85b826db901783c5534bc3fe5d8d
antshpra <antshpra@gmail.com>
2014-08-08 18:06:59 +0530

    Using FreeMarker to process Header widget.

---
+++ src-cms/com/claymus/module/websitewidget/header/Header.ftl
@@ -0,0 +1,4 @@
+<div>
+	<h1> ${title} </h1>
+	<h3> ${tagLine} </h3>
+</div>
--- src-cms/com/claymus/module/websitewidget/header/HeaderProcessor.java
+++ src-cms/com/claymus/module/websitewidget/header/HeaderProcessor.java
@@ -1,15 +1,40 @@
 package com.claymus.module.websitewidget.header;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.claymus.module.websitewidget.WebsiteWidgetProcessor;
+import com.claymus.servlet.ClaymusMain;
+
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 public class HeaderProcessor implements WebsiteWidgetProcessor<Header> {
 
+	private static final Logger logger =
+			Logger.getLogger( HeaderProcessor.class.getName() );
+	
+	private static final String TEMPLATE_NAME
+			= "com/claymus/module/websitewidget/header/Header.ftl";
+	
 	@Override
 	public String getHtml( Header header ) {
-		return "<div>" +
-					"<h1>" + header.getTitle() + "</h1>" +
-					"<h3>" + header.getTagLine() + "</h3>" +
-				"</div>";
+		Writer writer = new StringWriter();
+
+		try {
+			Template template
+					= ClaymusMain
+							.FREEMARKER_CONFIGURATION
+							.getTemplate( TEMPLATE_NAME );
+			template.process( header, writer );
+		} catch ( IOException | TemplateException e ) {
+			logger.log( Level.SEVERE, "Template processing failed.", e );
+		}
+		
+		return writer.toString();
 	}
-	
+
 }
