13b897cb239dc0820bc14f32d04da9be050604dd
antshpra <antshpra@gmail.com>
2014-08-12 17:28:45 +0530

    Fixing User Widget.

--- src-cms/com/claymus/module/websitewidget/user/UserWidget.ftl
+++ src-cms/com/claymus/module/websitewidget/user/UserWidget.ftl
@@ -1,10 +1,11 @@
 <div class="websitewidget-user">
 
 	<div>
-		<#if ClaymusHelper.getCurrentUser() ??>
-			Hello, Guest ! (<a href="${ ClaymusHelper.createLoginURL() }">login</a>)
+		<#if getCurrentUser() ??>
+			Hello, ${ getCurrentUser().getNickname() } ! (<a href="${ createLogoutURL() }">logout</a>)
 		<#else>
-			Hello, ${ ClaymusHelper.getCurrentUser().getNickname() } ! (<a href="${ ClaymusHelper.createLogoutURL() }">logout</a>)
+			Hello, Guest ! (<a href="${ createLoginURL() }">login</a>)
 		</#if>
 	</div>
+
 </div>
--- src-cms/com/claymus/module/websitewidget/user/UserWidgetProcessor.java
+++ src-cms/com/claymus/module/websitewidget/user/UserWidgetProcessor.java
@@ -1,21 +1,43 @@
 package com.claymus.module.websitewidget.user;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.claymus.ClaymusHelper;
 import com.claymus.module.websitewidget.WebsiteWidgetProcessor;
-import com.google.appengine.api.users.User;
+import com.claymus.servlet.ClaymusMain;
+
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 public class UserWidgetProcessor extends WebsiteWidgetProcessor<UserWidget> {
 
-	@Override
-	public String getHtml( UserWidget userInfo ) {
-
-		User user = ClaymusHelper.getCurrentUser();
+	private static final Logger logger =
+			Logger.getLogger( UserWidgetProcessor.class.getName() );
+	
+	public String getHtml( UserWidget userWidget ) {
 		
-		if( user == null )
-			return "<div> Hello, Guest ! (<a href='" + ClaymusHelper.createLoginURL() + "'>login</a>)</div>";
+		Writer writer = new StringWriter();
 
-		else
-			return "<div> Hello, " + user.getNickname() + " ! (<a href='" + ClaymusHelper.createLogoutURL() + "'>logout</a>)</div>";
+		try {
+			Template template
+					= ClaymusMain
+							.FREEMARKER_CONFIGURATION
+							.getTemplate( getTemplateName() );
+			template.process( new ClaymusHelper(), writer );
+		} catch ( IOException | TemplateException e ) {
+			logger.log( Level.SEVERE, "Template processing failed.", e );
+		}
+		
+		return writer.toString();
+	}
+	
+	@Override
+	public String getTemplateName() {
+		return "com/claymus/module/websitewidget/user/UserWidget.ftl";
 	}
 	
 }
