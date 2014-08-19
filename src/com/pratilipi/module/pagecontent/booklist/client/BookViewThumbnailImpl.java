8e4be2905be558f84966166cb02a80fe2b2e2e4b
Rahul Ranjan <genirahul@gmail.com>
2014-08-20 01:03:24 +0530

    Refactoring

--- src/com/pratilipi/module/pagecontent/booklist/client/BookViewThumbnailImpl.java
+++ src/com/pratilipi/module/pagecontent/booklist/client/BookViewThumbnailImpl.java
@@ -26,52 +26,6 @@ public class BookViewThumbnailImpl extends BookView {
 	
 	public BookViewThumbnailImpl() {
 		
-		/*
-		Panel panel = new HorizontalPanel();
-		panel.add( new Label( "Thumbnail View : " ) );
-		titleLabel.getElement().setPropertyString("cursor", "pointer");
-		panel.add( isbnLabel );
-		panel.add( titleLabel );
-		
-		FocusPanel wrapper = new FocusPanel();
-		wrapper.add(panel);
-		wrapper.addClickHandler(new ClickHandler(){
-
-			@Override
-			public void onClick(ClickEvent event) {
-				final PopupPanel popup = new PopupPanel();
-				BookDetailView bookDetail = new BookDetailView(popup, bookData);
-				VerticalPanel vPanel = bookDetail.getPopup();
-				
-				//Close Book detail window using ESCAPE keypress event
-				popup.addDomHandler(new KeyDownHandler(){
-
-					@Override
-					public void onKeyDown(KeyDownEvent event) {
-						if(event.getNativeKeyCode() == KeyCodes.KEY_ESCAPE)
-							popup.hide();
-					}} , KeyDownEvent.getType());
-				
-				//set focus on popup panel on mouseOver event. 
-				popup.addDomHandler(new MouseOverHandler(){
-
-					@Override
-					public void onMouseOver(MouseOverEvent event) {
-						// TODO Auto-generated method stub
-						popup.getElement().focus();
-					}}, MouseOverEvent.getType());
-				
-				popup.clear();
-				popup.setAnimationEnabled(true);
-				popup.setGlassEnabled(true);
-				popup.setAutoHideEnabled(false);
-				popup.setWidth(.7*Window.getClientWidth()+"px");
-				popup.setHeight(Window.getClientHeight()+"px");
-				popup.add(vPanel);
-				popup.center();
-				
-			}});*/
-		
 	}
 
 	
