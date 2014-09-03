a46579cd197698c417872f4be4e44daa9a74cc73
Prashant Gupta <antshpra@gmail.com>
2014-09-03 16:28:11 +0530

    Adding GWT widget for Accordion.

---
+++ src-cms/com/claymus/commons/client/ui/Accordion.java
@@ -0,0 +1,71 @@
+package com.claymus.commons.client.ui;
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.HeadingElement;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class Accordion extends Composite {
+
+	private Panel panel = new FlowPanel();
+	
+	private final Panel headingPanel = new SimplePanel();
+	private final Panel collapsePanel = new SimplePanel();
+	
+	private final Anchor titleAnchor = new Anchor();
+	private final HeadingElement titleElement = Document.get().createHElement( 4 );
+	
+	private final Panel bodyPanel = new FlowPanel();
+
+	
+	public Accordion() {
+
+		String id = HTMLPanel.createUniqueId();
+		titleAnchor.setHref( "#Tr-" + id );
+		collapsePanel.getElement().setAttribute( "id", "Tr-" + id );
+
+		
+		// Composing the widget
+		panel.add( headingPanel );
+		panel.add( collapsePanel );
+	
+		headingPanel.add( titleAnchor );
+		titleAnchor.getElement().appendChild( titleElement );
+		
+		collapsePanel.add( bodyPanel );
+
+
+		// Setting required style classes
+		panel.addStyleName( "panel panel-default" );
+		
+		headingPanel.setStyleName( "panel-heading" );
+		collapsePanel.setStyleName( "panel-collapse collapse" );
+		
+		titleAnchor.getElement().setAttribute( "data-toggle", "collapse" );
+		titleElement.setAttribute( "class", "panel-title" );
+		
+		bodyPanel.setStyleName( "panel-body" );
+
+		
+		initWidget( panel );
+	}
+	
+	public void setTitle( String title ) {
+		titleElement.setInnerText( title );
+	}
+
+	public void add( Widget child ) {
+		bodyPanel.add( child );
+	}
+
+	public void add( IsWidget child ) {
+		bodyPanel.add( child );
+	}
+	
+}
