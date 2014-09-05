2146ca87a0c9f902c0e1657751a9ee910d6195df
Rahul Ranjan <genirahul@gmail.com>
2014-09-05 17:42:15 +0530

    Book Add form format correction

--- src/com/pratilipi/commons/client/PratilipiDataInputViewImpl.java
+++ src/com/pratilipi/commons/client/PratilipiDataInputViewImpl.java
@@ -8,6 +8,7 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.pratilipi.commons.shared.PratilipiType;
 import com.pratilipi.service.shared.data.ArticleData;
 import com.pratilipi.service.shared.data.AuthorData;
@@ -22,19 +23,16 @@ public class PratilipiDataInputViewImpl<T extends PratilipiData> extends Pratili
 	
 	private Panel panel = new FlowPanel();
 	private Panel pratilipiTypePanel = new FlowPanel();
-	private Panel isbnPanel = new FlowPanel();
+	private Panel pratilipiTypeInputCol = new SimplePanel();
 	private Panel titlePanel = new FlowPanel();
+	private Panel titleInputCol = new SimplePanel();
 	private Panel authorPanel = new FlowPanel();
+	private Panel authorInputCol = new SimplePanel();
 	//private Panel publisherPanel = new FlowPanel();
 	private Panel languagePanel = new FlowPanel();
+	private Panel languageInputCol = new SimplePanel();
 	
 	private Label formHeader = new Label( "Enter Book Details" );
-	private Label pratilipiTypeLabel = new Label( "Type" );
-	private Label isbnLabel = new Label("ISBN ");
-	private Label titleLabel = new Label("Title ");
-	private Label authorLabel = new Label("Author ");
-	//private Label publisherLabel = new Label("Publisher ");
-	private Label languageLabel = new Label("Language ");
 	
 	private ListBoxFormField pratilipiTypeInput = new ListBoxFormField();
 	private TextInputFormField isbnInput = new TextInputFormField(); 
@@ -47,6 +45,7 @@ public class PratilipiDataInputViewImpl<T extends PratilipiData> extends Pratili
 	
 	public PratilipiDataInputViewImpl() {
 		
+		pratilipiTypeInput.addItem("Select Type", "");
 	    pratilipiTypeInput.setRequired( true );
 	    pratilipiTypeInput.addItem( PratilipiType.ARTICLE.getName() );
 	    pratilipiTypeInput.addItem( PratilipiType.BOOK.getName());
@@ -54,32 +53,33 @@ public class PratilipiDataInputViewImpl<T extends PratilipiData> extends Pratili
 	    pratilipiTypeInput.addItem( PratilipiType.STORY.getName());
 	    
 	    //adding label and input boxes to horizontal panels.
-	    pratilipiTypeLabel.addStyleName( "col-sm-2" );
-	    pratilipiTypeInput.addStyleName( "col-sm-4" );
+	    pratilipiTypeInputCol.addStyleName( "col-sm-4" );
+	    pratilipiTypeInputCol.add( pratilipiTypeInput );
 	    pratilipiTypePanel.addStyleName( "row" );
-	    pratilipiTypePanel.add( pratilipiTypeLabel );
-	    pratilipiTypePanel.add( pratilipiTypeInput );
+	    pratilipiTypePanel.add( pratilipiTypeInputCol );
 	    
-	    
+	    /*
 	    isbnLabel.addStyleName( "col-sm-2" );
-	    isbnInput.addStyleName( "col-sm-4" );
+	    isbnInputCol.addStyleName( "col-sm-4" );
+	    isbnInputCol.add( isbnInput );
 	    isbnPanel.addStyleName( "row" );
 	    isbnPanel.add( isbnLabel );
-	    isbnPanel.add( isbnInput );
+	    isbnPanel.add( isbnInputCol );
+	    */
 	    
+	    titleInput.setPlaceholder( "Title" );
 	    titleInput.setRequired( true );
-	    titleLabel.addStyleName( "col-sm-2" );
-	    titleInput.addStyleName( "col-sm-4" );
+	    titleInputCol.addStyleName( "col-sm-4" );
+	    titleInputCol.add( titleInput );
 	    titlePanel.addStyleName( "row" );
-	    titlePanel.add( titleLabel );
-	    titlePanel.add( titleInput );
+	    titlePanel.add( titleInputCol );
 	    
+	    authorInput.addItem("Select Author", "");
 	    authorInput.setRequired( true );
-	    authorLabel.addStyleName( "col-sm-2" );
-	    authorInput.addStyleName( "col-sm-4" );
+	    authorInputCol.addStyleName( "col-sm-4" );
+	    authorInputCol.add( authorInput );
 	    authorPanel.addStyleName( "row" );
-	    authorPanel.add(authorLabel);
-	    authorPanel.add(authorInput);
+	    authorPanel.add( authorInputCol );
 	    
 	    /* To be used in future implementations
 	    publisherInput.setRequired( true );
@@ -88,27 +88,22 @@ public class PratilipiDataInputViewImpl<T extends PratilipiData> extends Pratili
 	    publisherPanel.add(publisherLabelCol);
 	    publisherPanel.add(publisherInputCol);
 	    */
-	    
+	    languageInput.addItem("Select Language", "" );
 	    languageInput.setRequired( true );
-	    languageLabel.addStyleName( "col-sm-2" );
-	    languageInput.addStyleName( "col-sm-4" );
+	    languageInputCol.addStyleName( "col-sm-4" );
+	    languageInputCol.add( languageInput );
 	    languagePanel.addStyleName( "row" );
-	    languagePanel.add(languageLabel);
-	    languagePanel.add(languageInput);
+	    languagePanel.add(languageInputCol);
 	    
 	    add.addStyleName( "btn btn-default" );
 	    
 	    //adding horizontal panels to vertical panel
 	    panel.add(formHeader);
 	    panel.add( pratilipiTypePanel );
-		panel.add( isbnPanel );
 		panel.add( titlePanel );
 		panel.add( authorPanel );
-		//panel.add( publisherPanel );
 		panel.add( languagePanel );
 		panel.add( add );
-		
-		//form.add(panel);
 		
 		initWidget( panel );
 	}
@@ -135,31 +130,27 @@ public class PratilipiDataInputViewImpl<T extends PratilipiData> extends Pratili
 		isbnInput.setEnabled( enabled );
 		titleInput.setEnabled( enabled );
 		authorInput.setEnabled( enabled );
-		//publisherInput.setEnabled( enabled );
 		languageInput.setEnabled( enabled );		
 	}
 
 	@Override
 	public T getPratilipiData() {
 		PratilipiData pratilipiData;
-		if( pratilipiTypeInput.getItemText().equals( "BOOK" )){
+		if( pratilipiTypeInput.getItemText().equals( "Book" )){
 			pratilipiData = new BookData();
 		}
-		else if( pratilipiTypeInput.getItemText().equals( "POEM" ) )
+		else if( pratilipiTypeInput.getItemText().equals( "Poem" ) )
 			pratilipiData = new PoemData();
-		else if( pratilipiTypeInput.getItemText().equals( "STORY" ) )
+		else if( pratilipiTypeInput.getItemText().equals( "Story" ) )
 			pratilipiData = new StoryData();
 		else
 			pratilipiData = new ArticleData();
 
 		pratilipiData.setPratilipiType( pratilipiTypeInput.getItemText() );
 		pratilipiData.setIsbn( isbnInput.getText() );
-		//book.setId(bookId);
 		pratilipiData.setTitle( titleInput.getText() );
-		pratilipiData.setAuthorName( authorInput.getItemText());  //need to find a way to get AUTHOR_ID of required author from database
+		pratilipiData.setAuthorName( authorInput.getItemText());
 		pratilipiData.setAuthorId( Long.valueOf(authorInput.getValue()) );
-		//book.setPublisherName(publisherInput.getItemText());
-		//book.setPublisherId( Long.valueOf(publisherInput.getValue()) );
 		pratilipiData.setLanguageName(languageInput.getItemText());
 		pratilipiData.setLanguageId(Long.valueOf(languageInput.getValue()));
 		
