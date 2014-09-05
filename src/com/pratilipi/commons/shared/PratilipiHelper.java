1cf5a2da4692b2d750f57a3c341ac0eb93de2df7
Rahul Ranjan <genirahul@gmail.com>
2014-09-05 20:56:11 +0530

    Poem URLs

--- src/com/pratilipi/commons/shared/PratilipiHelper.java
+++ src/com/pratilipi/commons/shared/PratilipiHelper.java
@@ -3,6 +3,8 @@ package com.pratilipi.commons.shared;
 public class PratilipiHelper {
 
 	public static final String URL_BOOK_PAGE = "/book/";
+	
+	public static final String URL_POEM_PAGE = "/poem/";
 
 	public static final String URL_AUTHOR_PAGE = "/author/";
 
@@ -11,5 +13,7 @@ public class PratilipiHelper {
 	public static final String URL_GENRE_PAGE = "/genre/";
 
 	public static final String URL_BOOK_COVER = "/resource.book-cover/original/";
+	
+	public static final String URL_POEM_COVER = "/resource.poem-cover/original/";
 	
 }
