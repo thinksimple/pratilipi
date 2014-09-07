10dad3f87101125d55c7d1d424526864aa46f6da
Prashant Gupta <antshpra@gmail.com>
2014-09-07 23:41:30 +0530

    Adding "Classics" checkbox form field in PratilipiDataInputViewImpl.

--- src/com/pratilipi/commons/client/PratilipiDataInputViewImpl.java
+++ src/com/pratilipi/commons/client/PratilipiDataInputViewImpl.java
@@ -1,5 +1,6 @@
 package com.pratilipi.commons.client;
 
+import com.claymus.commons.client.ui.formfield.CheckBoxFormField;
 import com.claymus.commons.client.ui.formfield.ListBoxFormField;
 import com.claymus.commons.client.ui.formfield.TextInputFormField;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -7,25 +8,18 @@ import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Panel;
-import com.google.gwt.user.client.ui.SimplePanel;
 import com.pratilipi.commons.shared.PratilipiType;
 import com.pratilipi.service.shared.data.PratilipiData;
 
 public class PratilipiDataInputViewImpl extends PratilipiDataInputView {
 	
 	private Panel panel = new FlowPanel();
-
-	private Panel titleInputRow = new FlowPanel();
-	private Panel authorInputRow = new FlowPanel();
-	private Panel languageInputRow = new FlowPanel();
-	
-	private Panel titleInputCol = new SimplePanel();
-	private Panel authorInputCol = new SimplePanel();
-	private Panel languageInputCol = new SimplePanel();
+	private Panel innerPanel = new FlowPanel();
 	
 	private TextInputFormField titleInput = new TextInputFormField();
 	private ListBoxFormField authorList = new ListBoxFormField();
 	private ListBoxFormField languageList = new ListBoxFormField();;
+	private CheckBoxFormField isPublicDomain = new CheckBoxFormField();
 	private Button addButton = new Button();
 	
 	
@@ -42,33 +36,23 @@ public class PratilipiDataInputViewImpl extends PratilipiDataInputView {
 	    authorList.setRequired( true );
 	    languageList.addItem( "Select Language", "" );
 	    languageList.setRequired( true );
+		isPublicDomain.setText( "Classics" );
 		addButton.setText(  "Add " + pratilipiType.getName() );
 		
 	
 		// Composing the widget
-		panel.add( titleInputRow );
-		panel.add( authorInputRow );
-		panel.add( languageInputRow );
-		panel.add( addButton );
-
-		titleInputRow.add( titleInputCol );
-		authorInputRow.add( authorInputCol );
-	    languageInputRow.add( languageInputCol );
+		panel.add( innerPanel );
 	    
-		titleInputCol.add( titleInput );
-	    authorInputCol.add( authorList );
-	    languageInputCol.add( languageList );
+		innerPanel.add( titleInput );
+		innerPanel.add( authorList );
+		innerPanel.add( languageList );
+		innerPanel.add( isPublicDomain );
+		innerPanel.add( addButton );
 
 		
 		// Setting required style classes
-		titleInputRow.setStyleName( "row" );
-		authorInputRow.setStyleName( "row" );
-		languageInputRow.setStyleName( "row" );
-	    
-		titleInputCol.setStyleName( "col-sm-4" );
-	    authorInputCol.setStyleName( "col-sm-4" );
-	    languageInputCol.setStyleName( "col-sm-4" );
-	    
+		panel.setStyleName( "row" );
+		innerPanel.setStyleName( "col-sm-4" );
 	    addButton.setStyleName( "btn btn-default" );
 
 		
