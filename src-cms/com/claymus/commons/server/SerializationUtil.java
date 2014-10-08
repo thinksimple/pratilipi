60f4c0eafa70ad6f77f242f6d370f27045138b4d
Prashant Gupta <antshpra@gmail.com>
2014-10-08 22:07:46 +0530

    SerializationUtil

---
+++ src-cms/com/claymus/commons/server/SerializationUtil.java
@@ -0,0 +1,55 @@
+package com.claymus.commons.server;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.claymus.commons.client.UnexpectedServerException;
+import com.google.gwt.user.client.rpc.IsSerializable;
+import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.gwt.user.server.rpc.RPC;
+import com.google.gwt.user.server.rpc.impl.ServerSerializationStreamReader;
+import com.google.gwt.user.server.rpc.impl.ServerSerializationStreamWriter;
+import com.pratilipi.pagecontent.pratilipi.PratilipiContentProcessor;
+
+public class SerializationUtil {
+
+	private static final Logger logger =
+			Logger.getLogger( PratilipiContentProcessor.class.getName() );
+
+	
+	public static String encode( IsSerializable object )
+			throws UnexpectedServerException {
+		
+		ServerSerializationStreamWriter streamWriter =
+				new ServerSerializationStreamWriter(
+						RPC.getDefaultSerializationPolicy() );
+		streamWriter.prepareToWrite();
+		try {
+			streamWriter.serializeValue( object, object.getClass() );
+			return streamWriter.toString();
+		} catch( SerializationException e ) {
+			String msg = "Failed to serialize object.";
+			logger.log( Level.SEVERE, msg, e );
+			throw new UnexpectedServerException( msg );
+		}
+		
+	}
+	
+	public static IsSerializable decode( String encodedStr )
+			throws UnexpectedServerException {
+		
+		ServerSerializationStreamReader streamReader =
+				new ServerSerializationStreamReader(
+						Thread.currentThread().getContextClassLoader(), null );
+		try {
+			streamReader.prepareToRead( encodedStr );
+			return (IsSerializable) streamReader.readObject();
+		} catch (SerializationException e) {
+			String msg = "Failed to deserialize encoded string.";
+			logger.log( Level.SEVERE, msg, e );
+			throw new UnexpectedServerException( msg );
+		}
+		
+	}
+	
+}
