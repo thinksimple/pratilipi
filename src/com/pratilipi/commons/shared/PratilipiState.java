f7103d5fef1594d3e19be59d92df9dec4c0674ca
Prashant Gupta <antshpra@gmail.com>
2015-01-07 16:58:32 +0530

    Adding "SUBMITTED" state in PratilipiState.

--- src/com/pratilipi/commons/shared/PratilipiState.java
+++ src/com/pratilipi/commons/shared/PratilipiState.java
@@ -5,6 +5,7 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 public enum PratilipiState implements IsSerializable {
 
 	DRAFTED,				// Work in progress
+	SUBMITTED,				// Final work waiting for approval from Moderator
 	PUBLISHED,				// Published and free
 	PUBLISHED_PAID,			// Published and paid
 	PUBLISHED_DISCONTINUED,	// Published and discontinued
