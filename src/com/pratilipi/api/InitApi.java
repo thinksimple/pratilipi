cbbc3688e0235c03f8daa18ff9db78c6c92cdc19
Prashant Gupta <antshpra@gmail.com>
2015-01-22 15:14:45 +0530

    Updating InitApi

--- src/com/pratilipi/api/InitApi.java
+++ src/com/pratilipi/api/InitApi.java
@@ -5,9 +5,12 @@ import java.util.Date;
 import com.claymus.api.GenericApi;
 import com.claymus.api.annotation.Bind;
 import com.claymus.api.annotation.Get;
+import com.claymus.commons.shared.ClaymusPageType;
 import com.claymus.commons.shared.exception.InsufficientAccessException;
 import com.claymus.commons.shared.exception.InvalidArgumentException;
 import com.claymus.data.transfer.Page;
+import com.claymus.data.transfer.PageContent;
+import com.claymus.pagecontent.pages.PagesContentHelper;
 import com.pratilipi.api.shared.GetInitRequest;
 import com.pratilipi.api.shared.GetInitResponse;
 import com.pratilipi.commons.shared.PratilipiPageType;
@@ -46,6 +49,30 @@ public class InitApi extends GenericApi {
 		return new GetInitResponse( page.getUri() );
 
 		// END :: CREATE NEW EVENT :: END									*/
+
+
+/*		// START :: CREATE NEW PAGECONTENT :: START
+
+		Page page = dataAccessor.getPage( "/pages" );
+		if( page == null ) {
+			PageContent pageContent = PagesContentHelper.newPagesContent();
+			pageContent.setCreationDate( new Date() );
+			pageContent.setLastUpdated( new Date() );
+			pageContent = dataAccessor.createOrUpdatePageContent( pageContent );
+			
+			page = dataAccessor.newPage();
+			page.setType( ClaymusPageType.GENERIC.toString() );
+			page.setUriAlias( "/pages" );
+			page.setPrimaryContentId( pageContent.getId() );
+			page.setCreationDate( new Date() );
+			page = dataAccessor.createOrUpdatePage( page );
+			
+			pageContent.setPageId( page.getId() );
+			pageContent = dataAccessor.createOrUpdatePageContent( pageContent );
+		}
+		return new GetInitResponse( page.getId().toString() );
+		
+		// END :: CREATE NEW PAGECONTENT :: END									*/
 
 		
 		return new GetInitResponse( "Nothing to initialize !" );
