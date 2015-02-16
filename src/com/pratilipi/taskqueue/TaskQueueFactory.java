75ed9bc5d3b7d92f93a00c7594d6bed8f36fb60d
Prashant Gupta <antshpra@gmail.com>
2015-02-16 21:30:25 +0530

    Adding 'pratilipi' TaskQueue.

--- src/com/pratilipi/taskqueue/TaskQueueFactory.java
+++ src/com/pratilipi/taskqueue/TaskQueueFactory.java
@@ -4,6 +4,8 @@ import com.claymus.taskqueue.TaskQueue;
 
 public class TaskQueueFactory extends com.claymus.taskqueue.TaskQueueFactory {
 
+	private static final String QUEUE_PRATILIPI = "pratilipi";
+
 	private static final String QUEUE_CREATE_OR_UPDATE_DEFAULT_COVER = "create-or-update-default-cover";
 	
 	private static final String QUEUE_HTML_TO_PRATILIPI = "html-to-pratilipi";
@@ -15,6 +17,10 @@ public class TaskQueueFactory extends com.claymus.taskqueue.TaskQueueFactory {
 	private static final String QUEUE_UPDATE_INDEX_AUTHOR = "update-index-author";
 
 	
+	public static TaskQueue getPratilipiTaskQueue() {
+		return getTaskQueue( QUEUE_PRATILIPI );
+	}
+
 	public static TaskQueue getCreateOrUpdateDefaultCoverTaskQueue() {
 		return getTaskQueue( QUEUE_CREATE_OR_UPDATE_DEFAULT_COVER );
 	}
