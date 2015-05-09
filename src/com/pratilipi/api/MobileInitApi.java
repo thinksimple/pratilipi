697d4929ca2a6a68cf170b5029371c9769a904ed
Rahul Ranjan <genirahul@gmail.com>
2015-05-09 12:17:46 +0530

    Added languageId to mobileInitApi request object

--- src/com/pratilipi/api/MobileInitApi.java
+++ src/com/pratilipi/api/MobileInitApi.java
@@ -19,12 +19,25 @@ public class MobileInitApi extends GenericApi {
 	public GetMobileInitResponse getMobileInit( GetMobileInitRequest request ){
 		
 		List<Long> topReadPratilipiIdList = new ArrayList<Long>();
-		topReadPratilipiIdList.add( 4843865324388352L );
-		topReadPratilipiIdList.add( 5639221461123072L );
-		topReadPratilipiIdList.add( 5382523345436672L );
-		topReadPratilipiIdList.add( 5157903266742272L );
-		topReadPratilipiIdList.add( 4872221772218368L );
-		topReadPratilipiIdList.add( 6233002837278720L );
+		if( request.getLanguageId() == 5130467284090880L ){
+			topReadPratilipiIdList.add( 4821869924450304L );
+			topReadPratilipiIdList.add( 4843865324388352L );
+			topReadPratilipiIdList.add( 4629135213199360L );
+			topReadPratilipiIdList.add( 4538710716579840L );
+			topReadPratilipiIdList.add( 4571168321306624L );
+		} else if( request.getLanguageId() == 5965057007550464L ){
+			topReadPratilipiIdList.add( 4818859890573312L );
+			topReadPratilipiIdList.add( 4834680419385344L );
+			topReadPratilipiIdList.add( 4849784863064064L );
+			topReadPratilipiIdList.add( 4869201873338368L );
+			topReadPratilipiIdList.add( 4901223018790912L );
+		} else if( request.getLanguageId() == 6319546696728576L ){
+			topReadPratilipiIdList.add( 4664696015683584L );
+			topReadPratilipiIdList.add( 4801061093113856L );
+			topReadPratilipiIdList.add( 4685596450619392L );
+			topReadPratilipiIdList.add( 4803319927144448L );
+			topReadPratilipiIdList.add( 4837289477799936L );
+		}
 		
 		List<PratilipiData> topReadPratilipiDataList = 
 					PratilipiContentHelper.createPratilipiDataList( 
--- src/com/pratilipi/api/shared/GetMobileInitRequest.java
+++ src/com/pratilipi/api/shared/GetMobileInitRequest.java
@@ -1,8 +1,15 @@
 package com.pratilipi.api.shared;
 
+import com.claymus.api.annotation.Validate;
 import com.claymus.api.shared.GenericRequest;
 
 @SuppressWarnings( "serial" )
 public class GetMobileInitRequest extends GenericRequest {
 
+	@Validate( required=true )
+	private Long languageId;
+	
+	public Long getLanguageId(){
+		return languageId;
+	}
 }
