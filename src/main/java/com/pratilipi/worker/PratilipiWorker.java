8b53acbc3bea24dd767f6f7cbd9d725dba9ceeb5
Prashant Gutpa <antshpra@gmail.com>
2015-06-25 12:33:31 +0530

    Migrating author queue to Worker module.

--- src/main/java/com/pratilipi/worker/PratilipiWorker.java
+++ src/main/java/com/pratilipi/worker/PratilipiWorker.java
@@ -2,6 +2,7 @@ package com.pratilipi.worker;
 
 import com.pratilipi.api.ApiRegistry;
 import com.pratilipi.api.GenericService;
+import com.pratilipi.api.author.AuthorProcessApi;
 import com.pratilipi.api.pratilipi.PratilipiProcessApi;
 
 @SuppressWarnings("serial")
@@ -9,6 +10,7 @@ public class PratilipiWorker extends GenericService {
 	
 	static {
 		ApiRegistry.register( PratilipiProcessApi.class );
+		ApiRegistry.register( AuthorProcessApi.class );
 	}
 	
 }
