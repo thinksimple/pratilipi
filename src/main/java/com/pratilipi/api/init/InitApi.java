3dccaac644810c4bc2b0274c8b3d3c6070693037
Prashant Gutpa <antshpra@gmail.com>
2015-07-08 01:32:26 +0530

    Init Api & Cron for deleting session entities.

---
+++ src/main/java/com/pratilipi/api/init/InitApi.java
@@ -0,0 +1,57 @@
+package com.pratilipi.api.init;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.pratilipi.api.GenericApi;
+import com.pratilipi.api.annotation.Bind;
+import com.pratilipi.api.annotation.Get;
+import com.pratilipi.api.shared.GenericRequest;
+import com.pratilipi.api.shared.GenericResponse;
+import com.pratilipi.common.util.SystemProperty;
+
+@SuppressWarnings("serial")
+@Bind( uri = "/init" )
+public class InitApi extends GenericApi {
+
+	private static final Logger logger =
+			Logger.getLogger( InitApi.class.getName() );
+	
+	
+	@Get
+	public GenericResponse getAuthorProcess( GenericRequest request ) {
+		
+		if( SystemProperty.get( "cron" ).equals( "stop" ) )
+			return new GenericResponse();
+
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query query = new Query( "_ah_SESSION" )
+				.setKeysOnly();
+		PreparedQuery pq = datastore.prepare( query );
+		FetchOptions fo = FetchOptions.Builder
+				.withDefaults()
+				.chunkSize( 100 )
+				.limit( 1000 );
+
+		int cleared = 0;
+		try {
+			for( Entity entity : pq.asIterable( fo )) {				
+				datastore.delete( entity.getKey() );
+				cleared++;
+			}
+		} catch( Throwable e ) {
+			logger.log( Level.SEVERE, "Exception occured while clearing session entites.", e );
+		} finally {
+			logger.log( Level.INFO, "Cleared " + cleared + " session entities." );
+		}
+		
+		return new GenericResponse();
+	}
+	
+}
--- src/main/java/com/pratilipi/worker/PratilipiWorker.java
+++ src/main/java/com/pratilipi/worker/PratilipiWorker.java
@@ -3,12 +3,14 @@ package com.pratilipi.worker;
 import com.pratilipi.api.ApiRegistry;
 import com.pratilipi.api.GenericService;
 import com.pratilipi.api.author.AuthorProcessApi;
+import com.pratilipi.api.init.InitApi;
 import com.pratilipi.api.pratilipi.PratilipiProcessApi;
 
 @SuppressWarnings("serial")
 public class PratilipiWorker extends GenericService {
 	
 	static {
+		ApiRegistry.register( InitApi.class );
 		ApiRegistry.register( PratilipiProcessApi.class );
 		ApiRegistry.register( AuthorProcessApi.class );
 	}
