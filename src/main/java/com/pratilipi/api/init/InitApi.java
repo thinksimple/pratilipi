a74b990c515ab5f5ce5d52600247be0bd3961792
Prashant Gutpa <antshpra@gmail.com>
2015-07-10 14:07:51 +0530

    Clearing ACCESS_TOKEN table in InitApi.

--- src/main/java/com/pratilipi/api/init/InitApi.java
+++ src/main/java/com/pratilipi/api/init/InitApi.java
@@ -1,5 +1,6 @@
 package com.pratilipi.api.init;
 
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,12 +10,15 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.pratilipi.api.GenericApi;
 import com.pratilipi.api.annotation.Bind;
 import com.pratilipi.api.annotation.Get;
 import com.pratilipi.api.shared.GenericRequest;
 import com.pratilipi.api.shared.GenericResponse;
-import com.pratilipi.common.util.SystemProperty;
 
 @SuppressWarnings("serial")
 @Bind( uri = "/init" )
@@ -27,17 +31,15 @@ public class InitApi extends GenericApi {
 	@Get
 	public GenericResponse getAuthorProcess( GenericRequest request ) {
 		
-		if( SystemProperty.get( "cron" ).equals( "stop" ) )
-			return new GenericResponse();
-
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		Query query = new Query( "_ah_SESSION" )
-				.setKeysOnly();
+
+		
+		// Cleaning up _ah_SESSION table
+		
+		Query query = new Query( "_ah_SESSION" ).setKeysOnly();
 		PreparedQuery pq = datastore.prepare( query );
-		FetchOptions fo = FetchOptions.Builder
-				.withDefaults()
-				.chunkSize( 100 )
-				.limit( 1000 );
+		FetchOptions fo = FetchOptions.Builder.withDefaults()
+				.chunkSize( 100 ).limit( 1000 );
 
 		int cleared = 0;
 		try {
@@ -50,6 +52,45 @@ public class InitApi extends GenericApi {
 		} finally {
 			logger.log( Level.INFO, "Cleared " + cleared + " session entities." );
 		}
+		
+
+		// Cleaning up ACCESS_TOKEN table
+
+		Filter creationDateFilter = new FilterPredicate(
+				"CREATION_DATE",
+				FilterOperator.LESS_THAN,
+				new Date( new Date().getTime() - 30 * 24 * 60 * 60 * 1000 ) );
+		
+		Filter publisherIdFilter = new FilterPredicate(
+				"PUBLISHER_ID",
+				FilterOperator.EQUAL,
+				null );
+
+		Filter userIdFilter = new FilterPredicate(
+				"USER_ID",
+				FilterOperator.EQUAL,
+				0L );
+
+		query = new Query( "ACCESS_TOKEN" )
+				.setFilter( CompositeFilterOperator.and( creationDateFilter, publisherIdFilter, userIdFilter ) )
+				.setKeysOnly();
+		pq = datastore.prepare( query );
+		fo = FetchOptions.Builder.withDefaults()
+				.chunkSize( 100 ).limit( 1000 );
+		
+		cleared = 0;
+		try {
+			for( Entity entity : pq.asIterable( fo ) ) {
+				logger.log( Level.INFO, entity.toString() );
+//				datastore.delete( entity.getKey() );
+				cleared++;
+			}
+		} catch( Throwable e ) {
+			logger.log( Level.SEVERE, "Exception occured while clearing access token entites.", e );
+		} finally {
+			logger.log( Level.INFO, "Cleared " + cleared + " access token entities." );
+		}
+
 		
 		return new GenericResponse();
 	}
