2389423895599dc6beaaad78f6e55596297178ea
Raghu Raman <rraagghhu@gmail.com>
2015-07-24 18:18:14 +0530

    Installing the logging agent

--- shell-scripts/setup.sh
+++ shell-scripts/setup.sh
@@ -11,6 +11,12 @@ sudo apt-get install screen
 # sudo apt-get install maven
 sudo apt-get install ant
 
+# Installing the logging agent
+curl -sSO https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh
+sha256sum google-fluentd-install.sh
+48a50746b23c1deb7ec4b5e377631e2c77b95ecada5eda2bc8986a1e04359141  google-fluentd-install.sh
+sudo bash google-fluentd-install.sh
+
 # Manually install maven. Apt-get installs older version of maven.
 sudo mkdir -p /usr/local/apache-maven
 cd /usr/local/apache-maven
