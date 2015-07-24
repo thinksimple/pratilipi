aea96350d66494fa1740f4062c799c72819dae44
Prashant Gutpa <antshpra@gmail.com>
2015-07-24 17:03:08 +0530

    Minor refactoring in update-test.sh

--- shell-scripts/update-test.sh
+++ shell-scripts/update-test.sh
@@ -5,22 +5,25 @@ export PATH=$M2:$PATH
 
 cd ~/pratilipi
 
-
 while true
 do
-git remote update
-if [ $(git rev-parse HEAD) != $(git rev-parse origin) ]
-then
-git fetch
-git reset --hard origin/master
-date +"Last Updated on - %m-%d-%Y %r"
-mvn clean
-mvn install
-ant
-# Update devo-pratilipi/default
-mvn appengine:update -Dapp.id=devo-pratilipi -Dapp.module=default
-# Update prod-pratilipi/gamma
-mvn appengine:update -Dapp.id=prod-pratilipi -Dapp.module=gamma
-fi
-sleep 300
+	git remote update
+
+	if [ $(git rev-parse HEAD) != $(git rev-parse origin) ]
+	then
+		git fetch
+		git reset --hard origin/master
+		
+		mvn clean
+		mvn install
+		ant
+		
+		# Update devo-pratilipi/default
+		mvn appengine:update -Dapp.id=devo-pratilipi -Dapp.module=default
+		
+		# Update prod-pratilipi/gamma
+		mvn appengine:update -Dapp.id=prod-pratilipi -Dapp.module=gamma
+	fi
+
+	sleep 300
 done
