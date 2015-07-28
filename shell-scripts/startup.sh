8983343968afc4b972e6a14c099cbd47961cb93b
Prashant Gutpa <antshpra@gmail.com>
2015-07-28 23:28:21 +0530

    Updating shell scripts.

--- shell-scripts/startup.sh
+++ shell-scripts/startup.sh
@@ -7,30 +7,9 @@ cd ~/pratilipi
 
 while true
 do
-	git remote update
 
-	if [ $(git rev-parse HEAD) != $(git rev-parse origin) ]
-	then
-		git fetch
-		git reset --hard origin/master
-		
-		mvn clean
-		mvn install
-		ant
-		
-		# Update devo-pratilipi/default
-		mvn appengine:update -Dapp.id=devo-pratilipi -Dapp.module=default
-		
-		# Update prod-pratilipi/gamma
-		mvn appengine:update -Dapp.id=prod-pratilipi -Dapp.module=gamma
-
-		cp src/main/webapp/WEB-INF/worker-web.xml			src/main/webapp/WEB-INF/web.xml
-		cp src/main/webapp/WEB-INF/worker-appengine-web.xml	src/main/webapp/WEB-INF/appengine-web.xml
-		cp src/main/webapp/WEB-INF/worker-queue.xml			src/main/webapp/WEB-INF/queue.xml
-
-		# Update devo-pratilipi/worker
-		mvn appengine:update -Dapp.id=devo-pratilipi -Dapp.module=worker
-	fi
-
+	date
+	bash update-test.sh 2>&1 | logger
 	sleep 60
+	
 done
--- shell-scripts/update-test.sh
+++ shell-scripts/update-test.sh
@@ -1,29 +1,17 @@
-export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.3
-export M2=$M2_HOME/bin
-export MAVEN_OPTS="-Xms256m -Xmx512m"
-export PATH=$M2:$PATH
+git remote update
 
-cd ~/pratilipi
-
-while true
-do
-	git remote update
-
-	if [ $(git rev-parse HEAD) != $(git rev-parse origin) ]
-	then
-		git fetch
-		git reset --hard origin/master
-		
-		mvn clean
-		mvn install
-		ant
-		
-		# Update devo-pratilipi/default
-		mvn appengine:update -Dapp.id=devo-pratilipi -Dapp.module=default
-		
-		# Update prod-pratilipi/gamma
-		mvn appengine:update -Dapp.id=prod-pratilipi -Dapp.module=gamma
-	fi
-
-	sleep 60
-done
+if [ $(git rev-parse HEAD) != $(git rev-parse origin) ]
+then
+	git fetch
+	git reset --hard origin/master
+	
+	mvn clean
+	mvn install
+	ant
+	
+	# Update devo-pratilipi/default
+	mvn appengine:update -Dapp.id=devo-pratilipi -Dapp.module=default
+	
+	# Update prod-pratilipi/gamma
+	mvn appengine:update -Dapp.id=prod-pratilipi -Dapp.module=gamma
+fi
