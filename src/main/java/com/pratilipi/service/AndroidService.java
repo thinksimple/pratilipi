1f563281773780f20beb665b9351c723dfb03698
Prashant Gutpa <antshpra@gmail.com>
2015-07-28 19:37:43 +0530

    Setting up AndroidService.

---
+++ src/main/java/com/pratilipi/service/AndroidService.java
@@ -0,0 +1,33 @@
+package com.pratilipi.service;
+
+import com.pratilipi.api.ApiRegistry;
+import com.pratilipi.api.GenericService;
+import com.pratilipi.api.author.AuthorImageApi;
+import com.pratilipi.api.pratilipi.PratilipiApi;
+import com.pratilipi.api.pratilipi.PratilipiCoverApi;
+import com.pratilipi.api.pratilipi.PratilipiListApi;
+import com.pratilipi.api.user.UserLoginApi;
+import com.pratilipi.api.user.UserLogoutApi;
+import com.pratilipi.api.user.UserRegisterApi;
+import com.pratilipi.api.userpratilipi.UserPratilipiApi;
+import com.pratilipi.api.userpratilipi.UserPratilipiReviewListApi;
+
+@SuppressWarnings("serial")
+public class AndroidService extends GenericService {
+	
+	static {
+		ApiRegistry.register( UserLoginApi.class );
+		ApiRegistry.register( UserLogoutApi.class );
+		ApiRegistry.register( UserRegisterApi.class );
+
+		ApiRegistry.register( PratilipiApi.class );
+		ApiRegistry.register( PratilipiListApi.class );
+		ApiRegistry.register( PratilipiCoverApi.class );
+		
+		ApiRegistry.register( AuthorImageApi.class );
+		
+		ApiRegistry.register( UserPratilipiApi.class );
+		ApiRegistry.register( UserPratilipiReviewListApi.class );
+	}
+	
+}
