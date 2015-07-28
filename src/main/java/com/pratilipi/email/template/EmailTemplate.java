8177eead054b05d3d296ea42c1866fa468607697
Raghu Raman <rraagghhu@gmail.com>
2015-07-28 21:57:00 +0530

    Email Utility.

---
+++ src/main/java/com/pratilipi/email/template/EmailTemplate.java
@@ -0,0 +1,35 @@
+package com.pratilipi.email.template;
+
+import java.io.Serializable;
+
+public interface EmailTemplate extends Serializable {
+	
+	String getId();
+
+	void setId( String id );
+
+	String getSenderName();
+
+	void setSenderName( String senderName );
+
+	String getSenderEmail();
+
+	void setSenderEmail( String senderEmail );
+
+	String getReplyToName();
+
+	void setReplyToName( String replyToName );
+
+	String getReplyToEmail();
+
+	void setReplyToEmail( String replyToEmail );
+
+	String getSubject();
+
+	void setSubject( String subject );
+
+	String getBody();
+
+	void setBody( String body );
+	
+}
