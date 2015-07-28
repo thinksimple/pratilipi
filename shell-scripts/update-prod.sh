b2fd8d2329b80034845f438ea083b145845fa8c7
Prashant Gutpa <antshpra@gmail.com>
2015-07-29 02:32:04 +0530

    Updating shell scripts.

--- shell-scripts/update-prod.sh
+++ shell-scripts/update-prod.sh
@@ -1,5 +1,10 @@
 RELEASE_COMMIT_ID="d096cd724026065a2307c0d2acfbd3081995e7b4"
 
+export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.3
+export M2=$M2_HOME/bin
+export MAVEN_OPTS="-Xms256m -Xmx512m"
+export PATH=$M2:$PATH
+
 if [ $(git rev-parse HEAD) != $RELEASE_COMMIT_ID ]
 then
 	git fetch
--- shell-scripts/update-test.sh
+++ shell-scripts/update-test.sh
@@ -1,3 +1,8 @@
+export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.3
+export M2=$M2_HOME/bin
+export MAVEN_OPTS="-Xms256m -Xmx512m"
+export PATH=$M2:$PATH
+
 git remote update
 
 if [ $(git rev-parse HEAD) != $(git rev-parse origin) ]
