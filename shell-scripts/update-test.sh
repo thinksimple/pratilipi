4e93c67bee3639305cfc8f8a342cab8690fd9ea2
Prashant Gutpa <antshpra@gmail.com>
2015-07-29 00:23:40 +0530

    Updating update-test.sh

--- shell-scripts/update-test.sh
+++ shell-scripts/update-test.sh
@@ -14,4 +14,12 @@ then
 	
 	# Update prod-pratilipi/gamma
 	mvn appengine:update -Dapp.id=prod-pratilipi -Dapp.module=gamma
+	
+	cp src/main/webapp/WEB-INF/worker-web.xml			src/main/webapp/WEB-INF/web.xml
+	cp src/main/webapp/WEB-INF/worker-appengine-web.xml	src/main/webapp/WEB-INF/appengine-web.xml
+	cp src/main/webapp/WEB-INF/worker-queue.xml			src/main/webapp/WEB-INF/queue.xml
+	
+	# Update devo-pratilipi/worker
+	mvn appengine:update -Dapp.id=devo-pratilipi -Dapp.module=worker
+	
 fi
