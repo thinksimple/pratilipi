a1312919c8bb74d159e0f6374cdb43b50fdda56b
Prashant Gutpa <antshpra@gmail.com>
2015-07-29 02:56:44 +0530

    Bug fix in startup.sh

--- shell-scripts/startup.sh
+++ shell-scripts/startup.sh
@@ -4,10 +4,10 @@ do
 	date
 	
 	cd ~/android/pratilipi
-	sudo bash shell-scripts/update-android.sh 2>&1 | logger
+	sudo bash ~/test/pratilipi/shell-scripts/update-android.sh 2>&1 | logger
 	
 	cd ~/test/pratilipi
-	sudo bash shell-scripts/update-test.sh 2>&1 | logger
+	sudo bash ~/test/pratilipi/shell-scripts/update-test.sh 2>&1 | logger
 	
 	sleep 60
 
