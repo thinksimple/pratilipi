551b8049c6bfc73899a3dddb1668ad16ad4b92be
Prashant Gutpa <antshpra@gmail.com>
2015-07-30 01:38:00 +0530

    Updating android module.

--- shell-scripts/update-android.sh
+++ shell-scripts/update-android.sh
@@ -1,4 +1,4 @@
-RELEASE_COMMIT_ID="80f7770e7ef7d87eea5303f26a172446e8f2a573"
+RELEASE_COMMIT_ID="decd4236ec1d57ee3f5130b8b51d5102fcf995c2"
 
 export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.3
 export M2=$M2_HOME/bin
