857ed3b768449d075172ec68dc6269f41e44f16a
Prashant Gutpa <antshpra@gmail.com>
2015-07-29 09:33:26 +0530

    Updating startup.sh

--- shell-scripts/startup.sh
+++ shell-scripts/startup.sh
@@ -3,11 +3,11 @@ do
 
 	date
 	
-	cd ~/android/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/update-android.sh 2>&1 | logger
-	
 	cd ~/test/pratilipi
 	sudo bash ~/test/pratilipi/shell-scripts/update-test.sh 2>&1 | logger
+	
+	cd ~/android/pratilipi
+	sudo bash ~/test/pratilipi/shell-scripts/update-android.sh 2>&1 | logger
 	
 	sleep 60
 
