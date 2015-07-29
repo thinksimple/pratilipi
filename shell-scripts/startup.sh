adefb42fdbc2333c682f350d12abb5aca6f77874
Prashant Gutpa <antshpra@gmail.com>
2015-07-29 22:19:36 +0530

    Updating startup.sh

--- shell-scripts/startup.sh
+++ shell-scripts/startup.sh
@@ -6,9 +6,15 @@ do
 	cd ~/test/pratilipi
 	sudo bash ~/test/pratilipi/shell-scripts/update-test.sh 2>&1 | logger
 	
+	cd ~/prod/pratilipi
+	sudo bash ~/test/pratilipi/shell-scripts/update-prod.sh 2>&1 | logger
+
 	cd ~/android/pratilipi
 	sudo bash ~/test/pratilipi/shell-scripts/update-android.sh 2>&1 | logger
 	
+	cd ~/worker/pratilipi
+	sudo bash ~/test/pratilipi/shell-scripts/update-worker.sh 2>&1 | logger
+
 	sleep 60
 
 done
