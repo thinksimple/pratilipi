22108e033b715f3c92f5a5ffa5475efdb2236c7b
Prashant Gutpa <antshpra@gmail.com>
2015-09-14 16:22:56 +0530

    Updating WorkerService.

--- src/main/java/com/pratilipi/service/WorkerService.java
+++ src/main/java/com/pratilipi/service/WorkerService.java
@@ -2,11 +2,13 @@ package com.pratilipi.service;
 
 import com.pratilipi.api.ApiRegistry;
 import com.pratilipi.api.GenericService;
+import com.pratilipi.api.author.AuthorBackupApi;
 import com.pratilipi.api.author.AuthorProcessApi;
 import com.pratilipi.api.init.InitApi;
 import com.pratilipi.api.pratilipi.PratilipiBackupApi;
 import com.pratilipi.api.pratilipi.PratilipiIdfApi;
 import com.pratilipi.api.pratilipi.PratilipiProcessApi;
+import com.pratilipi.api.user.UserBackupApi;
 
 @SuppressWarnings("serial")
 public class WorkerService extends GenericService {
@@ -19,6 +21,10 @@ public class WorkerService extends GenericService {
 		ApiRegistry.register( PratilipiIdfApi.class );
 
 		ApiRegistry.register( AuthorProcessApi.class );
+		ApiRegistry.register( AuthorBackupApi.class );
+
+		ApiRegistry.register( UserBackupApi.class );
+		
 	}
 	
 }
