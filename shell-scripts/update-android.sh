4e6596fd5c5ad2e7cc989a4926da50f99c55a35f
Prashant Gutpa <antshpra@gmail.com>
2015-09-16 11:44:14 +0530

    Updating update-android script.

--- shell-scripts/update-android.sh
+++ shell-scripts/update-android.sh
@@ -1,14 +1,14 @@
-RELEASE_COMMIT_ID="decd4236ec1d57ee3f5130b8b51d5102fcf995c2"
-
 export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.3
 export M2=$M2_HOME/bin
 export MAVEN_OPTS="-Xms256m -Xmx512m"
 export PATH=$M2:$PATH
 
-if [ $(git rev-parse HEAD) != $RELEASE_COMMIT_ID ]
+git remote update
+
+if [ $(git rev-parse HEAD) != $(git rev-parse origin/release-android) ]
 then
 	git fetch
-	git reset --hard $RELEASE_COMMIT_ID
+	git reset --hard origin/release-android
 	
 	mvn clean
 	mvn install
