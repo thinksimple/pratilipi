23f31f1f5a2211198bdce1b1bc521e8d0f46fefa
Raghu Raman <rraagghhu@gmail.com>
2015-09-24 15:41:36 +0530

    - Validating facebook user.

--- src/main/java/com/pratilipi/common/util/FacebookApi.java
+++ src/main/java/com/pratilipi/common/util/FacebookApi.java
@@ -26,7 +26,7 @@ public class FacebookApi {
 	
 	private static final String GRAPH_API_2p2_URL = "https://graph.facebook.com/v2.2";
 	private static final String GRAPH_API_2p4_URL = "https://graph.facebook.com/v2.4";
-	private static final String GRAPH_API_DEBUG_TOKEN_URL = "https://graph.facebook.com/debug_token";
+	private static final String GRAPH_API_URL = "https://graph.facebook.com/";
 	
 	
 	public static String getAppId() {
@@ -41,11 +41,25 @@ public class FacebookApi {
 		return facebookCredentials.get( "appId" ) + "|" + facebookCredentials.get( "appSecret" );
 	}
 	
-	public static Boolean validateUserAccessToken( String fbUserId, String fbUserAccessToken )
+	public static String getFbUserEmail( String fbUserId ) {
+		String requestUrl = GRAPH_API_URL
+				+ "?id=" + fbUserId 
+				+ "&access_token="
+				+ getAccessToken();
+		String responsePayload = null;
+		try {
+			responsePayload = IOUtils.toString( new URL( requestUrl ).openStream(), "UTF-8" );
+		} catch ( IOException e ) {
+			logger.log( Level.SEVERE, "Failed to access Graph Api.", e );		}
+		return new Gson().fromJson( responsePayload, JsonElement.class ).getAsJsonObject().get( "email" ).getAsString();
+	}
+	
+	public static Boolean validateUserAccessToken( String fbUserId, String fbUserEmail, String fbUserAccessToken )
 			throws UnexpectedServerException {
 		
 		try {
-			String requestUrl = GRAPH_API_DEBUG_TOKEN_URL
+			String requestUrl = GRAPH_API_URL
+					+ "debug_token"
 					+ "?input_token=" + fbUserAccessToken
 					+ "&access_token=" + getAccessToken();
 			String responsePayload = IOUtils.toString( new URL( requestUrl ).openStream(), "UTF-8" );
@@ -58,6 +72,14 @@ public class FacebookApi {
 				return false;
 
 			JsonObject responseDataJson = responseJson.get( "data" ).getAsJsonObject();
+			
+			if( fbUserEmail != null ) {
+				return responseDataJson.get( "is_valid" ).getAsBoolean()
+					&& responseDataJson.get( "user_id" ).equals( fbUserId )
+					&& responseDataJson.get( "app_id" ).equals( getAppId() )
+					&& getFbUserEmail( responseDataJson.get( "user_id" ).getAsString() ).equals( fbUserEmail );
+			}
+				
 			return responseDataJson.get( "is_valid" ).getAsBoolean()
 					&& responseDataJson.get( "user_id" ).equals( fbUserId )
 					&& responseDataJson.get( "app_id" ).equals( getAppId() );
