2da04536099c635480626b22f8bcae00fd3dabc5
antshpra <antshpra@gmail.com>
2015-11-02 16:23:06 +0530

    Updating startup.sh

--- shell-scripts/startup.sh
+++ shell-scripts/startup.sh
@@ -9,6 +9,9 @@ do
 	cd ~/android/pratilipi
 	sudo bash ~/test/pratilipi/shell-scripts/update-android.sh 2>&1 | logger
 	
+	cd ~/backend/pratilipi
+	sudo bash ~/test/pratilipi/shell-scripts/update-backend.sh 2>&1 | logger
+	
 	cd ~/worker/pratilipi
 	sudo bash ~/test/pratilipi/shell-scripts/update-worker.sh 2>&1 | logger
 
