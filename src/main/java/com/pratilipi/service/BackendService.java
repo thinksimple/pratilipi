a82fc205844024729c6060b267c93560624cfa32
antshpra <antshpra@gmail.com>
2015-11-02 16:28:11 +0530

    Implementing BackendService.

---
+++ src/main/java/com/pratilipi/service/BackendService.java
@@ -0,0 +1,17 @@
+package com.pratilipi.service;
+
+import com.pratilipi.api.ApiRegistry;
+import com.pratilipi.api.GenericService;
+import com.pratilipi.api.author.AuthorImageApi;
+import com.pratilipi.api.pratilipi.PratilipiCoverApi;
+
+@SuppressWarnings("serial")
+public class BackendService extends GenericService {
+	
+	static {
+		ApiRegistry.register( PratilipiCoverApi.class );
+		
+		ApiRegistry.register( AuthorImageApi.class );
+	}
+	
+}
