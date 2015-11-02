11ceee05e2d389366467ea7e6a71c8ce7d024ad4
antshpra <antshpra@gmail.com>
2015-11-02 22:40:14 +0530

    Registering UserLoginFacebookApi in ApiService.

--- src/main/java/com/pratilipi/service/ApiService.java
+++ src/main/java/com/pratilipi/service/ApiService.java
@@ -4,12 +4,13 @@ import com.pratilipi.api.ApiRegistry;
 import com.pratilipi.api.GenericService;
 import com.pratilipi.api.author.AuthorImageApi;
 import com.pratilipi.api.pratilipi.PratilipiCoverApi;
+import com.pratilipi.api.user.UserLoginFacebookApi;
 
 @SuppressWarnings("serial")
 public class ApiService extends GenericService {
 	
 	static {
-		ApiRegistry.register( PratilipiCoverApi.class );
+		ApiRegistry.register( UserLoginFacebookApi.class );
 
 		ApiRegistry.register( PratilipiCoverApi.class );
 		
