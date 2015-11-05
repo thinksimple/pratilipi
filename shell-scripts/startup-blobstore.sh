d7e0e2520a4ac74f3ffddcc8aa3c1906320a0a70
Prashant Gutpa <antshpra@gmail.com>
2015-11-05 22:14:16 +0530

    Minor bug fix in startup-blobstore.sh

--- shell-scripts/startup-blobstore.sh
+++ shell-scripts/startup-blobstore.sh
@@ -4,7 +4,7 @@ do
 	logger "_BLOBSTORE_SCRIPT_RUNNING_"
 	
 	cd ~/test/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/backup-blobstore.sh 2>&1 | logger
+	sudo bash ~/test/pratilipi/shell-scripts/blobstore-backup.sh 2>&1 | logger
 	
 	sleep 30
 
