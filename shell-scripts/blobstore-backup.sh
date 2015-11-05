a67dc307726e4137c4a315498247841290c10ec6
Prashant Gutpa <antshpra@gmail.com>
2015-11-05 23:12:48 +0530

    Updating blobstore shell scripts.

--- shell-scripts/blobstore-backup.sh
+++ shell-scripts/blobstore-backup.sh
@@ -16,10 +16,8 @@ if [ $hour -ge 8 -a $hour -le 20 -a $minute -eq 0 ]
 then
 	{ #try block
 		gsutil -m cp -r $bucket_static "$bucket_backup/$backup_folder"
-		gsutil -m du -s $bucket_static "$bucket_backup/$backup_folder"
-
-		gsutil -m du -sh $bucket_static $bucket_public $bucket_backup
-
+		gsutil -m du -s $bucket_static "$bucket_backup/$backup_folder" 2>&1 | logger
+		gsutil -m du -sh $bucket_static $bucket_public $bucket_backup 2>&1 | logger
 		logger "_BLOBSTORE_BACKUP_SUCCEEDED_"
 	} || { #catch block
 		logger "_BLOBSTORE_BACKUP_FAILED_"
--- shell-scripts/startup-blobstore.sh
+++ shell-scripts/startup-blobstore.sh
@@ -4,7 +4,7 @@ do
 	logger "_BLOBSTORE_SCRIPT_RUNNING_"
 	
 	cd ~/test/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/blobstore-backup.sh 2>&1 | logger
+	sudo bash ~/test/pratilipi/shell-scripts/blobstore-backup.sh
 	
 	sleep 30
 
