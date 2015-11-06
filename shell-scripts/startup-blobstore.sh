35068d8be6f12e938d01096d431146607bfac523
Prashant Gutpa <antshpra@gmail.com>
2015-11-06 12:24:03 +0530

    Updating logging in shell scripts.

--- shell-scripts/startup-blobstore.sh
+++ shell-scripts/startup-blobstore.sh
@@ -4,10 +4,10 @@ do
 	logger -t blobstore_script "_BLOBSTORE_SCRIPT_RUNNING_"
 	
 	cd ~/test/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/blobstore-backup.sh 2>&1 | grep -v "Copying " | logger -t blobstore_script
+	sudo bash ~/test/pratilipi/shell-scripts/blobstore-backup.sh 2>&1 | grep -v "^Copying "            | logger -t blobstore_script
 	
 	cd ~/test/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/blobstore-meta.sh 2>&1 | grep -v "Setting metadata on" | logger -t blobstore_script
+	sudo bash ~/test/pratilipi/shell-scripts/blobstore-meta.sh   2>&1 | grep -v "^Setting metadata on" | logger -t blobstore_script
 	
 	sleep 30
 
--- shell-scripts/startup-update.sh
+++ shell-scripts/startup-update.sh
@@ -4,19 +4,19 @@ do
 	logger -t cont_dep_script "_CONTINUOUS_DEPLOYMENT_RUNNING_"
 	
 	cd ~/prod/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/update-prod.sh 2>&1 | logger -t cont_dep_script
+	sudo bash ~/test/pratilipi/shell-scripts/update-prod.sh    2>&1 | grep -v "^\[INFO\]" | logger -t cont_dep_script
 
 	cd ~/android/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/update-android.sh 2>&1 | logger -t cont_dep_script
+	sudo bash ~/test/pratilipi/shell-scripts/update-android.sh 2>&1 | grep -v "^\[INFO\]" | logger -t cont_dep_script
 	
 	cd ~/api/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/update-api.sh 2>&1 | logger -t cont_dep_script
+	sudo bash ~/test/pratilipi/shell-scripts/update-api.sh     2>&1 | grep -v "^\[INFO\]" | logger -t cont_dep_script
 	
 	cd ~/worker/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/update-worker.sh 2>&1 | logger -t cont_dep_script
+	sudo bash ~/test/pratilipi/shell-scripts/update-worker.sh  2>&1 | grep -v "^\[INFO\]" | logger -t cont_dep_script
 
 	cd ~/test/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/update-test.sh 2>&1 | logger -t cont_dep_script
+	sudo bash ~/test/pratilipi/shell-scripts/update-test.sh    2>&1 | grep -v "^\[INFO\]" | logger -t cont_dep_script
 	
 	sleep 60
 
