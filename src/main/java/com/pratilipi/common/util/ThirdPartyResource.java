59ec71aeff8d3ce33a8d33e71b4a3f25e5bdd06f
Prashant Gutpa <antshpra@gmail.com>
2015-11-06 23:49:00 +0530

    Updating ThirdPartyResource.

--- src/main/java/com/pratilipi/common/util/ThirdPartyResource.java
+++ src/main/java/com/pratilipi/common/util/ThirdPartyResource.java
@@ -2,20 +2,20 @@ package com.pratilipi.common.util;
 
 public enum ThirdPartyResource {
 	
-	JQUERY( "<script src='//jquery.__DOMAIN__/jquery-2.1.4/jquery-2.1.4.min.js'></script>" ),
+	JQUERY( "<script src='//j.__DOMAIN__/jquery-2.1.4/jquery-2.1.4.min.js'></script>" ),
 
-	BOOTSTRAP( "<script src='//bootstrap.__DOMAIN__/bootstrap-3.3.4/js/bootstrap.min.js'></script>"
-			 + "<link rel='stylesheet' href='//bootstrap.__DOMAIN__/bootstrap-3.3.4/css/bootstrap.min.css'>" ),
+	BOOTSTRAP( "<script src='//b.__DOMAIN__/bootstrap-3.3.4/js/bootstrap.min.js'></script>"
+			 + "<link rel='stylesheet' href='//b.__DOMAIN__/bootstrap-3.3.4/css/bootstrap.min.css'>" ),
 
-	FONT_AWESOME( "<link rel='stylesheet' href='//fontawesome.__DOMAIN__/font-awesome-4.3.0/css/font-awesome.min.css'>" ),
+	FONT_AWESOME( "<link rel='stylesheet' href='//f.__DOMAIN__/font-awesome-4.3.0/css/font-awesome.min.css'>" ),
 
-	POLYMER( "<script src='//polymer.__DOMAIN__/polymer-1.0/webcomponentsjs/webcomponents-lite.js'></script>"
-		   + "<link rel='import' href='//polymer.__DOMAIN__/polymer-1.0/polymer/polymer.html'>" ),
+	POLYMER( "<script src='//p.__DOMAIN__/polymer-1.0/webcomponentsjs/webcomponents-lite.js'></script>"
+		   + "<link rel='import' href='//p.__DOMAIN__/polymer-1.0/polymer/polymer.html'>" ),
 
-	POLYMER_IRON_AJAX( "<link rel='import' href='//polymer.__DOMAIN__/polymer-1.0/iron-ajax/iron-ajax.html'>" ),
-	POLYMER_IRON_COLLAPSE( "<link rel='import' href='//polymer.__DOMAIN__/polymer-1.0/iron-collapse/iron-collapse.html'>" ),
-	POLYMER_IRON_OVERLAY_BEHAVIOR( "<link rel='import' href='//polymer.__DOMAIN__/polymer-1.0/iron-overlay-behavior/iron-overlay-behavior.html'>" ),
-	POLYMER_IRON_RESIZABLE_BEHAVIOR( "<link rel='import' href='//polymer.__DOMAIN__/polymer-1.0/iron-resizable-behavior/iron-resizable-behavior.html'>" ),
+	POLYMER_IRON_AJAX( "<link rel='import' href='//p.__DOMAIN__/polymer-1.0/iron-ajax/iron-ajax.html'>" ),
+	POLYMER_IRON_COLLAPSE( "<link rel='import' href='//p.__DOMAIN__/polymer-1.0/iron-collapse/iron-collapse.html'>" ),
+	POLYMER_IRON_OVERLAY_BEHAVIOR( "<link rel='import' href='//p.__DOMAIN__/polymer-1.0/iron-overlay-behavior/iron-overlay-behavior.html'>" ),
+	POLYMER_IRON_RESIZABLE_BEHAVIOR( "<link rel='import' href='//p.__DOMAIN__/polymer-1.0/iron-resizable-behavior/iron-resizable-behavior.html'>" ),
 	;
 	
 	
