6f370e3b71eb2b327bb345ad2cedeb7245659326
Prashant Gutpa <antshpra@gmail.com>
2015-11-06 18:50:10 +0530

    Validate annotations in PostUserValidationFacebookRequest.

--- src/main/java/com/pratilipi/api/user/shared/PostUserValidationFacebookRequest.java
+++ src/main/java/com/pratilipi/api/user/shared/PostUserValidationFacebookRequest.java
@@ -1,11 +1,14 @@
 package com.pratilipi.api.user.shared;
 
+import com.pratilipi.api.annotation.Validate;
 import com.pratilipi.api.shared.GenericRequest;
 
 public class PostUserValidationFacebookRequest extends GenericRequest {
 	
+	@Validate( required = true )
 	private String fbAccessToken;
 	
+	@Validate( required = true )
 	private String pratilipiAccessToken;
 	
 	
