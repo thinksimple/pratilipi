38222d736a9c87dc7096a358bcd88812afa56f4a
Prashant Gutpa <antshpra@gmail.com>
2015-11-06 12:11:19 +0530

    Updating logging in blobstore shell scripts.

--- shell-scripts/blobstore-backup.sh
+++ shell-scripts/blobstore-backup.sh
@@ -16,8 +16,8 @@ backup_folder_public="$(date +%Y-%m-%d)/public.pratilipi.com-$(date +%Y-%m-%d-%H
 
 if [ $hour -ge 8 -a $hour -le 20 -a $minute -eq 0 ]
 then
-	gsutil -m cp -r $bucket_static "$bucket_backup/$backup_folder_static" 1> /dev/null
-	gsutil -m cp -r $bucket_static "$bucket_backup/$backup_folder_public" 1> /dev/null
+	gsutil -m cp -r $bucket_static "$bucket_backup/$backup_folder_static"
+	gsutil -m cp -r $bucket_static "$bucket_backup/$backup_folder_public"
 	gsutil -m du -s $bucket_static "$bucket_backup/$backup_folder_static"
 	gsutil -m du -s $bucket_public "$bucket_backup/$backup_folder_public"
 	gsutil -m du -sh $bucket_static $bucket_public $bucket_backup
