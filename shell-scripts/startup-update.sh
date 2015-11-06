8f6141fe59f23e747652f34d7f1177195ed59ae5
Prashant Gutpa <antshpra@gmail.com>
2015-11-06 12:56:04 +0530

    Updating logging in start-update.sh

--- shell-scripts/startup-update.sh
+++ shell-scripts/startup-update.sh
@@ -4,20 +4,20 @@ do
 	logger -t ping "_CONTINUOUS_DEPLOYMENT_RUNNING_"
 	
 	cd ~/prod/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/update-prod.sh    2>&1 | grep -v "^\[INFO\]" | logger -t cont_dep_script
+	sudo bash ~/test/pratilipi/shell-scripts/update-prod.sh    2>&1 | grep -v "^\[INFO\]" | grep -v "Fetching origin" | logger -t cont_dep_script
 
 	cd ~/android/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/update-android.sh 2>&1 | grep -v "^\[INFO\]" | logger -t cont_dep_script
+	sudo bash ~/test/pratilipi/shell-scripts/update-android.sh 2>&1 | grep -v "^\[INFO\]" | grep -v "Fetching origin" | logger -t cont_dep_script
 	
 	cd ~/api/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/update-api.sh     2>&1 | grep -v "^\[INFO\]" | logger -t cont_dep_script
+	sudo bash ~/test/pratilipi/shell-scripts/update-api.sh     2>&1 | grep -v "^\[INFO\]" | grep -v "Fetching origin" | logger -t cont_dep_script
 	
 	cd ~/worker/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/update-worker.sh  2>&1 | grep -v "^\[INFO\]" | logger -t cont_dep_script
+	sudo bash ~/test/pratilipi/shell-scripts/update-worker.sh  2>&1 | grep -v "^\[INFO\]" | grep -v "Fetching origin" | logger -t cont_dep_script
 
 	cd ~/test/pratilipi
-	sudo bash ~/test/pratilipi/shell-scripts/update-test.sh    2>&1 | grep -v "^\[INFO\]" | logger -t cont_dep_script
+	sudo bash ~/test/pratilipi/shell-scripts/update-test.sh    2>&1 | grep -v "^\[INFO\]" | grep -v "Fetching origin" | logger -t cont_dep_script
 	
-	sleep 60
+	sleep 30
 
 done
