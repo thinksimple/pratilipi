097d970715e5571288979b6b6053278c5f4558b3
Raghu Raman <rraagghhu@gmail.com>
2015-11-19 16:39:42 +0530

    - Based on the new algorithm. - Major fix.

--- shell-scripts/blobstore-backup.sh
+++ shell-scripts/blobstore-backup.sh
@@ -11,15 +11,19 @@ bucket_backup="gs://backup.pratilipi.com"
 bucket_static="gs://static.pratilipi.com"
 bucket_public="gs://public.pratilipi.com"
 
-backup_folder_static="static.pratilipi.com/$(date +%Y-%m-%d)/static.pratilipi.com-$(date +%Y-%m-%d-%H:%M-IST)"
-backup_folder_public="public.pratilipi.com/$(date +%Y-%m-%d)/public.pratilipi.com-$(date +%Y-%m-%d-%H:%M-IST)"
+backup_folder_static="static.pratilipi.com/$(date +%d)/static.pratilipi.com-$(date +%H:%M-IST)"
+backup_folder_public="public.pratilipi.com/$(date +%d)/public.pratilipi.com-$(date +%H:%M-IST)"
 
 if [ $hour -ge 8 -a $hour -le 20 -a $minute -eq 0 ]
 then
-	gsutil -m cp -r $bucket_static "$bucket_backup/$backup_folder_static"
-	gsutil -m cp -r $bucket_static "$bucket_backup/$backup_folder_public"
-	gsutil -m du -s $bucket_static "$bucket_backup/$backup_folder_static"
-	gsutil -m du -s $bucket_public "$bucket_backup/$backup_folder_public"
-	gsutil -m du -sh $bucket_static $bucket_public $bucket_backup
-	echo "BlobStore backup completed."
+	gsutil -m rsync -r $bucket_static "$bucket_backup/$backup_folder_static"
+	gsutil -m rsync -r $bucket_public "$bucket_backup/$backup_folder_public"
+
+#   gsutil -m du -s $bucket_static "$bucket_backup/$backup_folder_static"
+#	gsutil -m du -s $bucket_public "$bucket_backup/$backup_folder_public"
+#   gsutil -m du -sh $bucket_static $bucket_public $bucket_backup
+
+	echo "Last Backup Date - $(date)" > $bucket_backup/$backup_folder_public/info
+    echo "Last Backup Date - $(date)" > $bucket_backup/$backup_folder_static/info
+    
 fi
