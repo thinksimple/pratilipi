20b527f6e30eb8527845d77f32b89fd616a972f9
Raghu Raman <rraagghhu@gmail.com>
2015-11-20 12:49:40 +0530

    -Minor fixes in blob-store backup script.

--- shell-scripts/blobstore-backup.sh
+++ shell-scripts/blobstore-backup.sh
@@ -16,14 +16,25 @@ backup_folder_public="public.pratilipi.com/yyyy-mm-$(date +%d)/public.pratilipi.
 
 if [ $hour -ge 8 -a $hour -le 20 -a $minute -eq 0 ]
 then
+
+	echo "Initiated backup at : $(date)" > info
 	gsutil -m rsync -r $bucket_static "$bucket_backup/$backup_folder_static"
 	gsutil -m rsync -r $bucket_public "$bucket_backup/$backup_folder_public"
+	echo "Backup was successfully completed at : $(date)" >> info
+
+	gsutil cp info "$bucket_backup/$backup_folder_static/"
+	gsutil cp info "$bucket_backup/$backup_folder_public/"
+
+	echo "Last Backup taken at : $(date)" > last_backup_info
+	gsutil cp last_backup_info "$bucket_backup/static.pratilipi.com/"
+	gsutil cp last_backup_info "$bucket_backup/public.pratilipi.com/"
+
+	echo "Backup taken at : $(date)" >> logs
+	gsutil cp logs "$bucket_backup/static.pratilipi.com/"
+	gsutil cp logs "$bucket_backup/public.pratilipi.com/"
 
 #	gsutil -m du -s $bucket_static "$bucket_backup/$backup_folder_static"
 #	gsutil -m du -s $bucket_public "$bucket_backup/$backup_folder_public"
 #	gsutil -m du -sh $bucket_static $bucket_public $bucket_backup
 
-	echo "Last Backup Date - $(date)" > $bucket_backup/$backup_folder_public/info
-	echo "Last Backup Date - $(date)" > $bucket_backup/$backup_folder_static/info
-    
 fi
