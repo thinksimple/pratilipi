0f2a79d7c3705b6558cc1087bebff2c4ea44d10a
Raghu Raman <rraagghhu@gmail.com>
2015-11-20 19:43:29 +0530

    - Added a separate util for making network calls. - Added Scraping process in Facebook Api. - Improvements in code.

--- src/main/java/com/pratilipi/common/util/FacebookApi.java
+++ src/main/java/com/pratilipi/common/util/FacebookApi.java
@@ -1,7 +1,6 @@
 package com.pratilipi.common.util;
 
-import java.io.IOException;
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -10,8 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import org.apache.commons.io.IOUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
@@ -30,6 +27,7 @@ public class FacebookApi {
 	
 	private static final String GRAPH_API_2p2_URL = "https://graph.facebook.com/v2.2";
 	private static final String GRAPH_API_2p4_URL = "https://graph.facebook.com/v2.4";
+	private static final String FACEBOOK_RESCRAPE_URL = "http://graph.facebook.com:443";
 	
 	public static String getAppId() {
 		DataAccessor dataAccessor = DataAccessorFactory.getDataAccessor();
@@ -47,97 +45,79 @@ public class FacebookApi {
 	public static Boolean validateUserAccessToken( String userAccessToken ) 
 			throws UnexpectedServerException {
 		
-		try {
-			
-			String requestUrl = GRAPH_API_2p2_URL + "/" + "debug_token"
-					+ "?" + "input_token=" + userAccessToken
-					+ "&" + "access_token=" + getAccessToken();
-			String responsePayload = IOUtils.toString( new URL( requestUrl ).openStream(), "UTF-8" );
-			
-			logger.log( Level.INFO, "Graph Api Request : " + requestUrl );
-			logger.log( Level.INFO, "Graph Api Response : " + responsePayload );
-
-			JsonObject responseJson = new Gson().fromJson( responsePayload, JsonElement.class ).getAsJsonObject();
-			return responseJson.get( "error" ) == null
-					&& responseJson.get( "data" ) != null
-					&& responseJson.get( "data" ).getAsJsonObject().get( "app_id" ).getAsString().equals( getAppId() );
-			
-		} catch ( IOException e ) {
-			logger.log( Level.SEVERE, "Failed to access Facebook graph Api.", e );
-			throw new UnexpectedServerException();
-		}
+		Map<String, String> keyValueParameters = new HashMap<String, String>();
+		keyValueParameters.put( "input_token", userAccessToken );
+		keyValueParameters.put( "access_token", getAccessToken() );
+		String responsePayload = NetworkCallsUtil.makeGetCall( GRAPH_API_2p2_URL + "/debug_token", keyValueParameters );
+		
+		JsonObject responseJson = new Gson().fromJson( responsePayload, JsonElement.class ).getAsJsonObject();
+		return responseJson.get( "error" ) == null
+				&& responseJson.get( "data" ) != null
+				&& responseJson.get( "data" ).getAsJsonObject().get( "app_id" ).getAsString().equals( getAppId() );
 		
 	}
 	
 	public static UserData getUserData( String fbUserAccessToken )
 			throws UnexpectedServerException {
-		
-		try {
-			String requestUrl = GRAPH_API_2p4_URL + "/me?" + "access_token=" + fbUserAccessToken
-					+ "&fields=id,first_name,last_name,gender,birthday,email";
-			String responsePayload = IOUtils.toString( new URL( requestUrl ).openStream(), "UTF-8" );
+
+		Map<String, String> keyValueParameters = new HashMap<String, String>();
+		keyValueParameters.put( "access_token=", fbUserAccessToken );
+		keyValueParameters.put( "fields", "id,first_name,last_name,gender,birthday,email" );
+		String responsePayload = NetworkCallsUtil.makeGetCall( GRAPH_API_2p4_URL + "/me", keyValueParameters );
+
+		JsonObject responseJson = new Gson().fromJson( responsePayload, JsonElement.class ).getAsJsonObject();
+		if( responseJson.get( "error" ) != null ) {
+			// TODO: InvalidArgumentException if fbUserAccessToken is invalid or expired.
+			logger.log( Level.SEVERE, "Error response from Graph Api." );
+			throw new UnexpectedServerException();
+		} else {
+			UserData userData = new UserData( responseJson.get( "id" ).getAsString() );
+			userData.setFirstName( responseJson.get( "first_name" ).getAsString() );
+			userData.setLastName( responseJson.get( "last_name" ).getAsString() );
+			userData.setGender( Gender.valueOf( responseJson.get( "gender" ).getAsString().toUpperCase() ) );
 			
-			logger.log( Level.INFO, "Graph Api Request : " + requestUrl );
-			logger.log( Level.INFO, "Graph Api Response : " + responsePayload );
-
-			JsonObject responseJson = new Gson().fromJson( responsePayload, JsonElement.class ).getAsJsonObject();
-			if( responseJson.get( "error" ) != null ) {
-				// TODO: InvalidArgumentException if fbUserAccessToken is invalid or expired.
-				logger.log( Level.SEVERE, "Error response from Graph Api." );
-				throw new UnexpectedServerException();
-			} else {
-				UserData userData = new UserData( responseJson.get( "id" ).getAsString() );
-				userData.setFirstName( responseJson.get( "first_name" ).getAsString() );
-				userData.setLastName( responseJson.get( "last_name" ).getAsString() );
-				userData.setGender( Gender.valueOf( responseJson.get( "gender" ).getAsString().toUpperCase() ) );
-				
-				if( responseJson.get( "birthday" ) != null ) {
-					try {
-						userData.setDateOfBirth( new SimpleDateFormat( "MM/dd/yyyy" ).parse( responseJson.get( "birthday" ).getAsString() ) );
-					} catch ( ParseException e ) {
-						logger.log( Level.SEVERE, "Failed to parse Date of Birth.", e );
-					}
+			if( responseJson.get( "email" ) != null )
+				userData.setEmail( responseJson.get( "email" ).getAsString() );
+			
+			if( responseJson.get( "birthday" ) != null ) {
+				try {
+					userData.setDateOfBirth( new SimpleDateFormat( "MM/dd/yyyy" ).parse( responseJson.get( "birthday" ).getAsString() ) );
+				} catch ( ParseException e ) {
+					logger.log( Level.SEVERE, "Failed to parse Date of Birth.", e );
 				}
-				
-				if( responseJson.get( "email" ) != null )
-					userData.setEmail( responseJson.get( "email" ).getAsString() );
-
-				return userData;
 			}
 			
-		} catch( IOException e ) {
-			logger.log( Level.SEVERE, "Failed to access Graph Api.", e );
-			throw new UnexpectedServerException();
+			return userData;
 		}
-		
+			
 	}
 	
 	
-	public static long getUrlShareCount( String url ) throws UnexpectedServerException {
+	public static long getUrlShareCount( String url )
+			throws UnexpectedServerException {
+		
+		Map<String, String> keyValueParameters = new HashMap<String, String>();
 		try {
-			String requestUrl = GRAPH_API_2p2_URL
-					+ "?id=" + URLEncoder.encode( url, "UTF-8" )
-					+ "&access_token=" + getAccessToken();
-			logger.log( Level.INFO, "Request URL: " + requestUrl );
-
-			String responsePayload = IOUtils.toString( new URL( requestUrl ).openStream(), "UTF-8" );
-			logger.log( Level.INFO, "Response Payload: " + responsePayload );
-			
-			JsonElement responseJson = new Gson().fromJson( responsePayload, JsonElement.class );
-			JsonElement shareJson = responseJson.getAsJsonObject().get( "share" );
-			if( shareJson == null )
-				return 0L;
-			JsonElement shareCountJson = shareJson.getAsJsonObject().get( "share_count" );
-
-			return shareCountJson != null ? shareCountJson.getAsLong() : 0L; 
-		} catch( IOException e ) {
-			logger.log( Level.SEVERE, "Failed to fetch data from Facebook.", e );
+			keyValueParameters.put( "id", URLEncoder.encode( url, "UTF-8" ) );
+			keyValueParameters.put( "access_token", getAccessToken() );
+		} catch ( UnsupportedEncodingException e ) {
+			logger.log( Level.SEVERE, "Failed to Encode URL : " + url, e );
 			throw new UnexpectedServerException();
 		}
+		
+		String responsePayload = NetworkCallsUtil.makeGetCall( GRAPH_API_2p2_URL, keyValueParameters );
+		
+		JsonElement responseJson = new Gson().fromJson( responsePayload, JsonElement.class );
+		JsonElement shareJson = responseJson.getAsJsonObject().get( "share" );
+		if( shareJson == null )
+			return 0L;
+		JsonElement shareCountJson = shareJson.getAsJsonObject().get( "share_count" );
+
+		return shareCountJson != null ? shareCountJson.getAsLong() : 0L; 
 	}
 	
 	public static Map<String, Long> getUrlShareCount( List<String> urlList ) throws UnexpectedServerException {
-		Gson gson = new Gson();
+		
 		int urlsPerRequest = 20;
 		
 		Map<String, Long> urlCountMap = new HashMap<>();
@@ -148,29 +128,47 @@ public class FacebookApi {
 				urls = urls + urlList.get( i + j ) + ",";
 			urls = urls.substring( 0, urls.length() - 1 );
 
-			try{
-				String requestUrl = GRAPH_API_2p4_URL
-						+ "?ids=" + URLEncoder.encode( urls, "UTF-8" )
-						+ "&access_token=" + getAccessToken();
-				
-				String responsePayload = IOUtils.toString( new URL( requestUrl ).openStream(), "UTF-8" );
-				JsonElement responseJson = gson.fromJson( responsePayload, JsonElement.class );
-				
-				for( int j = 0; i + j < urlList.size() && j < urlsPerRequest; j++ ) {
-					JsonElement jsonElement = responseJson.getAsJsonObject().get( urlList.get( i + j ) );
-					JsonElement shareJson = jsonElement.getAsJsonObject().get( "share" );
-					if( shareJson != null ) {
-						JsonElement shareCountJson = shareJson.getAsJsonObject().get( "share_count" );
-						if( shareCountJson != null )
-							urlCountMap.put( urlList.get( i + j ), shareCountJson.getAsLong() );
-					}
-				}
-			} catch( IOException e ) {
-				logger.log( Level.SEVERE, "Failed to fetch data from Facebook.", e );
+			Map<String, String> keyValueParameters = new HashMap<String, String>();
+			try {
+				keyValueParameters.put( "ids", URLEncoder.encode( urls, "UTF-8" ) );
+				keyValueParameters.put( "access_token", getAccessToken() );
+			} catch ( UnsupportedEncodingException e ) {
+				logger.log( Level.SEVERE, "Failed to Encode URL : " + urls, e );
 				throw new UnexpectedServerException();
 			}
+			
+			String responsePayload = NetworkCallsUtil.makeGetCall( GRAPH_API_2p4_URL, keyValueParameters );
+			JsonElement responseJson = new Gson().fromJson( responsePayload, JsonElement.class );
+			
+			for( int j = 0; i + j < urlList.size() && j < urlsPerRequest; j++ ) {
+				JsonElement jsonElement = responseJson.getAsJsonObject().get( urlList.get( i + j ) );
+				JsonElement shareJson = jsonElement.getAsJsonObject().get( "share" );
+				if( shareJson != null ) {
+					JsonElement shareCountJson = shareJson.getAsJsonObject().get( "share_count" );
+					if( shareCountJson != null )
+						urlCountMap.put( urlList.get( i + j ), shareCountJson.getAsLong() );
+				}
+			}
+			
 		}
 		
 		return urlCountMap;
-	} 
+	}
+	
+	public static void sendScrapeRequestToFacebook( String link ) 
+			throws UnexpectedServerException {
+		
+		Map<String, String> keyValueParameters = new HashMap<String, String>();
+		keyValueParameters.put( "id", link );
+		keyValueParameters.put( "access_token", getAccessToken() );
+		String responsePayload = NetworkCallsUtil.makePostCall( FACEBOOK_RESCRAPE_URL, keyValueParameters );
+		
+		JsonObject responseJson = new Gson().fromJson( responsePayload, JsonObject.class );
+		if( responseJson.get( "error" ) == null ) {
+			logger.log( Level.INFO, "Facebook scrapping for link : " + link + " completed successfully." );
+		} else {
+			logger.log( Level.SEVERE, "Facebook scrapping for link : " + link + " failed with error message : " + responseJson.get( "error" ) );
+			throw new UnexpectedServerException();
+		}
+	}
 }
