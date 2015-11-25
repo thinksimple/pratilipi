2be337cd8b0b59211edc141fcc31e4cf43475b38
Prashant Gutpa <antshpra@gmail.com>
2015-11-25 21:36:56 +0530

    Minor update in startup-blobstore.sh

--- shell-scripts/startup-blobstore.sh
+++ shell-scripts/startup-blobstore.sh
@@ -3,10 +3,10 @@ do
 
 	logger -t ping "_BLOBSTORE_SCRIPT_RUNNING_"
 	
-	cd ~/test/pratilipi
+	cd ~
 	sudo bash ~/test/pratilipi/shell-scripts/blobstore-backup.sh 2>&1 | grep -v "^Copying "            | logger -t blobstore_script
 	
-	cd ~/test/pratilipi
+	cd ~
 	sudo bash ~/test/pratilipi/shell-scripts/blobstore-meta.sh   2>&1 | grep -v "^Setting metadata on" | logger -t blobstore_script
 	
 	sleep 30
