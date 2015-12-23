bc993f688a667c345dd9fd0d4840fadc3fd9421f
Raghu Raman <rraagghhu@gmail.com>
2015-12-23 19:06:51 +0530

    - Updating ThirdPartyResource.

--- src/main/java/com/pratilipi/common/util/ThirdPartyResource.java
+++ src/main/java/com/pratilipi/common/util/ThirdPartyResource.java
@@ -25,13 +25,13 @@ public enum ThirdPartyResource {
 	POLYMER_PAPER_DROPDOWN_MENU		( "<link rel='import' href='/third-party/polymer-1.0/paper-dropdown-menu/paper-dropdown-menu.html'>" ),
 	POLYMER_PAPER_FAB				( "<link rel='import' href='/third-party/polymer-1.0/paper-fab/paper-fab.html'>" ),
 	POLYMER_PAPER_ICON_BUTTON		( "<link rel='import' href='/third-party/polymer-1.0/paper-icon-button/paper-icon-button.html'>" ),
-	POLYMER_PAPER_INPUT				( "<link rel='import' href='/third-party/polymer-1.0/paper-input/paper-input.html'>" ),
+	POLYMER_PAPER_INPUT				( "<link rel='import' href='/third-party/polymer-1.0/paper-input/paper-input.html'>"
+									+ "<link rel='import' href='/third-party/polymer-1.0/paper-input/paper-textarea.html'>" ),
 	POLYMER_PAPER_ITEM				( "<link rel='import' href='/third-party/polymer-1.0/paper-item/paper-item.html'>" ),
-	POLYMER_PAPER_LISTBOX			( "<link rel='import' href='/third-party/polymer-1.0/paper-listbox/paper-listbox.html'>" ),
+	POLYMER_PAPER_MENU				( "<link rel='import' href='/third-party/polymer-1.0/paper-menu/paper-menu.html'>" ),
 	POLYMER_PAPER_SLIDER			( "<link rel='import' href='/third-party/polymer-1.0/paper-slider/paper-slider.html'>" ),
 	POLYMER_PAPER_HEADER_PANEL		( "<link rel='import' href='/third-party/polymer-1.0/paper-header-panel/paper-header-panel.html'>"
 										+ "<link type='text/css' rel='stylesheet' href='/third-party/polymer-1.0/paper-header-panel/paper-header-panel.css'>" )
-	
 	;
 	
 	
