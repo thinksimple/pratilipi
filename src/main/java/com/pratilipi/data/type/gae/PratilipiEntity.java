ad27116d6f3370f2f045054d90241e0aa74375fa
Prashant Gutpa <antshpra@gmail.com>
2016-01-13 12:26:09 +0530

    Bug fix in Pratilipi & Author Entities.

--- src/main/java/com/pratilipi/data/type/gae/PratilipiEntity.java
+++ src/main/java/com/pratilipi/data/type/gae/PratilipiEntity.java
@@ -182,6 +182,29 @@ public class PratilipiEntity implements Pratilipi {
 
 	@Override
 	public void setLanguage( Language language ) {
+		switch( language ) {
+			case HINDI:
+				languageId = 5130467284090880L;
+				break;
+			case GUJARATI:
+				languageId = 5965057007550464L;
+				break;
+			case TAMIL:
+				languageId = 6319546696728576L;
+				break;
+			case MARATHI:
+				languageId = 5173513199550464L;
+				break;
+			case MALAYALAM:
+				languageId = 5752669171875840L;
+				break;
+			case BENGALI:
+				languageId = 6235363433512960L;
+				break;
+			case ENGLISH:
+				languageId = 6213615354904576L;
+				break;
+		}
 		this.language = language;
 	}
 
