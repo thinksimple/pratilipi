4785d9a0d1a3b90a6013d9d100c32097d67a3dbf
Prashant Gutpa <antshpra@gmail.com>
2016-01-18 20:40:13 +0530

    Bug fix in AuthorDataUtil.

--- src/main/java/com/pratilipi/api/impl/userpratilipi/shared/GenericUserPratilipiResponse.java
+++ src/main/java/com/pratilipi/api/impl/userpratilipi/shared/GenericUserPratilipiResponse.java
@@ -1,6 +1,7 @@
 package com.pratilipi.api.impl.userpratilipi.shared;
 
 import com.pratilipi.api.shared.GenericResponse;
+import com.pratilipi.common.type.UserReviewState;
 
 @SuppressWarnings("unused")
 public class GenericUserPratilipiResponse extends GenericResponse {
@@ -12,6 +13,7 @@ public class GenericUserPratilipiResponse extends GenericResponse {
 	private Integer rating;
 	private String reviewTitle;
 	private String review;
+	private UserReviewState reviewState;
 	private Long reviewDateMills;
 	private Boolean hasAccessToReview;
 	
