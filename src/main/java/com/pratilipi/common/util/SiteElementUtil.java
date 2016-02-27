a613b36d5155a4d422bb104677fb1db127b1c421
Prashant Gutpa <antshpra@gmail.com>
2016-02-27 18:04:39 +0530

    Updating SiteElementUtil.

--- src/main/java/com/pratilipi/common/util/SiteElementUtil.java
+++ src/main/java/com/pratilipi/common/util/SiteElementUtil.java
@@ -8,10 +8,6 @@ import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.LineIterator;
-
-import com.google.gson.Gson;
 import com.pratilipi.common.exception.UnexpectedServerException;
 import com.pratilipi.common.type.Language;
 import com.pratilipi.i18n.I18n;
@@ -26,9 +22,7 @@ public class SiteElementUtil {
 			ClassNotFoundException {
 
 		String elementFolderName = args[0];
-		String elementDataFolder = args[1];
-		String elementDataModelPackage = args[2];
-		String i18nElementFolderNamePrefix = args[3];
+		String i18nElementFolderNamePrefix = args[1];
 		
 		
 		for( Language language : Language.values() ) {
@@ -44,12 +38,11 @@ public class SiteElementUtil {
 
 				// Data model required for i18n element generation
 				Map<String, Object> dataModel = new HashMap<>();
-				dataModel.put( "language", language );
 				dataModel.put( "lang", language.getCode() );
+				dataModel.put( "language", language );
 				dataModel.put( "domain", language.getHostName() );
 				dataModel.put( "fbAppId", "293990794105516" );
 				dataModel.put( "_strings", I18n.getStrings( language ) );
-				dataModel.put( "data", getElementData( elementName, language.getCode(), elementDataFolder, elementDataModelPackage ) );
 
 				// I18n element file output stream
 				File i18nElementFolder = new File( i18nElementFolderNamePrefix + language.getCode() );
@@ -69,37 +62,6 @@ public class SiteElementUtil {
 			}
 			
 		}
-	}
-	
-	private static Object getElementData( String elementName, String lang,
-			String elementDataFolder, String elementDataModelPackage )
-			throws IOException, ClassNotFoundException {
-
-		File file = new File( elementDataFolder + "/" + elementName.replaceFirst( "\\.html$", "." + lang + ".json" ) );
-		if( ! file.exists() )
-			file = new File( elementDataFolder + "/" + elementName.replaceFirst( "\\.html$", "." + Language.ENGLISH.getCode() + ".json" ) );
-		if( ! file.exists() )
-			return null;
-
-		// Fetching content (json) from data file
-		String jsonStr = "";
-		LineIterator it = FileUtils.lineIterator( file, "UTF-8" );
-		try {
-			while( it.hasNext() )
-				jsonStr += it.nextLine() + '\n';
-		} finally {
-			LineIterator.closeQuietly( it );
-		}
-
-		// Creating class object of data model class
-		String className = "";
-		for( String str : elementName.split( "-" ) )
-			className += Character.toUpperCase( str.charAt( 0 ) ) + str.substring( 1 );
-		className = className.substring( 0, className.indexOf( '.' ) );
-		Class<?> clazz = Class.forName( elementDataModelPackage + "." + className );
-
-		// The magic
-		return new Gson().fromJson( jsonStr, clazz );
 	}
 	
 }
