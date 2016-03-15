ba8f86ffeccee3f3f3ae7c92d69d5755063fad19
Raghu Raman <rraagghhu@gmail.com>
2016-03-15 20:09:32 +0530

    - Minor change in the Api.

--- src/main/java/com/pratilipi/api/impl/userpratilipi/UserLibraryApi.java
+++ src/main/java/com/pratilipi/api/impl/userpratilipi/UserLibraryApi.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.pratilipi.api.GenericApi;
 import com.pratilipi.api.annotation.Bind;
 import com.pratilipi.api.annotation.Get;
-import com.pratilipi.api.impl.pratilipi.shared.GetPratilipiListResponse;
 import com.pratilipi.api.shared.GenericRequest;
 import com.pratilipi.common.exception.InsufficientAccessException;
 import com.pratilipi.data.client.PratilipiData;
@@ -18,17 +17,17 @@ import com.pratilipi.filter.AccessTokenFilter;
 public class UserLibraryApi extends GenericApi {
 	
 	@Get
-	public static GetPratilipiListResponse getUserPratilipiList( GenericRequest request ) 
+	public static List<PratilipiData> getUserPratilipiList( GenericRequest request ) 
 			throws InsufficientAccessException {
 		
 		Long userId = AccessTokenFilter.getAccessToken().getUserId();
 		if( userId.equals( 0L ) )
-			return new GetPratilipiListResponse( null, null );
+			return null;
 		
 		List<Long> pratilipiIdList = UserPratilipiDataUtil.getUserLibraryPratilipiList( userId );
 		if( pratilipiIdList == null || pratilipiIdList.size() == 0 )
-			return new GetPratilipiListResponse( null, null );
-		List<PratilipiData> pratilipiList = PratilipiDataUtil.createPratilipiDataList( pratilipiIdList, true );
-		return new GetPratilipiListResponse( pratilipiList, null );
+			return null;
+		
+		return PratilipiDataUtil.createPratilipiDataList( pratilipiIdList, true );
 	}
 }
