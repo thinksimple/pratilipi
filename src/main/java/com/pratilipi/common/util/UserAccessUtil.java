c3ed1a6a0599870beb6c84d55764dc36b452252b
Raghu Raman <rraagghhu@gmail.com>
2016-04-01 15:18:23 +0530

    Gujarati access to Raghu to create event page.

--- src/main/java/com/pratilipi/common/util/UserAccessUtil.java
+++ src/main/java/com/pratilipi/common/util/UserAccessUtil.java
@@ -82,7 +82,7 @@ public class UserAccessUtil {
 				return new Role[] { Role.MEMBER, Role.ADMIN, Role.ADMIN_KANNADA, Role.ADMIN_MALAYALAM, Role.ADMIN_TAMIL, Role.ADMIN_TELUGU };
 
 			case "6196244602945536": // raghu@
-				return new Role[] { Role.MEMBER, Role.ADMIN, Role.ADMIN_MALAYALAM, Role.ADMIN_TAMIL, Role.ADMIN_TELUGU };
+				return new Role[] { Role.MEMBER, Role.ADMIN, Role.ADMIN_MALAYALAM, Role.ADMIN_TAMIL, Role.ADMIN_TELUGU, Role.ADMIN_GUJARATI };
 			case "5705241014042624": // prashant@
 				return new Role[] { Role.MEMBER, Role.ADMINISTRATOR };
 			
