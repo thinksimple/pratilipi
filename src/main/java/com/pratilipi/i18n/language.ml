d25da46197ed10c12e209eef668226fc751c15ab
Raghu Raman <rraagghhu@gmail.com>
2016-04-07 15:25:52 +0530

    Adding navigation and language files for telugu and malayalam.

---
+++ src/main/java/com/pratilipi/i18n/language.ml
@@ -0,0 +1,300 @@
+# Do NOT edit
+
+pratilipi					= Pratilipi
+tinymce_language			= en
+
+home_page_title				= Read Hindi, Gujarati and Tamil Stories, Poems and Books
+
+facebook_page				= https://www.facebook.com/Pratilipidotcom
+twitter_page				= https://twitter.com/TeamPratilipi
+googleplus_page				= https://plus.google.com/+PratilipiTeam
+linkedin_page				= https://www.linkedin.com/company/pratilipi
+
+
+
+# Language
+
+language_choose_language	= Choose Language
+language_all				= All Languages
+language_hi					= Hindi
+language_gu					= Gujarati
+language_ta					= Tamil
+language_mr					= Marathi
+language_ml					= Malayalam
+language_bn					= Bengali
+language_en					= English
+language_te					= Telugu
+
+
+
+# Pratilipi Types
+
+_pratilipi_type_book		= Book
+_pratilipi_type_books		= Books
+_pratilipi_type_poem		= Poem
+_pratilipi_type_poems		= Poems
+_pratilipi_type_story		= Story
+_pratilipi_type_stories		= Stories
+_pratilipi_type_article		= Article
+_pratilipi_type_articles	= Articles
+_pratilipi_type_magazine	= Magazine
+_pratilipi_type_magazines	= Magazines
+
+
+
+# Author
+
+author_about					= About the Author
+author_language					= Language
+author_since					= Author Since
+
+author_count_works				= WORKS
+author_count_reads				= READS
+author_count_likes				= LIKES
+
+author_edit_profile				= Edit My Profile
+author_update_image				= Upload Photo
+
+author_drafts					= Drafts
+author_published_works			= Published Works
+author_add_new_content			= Add New Content
+
+author_no_contents_published	= Sorry! This Author doesn't have any books published!
+
+
+
+# Pratilipi
+
+pratilipi_type				= Type
+pratilipi_listing_date		= Listing Date
+pratilipi_published			= Published
+pratilipi_published_year	= Published Year
+pratilipi_summary			= Summary
+
+pratilipi_edit_info			= Edit Info
+pratilipi_edit_content		= Edit Content
+pratilipi_update_cover		= Upload Photo
+
+pratilipi_count_reads		= READS
+pratilipi_count_reviews		= REVIEWS
+pratilipi_count_likes		= LIKES
+
+pratilipi_move_to_drafts	= Move to Drafts
+pratilipi_publish_it		= Publish it!
+
+pratilipi_no_reviews		= Sorry! No Reviews found for this content!
+
+
+
+# Rating
+
+rating_rating				= Rating 
+rating_ratings				= Ratings
+rating_rate_this			= Rate This !
+rating_your_rating			= Your Rating
+
+rating_1_star				= Poor
+rating_2_star				= Fair
+rating_3_star				= Good
+rating_4_star				= Very Good
+rating_5_star				= Excellent
+
+
+
+# Review
+
+review_heading				= Reviews 
+review_write_a_review		= Write a Review
+review_title				= Review Title
+review_title_help			= e.g., Great Story !
+review_content				= Your Review
+review_content_help			= Share your thoughts ! What made this work interesting ? What made it inspiring ? What did you learn from it ? You are helping others make a better choice !
+review_submit_review		= SUBMIT
+review_edit_review			= Edit Review
+review_delete_review		= Delete Review
+review_delete_review_confirm	= Are you sure to delete your review?
+review_delete_okay			= Yes, Delete review!
+review_delete_cancel		= No, Don't!
+review_helpful				= Was this review helpful?
+review_see_all_reviews		= See All Reviews
+
+
+
+# Search
+
+search 						= Search
+search_bar_help				= Search for books, stories, poems and more ...
+search_results				= Search Results
+search_no_results_found		= Sorry! No Search Results found for your search query. Try searching for different search query!
+
+
+
+# Write
+
+write_heading				= Add New Content
+write_help					= You can write on Pratilipi!
+write_title					= Title *
+write_title_en				= Title (English)
+write_type					= Select Type *
+write_language				= Language
+write_summary				= Summary (Optional)
+write_create_new_content	= Create New Content
+
+
+
+# Common User Popups
+
+user_sign_in				= Sign In
+user_to_sign_in				= To Sign In
+user_sign_in_with_facebook	= Sign In with Facebook
+user_sign_in_to_pratilipi	= Sign in to Pratilipi
+user_sign_out				= Sign Out
+
+user_sign_up				= Sign Up !
+user_sign_up_for_pratilipi	= Sign Up for Pratilipi
+user_sign_up_with_facebook	= Sign Up with Facebook
+
+user_forgot_password		= Forgot Password ?
+user_reset_password			= Reset Password
+user_reset_password_help	=  Please enter the email address you signed up with. We will send you a link to reset your password.
+
+user_current_password		= Current Password
+user_new_password			= New Password
+
+user_full_name				= Full Name
+user_email					= Email
+user_password				= Password
+user_confirm_password		= Confirm Password
+
+user_my_profile				= My Profile
+user_my_account				= My Account
+
+user_is_new					= New?
+user_is_registered			= Already a Member?
+user_email_verification		= Email Verification
+
+
+
+# My Account popup
+
+edit_account_heading		= Edit My Account
+edit_account_verify_email	= Verify Email
+edit_account_email_verified	= Email Verified
+edit_account_change_password	= Change Password
+
+
+
+# Footer
+
+footer_follow_us_on_social_media	= Follow us on Social Media
+footer_about_us				= About Us
+footer_work_with_us			= Work With Us
+footer_blog					= Blog
+footer_privacy_policy		= Privacy Policy
+footer_terms_of_service		= Terms
+footer_contact_email		= contact@pratilipi.com
+footer_contact_10_digit_number		= 9789316700
+
+
+
+# Common Words
+
+okay						= Okay
+cancel						= Cancel
+save_changes				= Save Changes
+show_more					= show more
+show_less					= show less
+read						= Read
+write						= Write
+free						= FREE !
+saving						= Saving ...
+working						= Working ...
+update						= Update
+share						= Share
+menu						= Menu
+back						= Back
+load_more_contents			= Load More Contents
+view_more					= View More
+
+
+
+# Success Messages
+
+password_reset_request_success		= A mail has been sent to your email Id! 
+user_login_success					= Success !
+user_register_success				= Success !
+password_change_success				= Password has been changed Successfully!
+verification_email_sent_success		= Email Sent Successfully !
+user_verified_success				= Verified Successfully !
+content_created_success				= Success !
+updated_author_info_success			= Success !
+updated_pratilipi_info_success		= Success !
+updated_review						= Success !
+
+# Author Edit Popup
+
+edit_author_heading				= Edit Author Information	
+edit_author_first_name			= First Name( Vernacular )
+edit_author_last_name			= Last Name( Vernacular )
+edit_author_first_name_en		= First Name( English )
+edit_author_last_name_en		= Last Name( English )
+edit_author_pen_name			= Pen Name ( Vernacular )
+edit_author_pen_name_en			= Pen Name ( English )
+edit_author_birthday_ddmmyyyy	= Birthday (dd-mm-yyyy)
+edit_author_language			= Select Language
+edit_author_summary				= Summary ( Optional )
+edit_author_save_changes		= Save Changes
+
+# Pratilipi Edit Popup
+
+edit_pratilipi_heading			= Edit Content Information
+edit_pratilipi_title			= Title *
+edit_pratilipi_title_en			= Title (English)
+edit_pratilipi_type				= Select Type *
+edit_pratilipi_language			= Language
+edit_pratilipi_summary			= Summary (Optional) :
+edit_pratilipi_save_changes		= Save Changes
+
+
+
+# Register Form
+
+register_part_1					= By registering, you agree to the 
+register_part_2					= and
+register_part_3					= .
+
+
+# Event Page
+
+event_entries			= Event Entries
+event_name				= Event Name
+event_name_en			= Event Name in English
+event_description		= Event Description
+event_entries_url_list	= Entries URL List
+event_save_changes		= Save Changes
+
+
+# Gender
+gender_male			= Male
+gender_female		= Female
+gender_other		= Other
+
+# Months
+month_jan	= January
+month_feb	= February
+month_mar	= March
+month_apr	= April
+month_may	= May
+month_jun	= June
+month_jul	= July
+month_aug	= August
+month_sep	= September
+month_oct	= October
+month_nov	= November
+month_dec	= December
+
+# Library
+my_library			= My Library
+add_to_library		= Add to Library
+remove_from_library	= Remove from Library
+empty_library		= Sorry, there are no books in your library, you can add books from content page.
