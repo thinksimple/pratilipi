391418196aec1ef6898e6f596079bdc150246162
Prashant <prashant@pratilipi.com>
2016-04-12 16:09:09 +0530

    Implementing BlogPostApi.

---
+++ src/main/java/com/pratilipi/api/impl/blogpost/BlogPostApi.java
@@ -0,0 +1,34 @@
+package com.pratilipi.api.impl.blogpost;
+
+import com.pratilipi.api.GenericApi;
+import com.pratilipi.api.annotation.Bind;
+import com.pratilipi.api.annotation.Post;
+import com.pratilipi.api.impl.blogpost.shared.GenericBlogPostResponse;
+import com.pratilipi.api.impl.blogpost.shared.PostBlogPostRequest;
+import com.pratilipi.common.exception.InsufficientAccessException;
+import com.pratilipi.common.exception.InvalidArgumentException;
+import com.pratilipi.data.client.BlogPostData;
+import com.pratilipi.data.util.BlogPostDataUtil;
+
+@SuppressWarnings("serial")
+@Bind( uri = "/blogpost" )
+public class BlogPostApi extends GenericApi {
+
+	@Post
+	public GenericBlogPostResponse post( PostBlogPostRequest request )
+			throws InvalidArgumentException, InsufficientAccessException {
+		
+		BlogPostData blogPostData = new BlogPostData( request.getId() );
+		blogPostData.setBlogId( request.getBlogId() );
+		blogPostData.setTitle( request.getTitle() );
+		blogPostData.setTitleEn( request.getTitleEn() );
+		blogPostData.setContent( request.getContent() );
+		blogPostData.setState( request.getState() );
+		
+		blogPostData = BlogPostDataUtil.saveBlogPostData( blogPostData );
+		
+		return new GenericBlogPostResponse( blogPostData );
+		
+	}
+
+}
