ca67f599aed36a6842711d5c02ed1d1a2c822576
Prashant <prashant@pratilipi.com>
2016-04-13 18:48:12 +0530

    Minor bug fix in DataAccessor.

--- src/main/java/com/pratilipi/data/DataAccessorGaeImpl.java
+++ src/main/java/com/pratilipi/data/DataAccessorGaeImpl.java
@@ -813,10 +813,10 @@ public class DataAccessorGaeImpl implements DataAccessor {
 		com.googlecode.objectify.cmd.Query<BlogPostEntity> query
 				= ObjectifyService.ofy().load().type( BlogPostEntity.class );
 		
-		if( blogPostFilter.getBlogId() == null )
+		if( blogPostFilter.getBlogId() != null )
 			query.filter( "BLOG_ID", blogPostFilter.getBlogId() );
 		
-		if( blogPostFilter.getLanguage() == null )
+		if( blogPostFilter.getLanguage() != null )
 			query.filter( "LANGUAGE", blogPostFilter.getLanguage() );
 		
 		if( blogPostFilter.getState() != null )
