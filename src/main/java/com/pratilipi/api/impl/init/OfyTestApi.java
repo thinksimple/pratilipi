474cfd8a33dc2c0d0d492799c471947c639dba9b
Prashant <prashant@pratilipi.com>
2016-04-14 20:01:51 +0530

    Updating OfyTestApi.

--- src/main/java/com/pratilipi/api/impl/init/OfyTestApi.java
+++ src/main/java/com/pratilipi/api/impl/init/OfyTestApi.java
@@ -1,10 +1,10 @@
 package com.pratilipi.api.impl.init;
 
-import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.google.appengine.api.datastore.QueryResultIterator;
 import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.cmd.Query;
 import com.pratilipi.api.GenericApi;
 import com.pratilipi.api.annotation.Bind;
 import com.pratilipi.api.annotation.Get;
@@ -12,7 +12,6 @@ import com.pratilipi.api.impl.init.shared.GetInitApiRequest;
 import com.pratilipi.api.shared.GenericResponse;
 import com.pratilipi.common.exception.InsufficientAccessException;
 import com.pratilipi.common.exception.InvalidArgumentException;
-import com.pratilipi.data.type.Blog;
 import com.pratilipi.data.type.gae.BlogEntity;
 
 @SuppressWarnings("serial")
@@ -25,11 +24,10 @@ public class OfyTestApi extends GenericApi {
 	@Get
 	public GenericResponse get( GetInitApiRequest request ) throws InvalidArgumentException, InsufficientAccessException {
 
-		List<BlogEntity> blogList = ObjectifyService.ofy().load().type( BlogEntity.class ).list();
-
-		logger.log( Level.INFO, blogList.size() + " blog entities fetched" );
+		Query<BlogEntity> query = ObjectifyService.ofy().load().type( BlogEntity.class );
+		QueryResultIterator<BlogEntity> iterator = query.iterator();
 		
-		Blog blog = ObjectifyService.ofy().load().type( BlogEntity.class ).id( 5197509039226880L ).now();
+//		Blog blog = ObjectifyService.ofy().load().type( BlogEntity.class ).id( 5197509039226880L ).now();
 		
 		return new GenericResponse();
 	}
