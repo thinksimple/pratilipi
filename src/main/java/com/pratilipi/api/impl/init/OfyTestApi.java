b80aa33ab2bf9182dcfb15dee9035ba784d01622
Prashant <prashant@pratilipi.com>
2016-04-15 13:06:01 +0530

    Updating OfyTestApi.

--- src/main/java/com/pratilipi/api/impl/init/OfyTestApi.java
+++ src/main/java/com/pratilipi/api/impl/init/OfyTestApi.java
@@ -1,12 +1,8 @@
 package com.pratilipi.api.impl.init;
 
-import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.google.appengine.api.datastore.QueryResultIterator;
 import com.googlecode.objectify.ObjectifyService;
-import com.googlecode.objectify.cmd.Query;
 import com.pratilipi.api.GenericApi;
 import com.pratilipi.api.annotation.Bind;
 import com.pratilipi.api.annotation.Get;
@@ -14,7 +10,8 @@ import com.pratilipi.api.impl.init.shared.GetInitApiRequest;
 import com.pratilipi.api.shared.GenericResponse;
 import com.pratilipi.common.exception.InsufficientAccessException;
 import com.pratilipi.common.exception.InvalidArgumentException;
-import com.pratilipi.data.type.gae.BlogPostEntity;
+import com.pratilipi.data.type.Blog;
+import com.pratilipi.data.type.gae.BlogEntity;
 
 @SuppressWarnings("serial")
 @Bind( uri = "/ofy" )
@@ -26,17 +23,8 @@ public class OfyTestApi extends GenericApi {
 	@Get
 	public GenericResponse get( GetInitApiRequest request ) throws InvalidArgumentException, InsufficientAccessException {
 
-		Query<BlogPostEntity> query = ObjectifyService.ofy().load().type( BlogPostEntity.class ).limit( 20 );
-		QueryResultIterator<BlogPostEntity> iterator = query.iterator();
-		
-//		while( iterator.hasNext() )
-//			logger.log( Level.INFO, iterator.next().getId() + "" );
-
-		List<BlogPostEntity> list = query.list();
-		for( BlogPostEntity post : list )
-			logger.log( Level.INFO, post.getId() + "" );
-		
-		logger.log( Level.INFO, "Cursor: " + iterator.getCursor() );
+		ObjectifyService.ofy().load().type( BlogEntity.class ).id( 5197509039226880L );
+		Blog blog = ObjectifyService.ofy().load().type( BlogEntity.class ).id( 5683739602452480L ).now();
 		
 		return new GenericResponse();
 	}
