fbe45a1d28a7e6d2101b6a34d056f8d0c40802a3
Prashant Gupta <antshpra@gmail.com>
2016-04-15 15:40:29 +0530

    Update navigation.ml

--- src/main/java/com/pratilipi/data/curated/navigation.ml
+++ src/main/java/com/pratilipi/data/curated/navigation.ml
@@ -1,13 +1,6 @@
-Navigation
-/search?q=books     Books
-/search?q=stories   Stories
-/search?q=poems     Poems
-/search?q=articles  Articles
-/search?q=magazines Magazines
-
-Navigation 2
-/search?q=books     Books
-/search?q=stories   Stories
-/search?q=poems     Poems
-/search?q=articles  Articles
-/search?q=magazines Magazines
+Categories
+/books     Books
+/stories   Stories
+/poems     Poems
+/articles  Articles
+/sanjayan  Sanjayan
