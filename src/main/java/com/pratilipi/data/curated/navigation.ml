7992eef8fb48e96b1b1bf9d66912e1ac6b03f7b8
siddu23 <sankar@pratilipi.com>
2016-04-22 11:50:17 +0530

    Update navigation.ml

--- src/main/java/com/pratilipi/data/curated/navigation.ml
+++ src/main/java/com/pratilipi/data/curated/navigation.ml
@@ -1,6 +1,6 @@
 Categories
-/books     Books
-/stories   Stories
-/poems     Poems
-/articles  Articles
+/books     പുസ്തകങ്ങൾ 
+/stories   കഥകൾ 
+/poems     കവിതകൾ 
+/articles  ലേഖനങ്ങൾ 
 /classic   ക്ലാസ്സിക്‌ 
