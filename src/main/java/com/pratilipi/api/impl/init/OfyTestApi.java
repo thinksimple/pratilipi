b9c1f79cd8ea1c2aa0e9bf757ac36c71b1ae350a
Prashant <prashant@pratilipi.com>
2016-05-02 13:51:15 +0530

    Updates in OfyTestApi.

--- src/main/java/com/pratilipi/api/impl/init/OfyTestApi.java
+++ src/main/java/com/pratilipi/api/impl/init/OfyTestApi.java
@@ -3,6 +3,7 @@ package com.pratilipi.api.impl.init;
 import java.util.List;
 import java.util.logging.Logger;
 
+import com.google.gson.Gson;
 import com.pratilipi.api.GenericApi;
 import com.pratilipi.api.annotation.Bind;
 import com.pratilipi.api.annotation.Get;
@@ -10,9 +11,13 @@ import com.pratilipi.api.impl.init.shared.GetInitApiRequest;
 import com.pratilipi.api.shared.GenericResponse;
 import com.pratilipi.common.exception.InsufficientAccessException;
 import com.pratilipi.common.exception.InvalidArgumentException;
+import com.pratilipi.common.type.AccessType;
 import com.pratilipi.common.util.PratilipiFilter;
-import com.pratilipi.data.client.PratilipiData;
-import com.pratilipi.data.util.PratilipiDataUtil;
+import com.pratilipi.data.DataAccessor;
+import com.pratilipi.data.DataAccessorFactory;
+import com.pratilipi.data.type.AuditLog;
+import com.pratilipi.data.type.Pratilipi;
+import com.pratilipi.filter.AccessTokenFilter;
 
 @SuppressWarnings("serial")
 @Bind( uri = "/ofy" )
@@ -26,15 +31,27 @@ public class OfyTestApi extends GenericApi {
 
 		PratilipiFilter pratilipiFilter = new PratilipiFilter();
 		pratilipiFilter.setAuthorId( 4809728372768768L );
+
+		DataAccessor dataAccessor = DataAccessorFactory.getDataAccessor();
 		
-		List<PratilipiData> pratilipiDataList = PratilipiDataUtil.getPratilipiDataList( null, pratilipiFilter, null, null, null ).getDataList();
-		if( pratilipiDataList.size() == 4 ) {
-			for( PratilipiData pratilipiData : pratilipiDataList ) {
-				pratilipiData.setAuthorId( 5734588399747072L );
-				PratilipiDataUtil.savePratilipiData( pratilipiData );
-			}
+		List<Pratilipi> pratilipiList = dataAccessor.getPratilipiList( pratilipiFilter, null, null ).getDataList();
+		
+		
+		Gson gson = new Gson();
+		
+		for( Pratilipi pratilipi : pratilipiList ) {
+			AuditLog auditLog = dataAccessor.newAuditLogOfy();
+			auditLog.setAccessId( AccessTokenFilter.getAccessToken().getId() );
+			auditLog.setAccessType( AccessType.AUTHOR_UPDATE );
+			auditLog.setEventDataOld( gson.toJson( pratilipi ) );
+			
+			pratilipi.setAuthorId( 5734588399747072L );
+			
+			auditLog.setEventDataNew( gson.toJson( pratilipi ) );
+
+			dataAccessor.createOrUpdatePratilipi( pratilipi, auditLog );
 		}
-	
+		
 		return new GenericResponse();
 		
 	}
