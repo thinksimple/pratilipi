5e0922c99f50304e5d86f73dce5778d525fc49b9
Prashant <prashant@pratilipi.com>
2016-05-05 14:58:04 +0530

    Updates in OfyTestApi.

--- src/main/java/com/pratilipi/api/impl/init/OfyTestApi.java
+++ src/main/java/com/pratilipi/api/impl/init/OfyTestApi.java
@@ -1,14 +1,11 @@
 package com.pratilipi.api.impl.init;
 
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 import java.util.logging.Logger;
 
-import com.google.common.reflect.TypeToken;
-import com.google.gson.Gson;
 import com.pratilipi.api.GenericApi;
 import com.pratilipi.api.annotation.Bind;
 import com.pratilipi.api.annotation.Get;
@@ -17,10 +14,6 @@ import com.pratilipi.api.impl.init.shared.GetOfyResponse;
 import com.pratilipi.common.exception.InsufficientAccessException;
 import com.pratilipi.common.exception.InvalidArgumentException;
 import com.pratilipi.common.exception.UnexpectedServerException;
-import com.pratilipi.common.util.GoogleAnalyticsApi;
-import com.pratilipi.data.BlobAccessor;
-import com.pratilipi.data.DataAccessorFactory;
-import com.pratilipi.data.type.BlobEntry;
 
 @SuppressWarnings("serial")
 @Bind( uri = "/ofy" )
@@ -33,46 +26,11 @@ public class OfyTestApi extends GenericApi {
 	public GetOfyResponse get( GetOfyRequest request )
 			throws InvalidArgumentException, InsufficientAccessException, UnexpectedServerException {
 
-		String date = request.getDate();
-		Gson gson = new Gson();
+		DateFormat formatter = new SimpleDateFormat( "yyyy-dd-mm HH:mm:ss z" );    
+		formatter.setTimeZone( TimeZone.getTimeZone( "IST" ) );
+		String dateStr = formatter.format( new Date() );
 		
-		Map<String, Integer> oldPageViewsMap, newPageViewsMap, diffPageViewsMap;
-		
-		
-		String fileName = "google-analytics/page-views/" + date;
-		BlobAccessor blobAccessor = DataAccessorFactory.getBlobAccessor();
-		BlobEntry blobEntry = blobAccessor.getBlob( fileName );
-		if( blobEntry == null ) {
-			blobEntry = blobAccessor.newBlob( fileName );
-			blobEntry.setMimeType( "application/json" );
-			oldPageViewsMap = new HashMap<>();
-		} else {
-			oldPageViewsMap = gson.fromJson(
-					new String( blobEntry.getData(), Charset.forName( "UTF-8" ) ),
-					new TypeToken<Map<String, Integer>>(){}.getType() );
-		}
-		
-		newPageViewsMap = GoogleAnalyticsApi.getPageViews( request.getDate() );
-		
-		diffPageViewsMap = new HashMap<>();
-		for( Entry<String, Integer> entry : newPageViewsMap.entrySet() ) {
-			if( oldPageViewsMap.get( entry.getKey() ) == null ||
-					! oldPageViewsMap.get( entry.getKey() ).equals( entry.getValue() ) )
-				diffPageViewsMap.put( entry.getKey(), entry.getValue() );
-		}
-		
-		if( diffPageViewsMap.size() > 0 ) {
-			try {
-				blobEntry.setData( gson.toJson( newPageViewsMap ).getBytes( "UTF-8" ) );
-				blobAccessor.createOrUpdateBlob( blobEntry );
-			} catch( UnsupportedEncodingException e ) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				throw new UnexpectedServerException();
-			}
-		}
-		
-		return new GetOfyResponse( diffPageViewsMap.size() );
+		return new GetOfyResponse( dateStr );
 		
 	}
 	
