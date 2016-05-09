e572459ace1bbe804a28d58e7047c3db087f93e0
Prashant <prashant@pratilipi.com>
2016-05-09 12:21:15 +0530

    Updating InitApi.

--- src/main/java/com/pratilipi/api/impl/init/InitApi.java
+++ src/main/java/com/pratilipi/api/impl/init/InitApi.java
@@ -60,26 +60,48 @@ public class InitApi extends GenericApi {
 		logger.log( Level.INFO, "Added " + taskList.size() + " tasks in the queue." );
 */
 		
+		long[][] offset = {
+//				new long[] {5135361533542400L,2663L},
+				new long[] {5196411878506496L,183L},
+				new long[] {5741295515467776L,1483L},
+				new long[] {5709949130768384L,78L},
+//				new long[] {5155035268775936L,474L},
+				new long[] {5645455495004160L,6L},
+				new long[] {5725637813731328L,52L},
+//				new long[] {4946718139351040L,0L},
+//				new long[] {5822385531912192L,10L},
+//				new long[] {5669071953592320L,1609L},
+				new long[] {5693765442338816L,193L},
+//				new long[] {5693908432453632L,0L},
+//				new long[] {5670554354843648L,2119L},
+//				new long[] {5691188921237504L,3040L},
+//				new long[] {5096266023305216L,1009L},
+		};
+		
+		
 		DataAccessor dataAccessor = DataAccessorFactory.getDataAccessor();
 		
 		Gson gson = new Gson();
-		
-		Pratilipi pratilipi = dataAccessor.getPratilipi( 5761003044208640L );
-		
 
-		AccessToken accessToken = AccessTokenFilter.getAccessToken();
-		AuditLog auditLog = dataAccessor.newAuditLogOfy();
-		auditLog.setAccessId( accessToken.getId() );
-		auditLog.setAccessType( AccessType.PRATILIPI_UPDATE );
-		auditLog.setEventDataOld( gson.toJson( pratilipi ) );
+		for( long[] offsetCount : offset ) {
 		
-		pratilipi.setFbLikeShareCountOffset( 92L );
-		pratilipi.setFbLikeShareCount( 2L );
-		
-		auditLog.setEventDataNew( gson.toJson( pratilipi ) );
-
-		pratilipi = dataAccessor.createOrUpdatePratilipi( pratilipi, auditLog );
-		
+			Pratilipi pratilipi = dataAccessor.getPratilipi( offsetCount[0] );
+	
+			AccessToken accessToken = AccessTokenFilter.getAccessToken();
+			AuditLog auditLog = dataAccessor.newAuditLogOfy();
+			auditLog.setAccessId( accessToken.getId() );
+			auditLog.setAccessType( AccessType.PRATILIPI_UPDATE );
+			auditLog.setEventDataOld( gson.toJson( pratilipi ) );
+			
+	//		pratilipi.setFbLikeShareCountOffset( 92L );
+	//		pratilipi.setFbLikeShareCount( 2L );
+			pratilipi.setReadCountOffset( offsetCount[1] );
+			
+			auditLog.setEventDataNew( gson.toJson( pratilipi ) );
+	
+			pratilipi = dataAccessor.createOrUpdatePratilipi( pratilipi, auditLog );
+			
+		}
 		
 		return new GenericResponse();
 		
