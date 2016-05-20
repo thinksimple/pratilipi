a1d02ea88d39449972cbffd331f94631df2d9128
Raghu Raman <rraagghhu@gmail.com>
2016-05-20 18:46:11 +0530

    [Backend] Updating www mark-6 version - continuous deployment script.

--- shell-scripts/update-prod.sh
+++ shell-scripts/update-prod.sh
@@ -19,6 +19,7 @@ then
 
 	# Update prod-pratilipi/prod
 	mvn appengine:update -Dapp.id=prod-pratilipi -Dapp.module=default
+	mvn appengine:update -Dapp.id=prod-pratilipi -Dapp.module=www
 
 	
 	cp src/main/webapp/WEB-INF/prod-dispatch.xml		src/main/webapp/WEB-INF/dispatch.xml
