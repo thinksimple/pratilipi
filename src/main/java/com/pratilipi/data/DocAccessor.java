3161e21a4558ad563e0f6fc5f54e73dfe5cbd34c
Prashant <prashant@pratilipi.com>
2016-06-02 16:37:14 +0530

    Implementing DocAccessor.getPratilipiReviewsDoc

--- src/main/java/com/pratilipi/data/DocAccessor.java
+++ src/main/java/com/pratilipi/data/DocAccessor.java
@@ -1,14 +1,28 @@
 package com.pratilipi.data;
 
 import com.pratilipi.common.exception.UnexpectedServerException;
+import com.pratilipi.data.type.CommentDoc;
 import com.pratilipi.data.type.PratilipiGoogleAnalyticsDoc;
+import com.pratilipi.data.type.PratilipiReviewsDoc;
+import com.pratilipi.data.type.UserPratilipiDoc;
 
 public interface DocAccessor {
 
-	PratilipiGoogleAnalyticsDoc getPratilipiGoogleAnalyticsDoc( Long pratilipiId )
-			throws UnexpectedServerException;
+	// UserPratilipi Doc
+	UserPratilipiDoc newUserPratilipiDoc();
+	
+	// Comment Doc
+	CommentDoc newCommentDoc();
+	
 
-	void save( Long pratilipiId, PratilipiGoogleAnalyticsDoc gaDoc )
-			throws UnexpectedServerException;
+
+	// Pratilipi Reviews Doc
+	PratilipiReviewsDoc getPratilipiReviewsDoc( Long pratilipiId ) throws UnexpectedServerException;
+	void save( Long pratilipiId, PratilipiReviewsDoc reviewsDoc ) throws UnexpectedServerException;
+	
+
+	// Pratilipi GoogleAnalytics Doc
+	PratilipiGoogleAnalyticsDoc getPratilipiGoogleAnalyticsDoc( Long pratilipiId ) throws UnexpectedServerException;
+	void save( Long pratilipiId, PratilipiGoogleAnalyticsDoc gaDoc ) throws UnexpectedServerException;
 	
 }
--- src/main/java/com/pratilipi/data/DocAccessorImpl.java
+++ src/main/java/com/pratilipi/data/DocAccessorImpl.java
@@ -8,8 +8,14 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.pratilipi.common.exception.UnexpectedServerException;
 import com.pratilipi.data.type.BlobEntry;
+import com.pratilipi.data.type.CommentDoc;
 import com.pratilipi.data.type.PratilipiGoogleAnalyticsDoc;
+import com.pratilipi.data.type.PratilipiReviewsDoc;
+import com.pratilipi.data.type.UserPratilipiDoc;
+import com.pratilipi.data.type.doc.CommentDocImpl;
 import com.pratilipi.data.type.doc.PratilipiGoogleAnalyticsDocImpl;
+import com.pratilipi.data.type.doc.PratilipiReviewsDocImpl;
+import com.pratilipi.data.type.doc.UserPratilipiDocImpl;
 
 public class DocAccessorImpl implements DocAccessor {
 
@@ -23,57 +29,77 @@ public class DocAccessorImpl implements DocAccessor {
 	
 	private static final Logger logger =
 			Logger.getLogger( DocAccessorImpl.class.getName() );
+
+	
+	// UserPratilipi Doc
+	public UserPratilipiDoc newUserPratilipiDoc() {
+		return new UserPratilipiDocImpl();
+	}
 	
 	
-	public PratilipiGoogleAnalyticsDoc getPratilipiGoogleAnalyticsDoc( Long pratilipiId )
-			throws UnexpectedServerException {
+	// Comment Doc
+	public CommentDoc newCommentDoc() {
+		return new CommentDocImpl();
+	}
 		
-		if( pratilipiId == null )
-			return null;
-		
-		Gson gson = new Gson();
-		
-		String fileName = "pratilipi/" + pratilipiId + "/googleAnalytics";
-		BlobEntry blobEntry = blobAccessor.getBlob( fileName );
-		
-		if( blobEntry == null )
-			return new PratilipiGoogleAnalyticsDocImpl();
-		
+
+
+	// Pratilipi Reviews Doc
+	
+	@Override
+	public PratilipiReviewsDoc getPratilipiReviewsDoc( Long pratilipiId ) throws UnexpectedServerException {
+		if( pratilipiId != null )
+			return _get( "pratilipi/" + pratilipiId + "/reviews", PratilipiReviewsDocImpl.class );
+		return null;
+	}
+	
+	@Override
+	public void save( Long pratilipiId, PratilipiReviewsDoc reviewsDoc ) throws UnexpectedServerException {
+		if( pratilipiId != null )
+			_save( "pratilipi/" + pratilipiId + "/reviews", reviewsDoc );
+	}
+	
+	
+	// Pratilipi GoogleAnalytics Doc
+	
+	@Override
+	public PratilipiGoogleAnalyticsDoc getPratilipiGoogleAnalyticsDoc( Long pratilipiId ) throws UnexpectedServerException {
+		if( pratilipiId != null )
+			return _get( "pratilipi/" + pratilipiId + "/googleAnalytics", PratilipiGoogleAnalyticsDocImpl.class );
+		return null;
+	}
+	
+	@Override
+	public void save( Long pratilipiId, PratilipiGoogleAnalyticsDoc gaDoc ) throws UnexpectedServerException {
+		if( pratilipiId != null )
+			_save( "pratilipi/" + pratilipiId + "/googleAnalytics", gaDoc );
+	}
+	
+	
+	private <T> T _get( String docPath, Class<T> clazz ) throws UnexpectedServerException {
+		BlobEntry blobEntry = blobAccessor.getBlob( docPath );
 		try {
-			return gson.fromJson(
-					new String( blobEntry.getData(), "UTF-8" ),
-					PratilipiGoogleAnalyticsDocImpl.class );
-		} catch( JsonSyntaxException | UnsupportedEncodingException e) {
+			if( blobEntry == null )
+				return clazz.newInstance();
+			else
+				return new Gson().fromJson(
+						new String( blobEntry.getData(), "UTF-8" ),
+						clazz );
+		} catch( InstantiationException | IllegalAccessException | JsonSyntaxException | UnsupportedEncodingException e) {
 			logger.log( Level.SEVERE, e.getMessage() );
 			throw new UnexpectedServerException();
 		}
-		
 	}
-
-	public void save( Long pratilipiId, PratilipiGoogleAnalyticsDoc gaDoc )
-			throws UnexpectedServerException {
-		
-		if( pratilipiId == null )
-			return;
-		
-		Gson gson = new Gson();
-		
-		String fileName = "pratilipi/" + pratilipiId + "/googleAnalytics";
-		BlobEntry blobEntry = blobAccessor.getBlob( fileName );
-		
+	
+	private <T> void _save( String docPath, T doc ) throws UnexpectedServerException {
 		try {
-			byte[] blobData = gson.toJson( gaDoc ).getBytes( "UTF-8" );
-			if( blobEntry == null )
-				blobEntry = blobAccessor.newBlob( fileName, blobData, "application/json" );
-			else
-				blobEntry.setData( blobData );
+			byte[] blobData = new Gson().toJson( doc ).getBytes( "UTF-8" );
+			BlobEntry blobEntry = blobAccessor.newBlob( docPath, blobData, "application/json" );
+			blobAccessor.createOrUpdateBlob( blobEntry );
 		} catch( UnsupportedEncodingException e ) {
 			logger.log( Level.SEVERE, e.getMessage() );
 			throw new UnexpectedServerException();
 		}
-		
-		blobAccessor.createOrUpdateBlob( blobEntry );
-		
 	}
 	
 }
