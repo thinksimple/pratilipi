41a1bf15a84c32d8b25d9b1158aadc8d963a040e
Prashant <prashant@pratilipi.com>
2016-06-06 16:42:02 +0530

    Adding PratilipiStatsApi in WorkerService.

--- src/main/java/com/pratilipi/service/WorkerService.java
+++ src/main/java/com/pratilipi/service/WorkerService.java
@@ -8,6 +8,7 @@ import com.pratilipi.api.impl.init.InitApi;
 import com.pratilipi.api.impl.pratilipi.PratilipiBackupApi;
 import com.pratilipi.api.impl.pratilipi.PratilipiIdfApi;
 import com.pratilipi.api.impl.pratilipi.PratilipiProcessApi;
+import com.pratilipi.api.impl.pratilipi.PratilipiStatsApi;
 import com.pratilipi.api.impl.user.UserBackupApi;
 import com.pratilipi.api.impl.user.UserEmailApi;
 import com.pratilipi.api.impl.user.UserFacebookValidationApi;
@@ -26,6 +27,7 @@ public class WorkerService extends GenericService {
 		ApiRegistry.register( UserBackupApi.class );
 		
 		ApiRegistry.register( PratilipiProcessApi.class );
+		ApiRegistry.register( PratilipiStatsApi.class );
 		ApiRegistry.register( PratilipiBackupApi.class );
 		ApiRegistry.register( PratilipiIdfApi.class );
 
