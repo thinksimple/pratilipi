3ad5f655ab048d109e1b88c00892ddc056b7916d
Prashant <prashant@pratilipi.com>
2016-06-10 15:14:47 +0530

    Minor bug fix in GenericApi.

--- src/main/java/com/pratilipi/api/GenericApi.java
+++ src/main/java/com/pratilipi/api/GenericApi.java
@@ -93,7 +93,7 @@ public abstract class GenericApi extends HttpServlet {
 			if( value.startsWith( "{" ) || value.startsWith( "[" ) ) // Value could be a valid JSON string.
 				queryParamsStr.append( "\"" + param + "\":" + value + "," );
 			else
-				queryParamsStr.append( "\"" + param + "\":\"" + value.replaceAll( "\"", "\\\"" ) + "\"," );
+				queryParamsStr.append( "\"" + param + "\":\"" + value.replaceAll( "\"", "\\\\\"" ) + "\"," );
 		}
 		if( queryParamsStr.length() > 1 )
 			queryParamsStr.setCharAt( queryParamsStr.length() - 1, '}' );
