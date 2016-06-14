998a49be4968502d8732df0558f250b11faf23a1
Prashant <prashant@pratilipi.com>
2016-06-14 21:01:59 +0530

    Enabling InitApi in worker module.

--- src/main/java/com/pratilipi/service/WorkerService.java
+++ src/main/java/com/pratilipi/service/WorkerService.java
@@ -19,6 +19,8 @@ public class WorkerService extends GenericService {
 	
 	static {
 
+		ApiRegistry.register( InitApi.class );
+		
 		ApiRegistry.register( UserProcessApi.class );
 		ApiRegistry.register( UserEmailApi.class );
 		ApiRegistry.register( UserFacebookValidationApi.class );
