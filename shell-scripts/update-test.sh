15dcfa837e370657151c1d61fcedf11571113ba8
Prashant <prashant@pratilipi.com>
2016-06-14 21:06:32 +0530

    Cleaning up update-test.sh

--- shell-scripts/update-test.sh
+++ shell-scripts/update-test.sh
@@ -19,17 +19,4 @@ then
 	
 	# Update prod-pratilipi/gamma
 	mvn appengine:update -Dapp.id=prod-pratilipi -Dapp.module=gamma
-	
-	
-	# Update devo-pratilipi/default
-	mvn appengine:update -Dapp.id=devo-pratilipi -Dapp.module=default
-	
-	cp src/main/webapp/WEB-INF/worker-web.xml			src/main/webapp/WEB-INF/web.xml
-	cp src/main/webapp/WEB-INF/worker-appengine-web.xml	src/main/webapp/WEB-INF/appengine-web.xml
-	cp src/main/webapp/WEB-INF/worker-queue.xml			src/main/webapp/WEB-INF/queue.xml
-	
-	# Update devo-pratilipi/worker
-	mvn appengine:update -Dapp.id=devo-pratilipi -Dapp.module=worker
-	
-	rm src/main/webapp/WEB-INF/queue.xml
 fi
