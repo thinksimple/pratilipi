27f1612ce36001a1c381f61eea85dd7e138ecd38
Prashant <prashant@pratilipi.com>
2016-06-15 18:29:01 +0530

    Removing deprecated state from PratilipiState.

--- src/main/java/com/pratilipi/common/type/PratilipiState.java
+++ src/main/java/com/pratilipi/common/type/PratilipiState.java
@@ -5,8 +5,6 @@ public enum PratilipiState {
 	DRAFTED,		// Work in progress
 	SUBMITTED,		// Final work waiting for approval from Moderator
 	PUBLISHED,		// Published work (free or paid)
-	@Deprecated
-	PUBLISHED_PAID,
 	DISCONTINUED,	// Work discontinued by Author
 	BLOCKED,		// Work blocked by System when Author's profile is BLOCKED
 	DELETED,		// Work deleted by User OR by System when Author's profile is DELETED
