c5b45b23fbb86a0e8ea470852247c002a6d075a6
Rahul Ranjan <genirahul@users.noreply.github.com>
2016-06-25 12:58:17 +0530

    category.ml

---
+++ src/main/java/com/pratilipi/data/curated/category.ml
@@ -0,0 +1,7 @@
+പുതിയവ         { "language":"KANNADA", "listName":"new", "state":"PUBLISHED" }
+പുസ്തകങ്ങൾ        { "language":"KANNADA", "listName":"books", "state":"PUBLISHED" }
+കഥകൾ        { "language":"KANNADA", "listName":"stories", "state":"PUBLISHED" }
+കവിതകൾ       { "language":"KANNADA", "listName":"poems", "state":"PUBLISHED" }
+ലേഖനങ്ങൾ       { "language":"KANNADA", "listName":"articles", "state":"PUBLISHED" }
+ക്ലാസ്സിക്സ്       { "language":"KANNADA", "listName":"classics", "state":"PUBLISHED" }
+സ്ത്രീ      { "language":"KANNADA", "listName":"women", "state":"PUBLISHED" }
