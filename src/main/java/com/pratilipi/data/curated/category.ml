d0138ef152763ecfb0fc67b2fa4df62cd09b367f
Rahul Ranjan <genirahul@users.noreply.github.com>
2016-06-25 13:01:40 +0530

    changing language from kn to ml in category.ml

--- src/main/java/com/pratilipi/data/curated/category.ml
+++ src/main/java/com/pratilipi/data/curated/category.ml
@@ -1,7 +1,7 @@
-പുതിയവ         { "language":"KANNADA", "listName":"new", "state":"PUBLISHED" }
-പുസ്തകങ്ങൾ        { "language":"KANNADA", "listName":"books", "state":"PUBLISHED" }
-കഥകൾ        { "language":"KANNADA", "listName":"stories", "state":"PUBLISHED" }
-കവിതകൾ       { "language":"KANNADA", "listName":"poems", "state":"PUBLISHED" }
-ലേഖനങ്ങൾ       { "language":"KANNADA", "listName":"articles", "state":"PUBLISHED" }
-ക്ലാസ്സിക്സ്       { "language":"KANNADA", "listName":"classics", "state":"PUBLISHED" }
-സ്ത്രീ      { "language":"KANNADA", "listName":"women", "state":"PUBLISHED" }
+പുതിയവ         { "language":"MALAYALAM", "listName":"new", "state":"PUBLISHED" }
+പുസ്തകങ്ങൾ        { "language":"MALAYALAM", "listName":"books", "state":"PUBLISHED" }
+കഥകൾ        { "language":"MALAYALAM", "listName":"stories", "state":"PUBLISHED" }
+കവിതകൾ       { "language":"MALAYALAM", "listName":"poems", "state":"PUBLISHED" }
+ലേഖനങ്ങൾ       { "language":"MALAYALAM", "listName":"articles", "state":"PUBLISHED" }
+ക്ലാസ്സിക്സ്       { "language":"MALAYALAM", "listName":"classics", "state":"PUBLISHED" }
+സ്ത്രീ      { "language":"MALAYALAM", "listName":"women", "state":"PUBLISHED" }
