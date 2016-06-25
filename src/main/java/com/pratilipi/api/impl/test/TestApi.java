7f59a9438f1eabf0bcba39ce06d1e6fb30e231e3
Prashant <prashant@pratilipi.com>
2016-06-25 12:00:59 +0530

    Updating TestApi.

--- src/main/java/com/pratilipi/api/impl/test/TestApi.java
+++ src/main/java/com/pratilipi/api/impl/test/TestApi.java
@@ -1,16 +1,17 @@
 package com.pratilipi.api.impl.test;
 
-import java.util.Date;
-
 import com.pratilipi.api.GenericApi;
 import com.pratilipi.api.annotation.Bind;
 import com.pratilipi.api.annotation.Get;
 import com.pratilipi.api.shared.GenericRequest;
 import com.pratilipi.api.shared.GenericResponse;
 import com.pratilipi.common.exception.InsufficientAccessException;
+import com.pratilipi.common.type.PageType;
 import com.pratilipi.data.DataAccessor;
 import com.pratilipi.data.DataAccessorFactory;
-import com.pratilipi.data.type.AppProperty;
+import com.pratilipi.data.type.Page;
+import com.pratilipi.data.type.Pratilipi;
+import com.pratilipi.data.util.PratilipiDataUtil;
 
 @SuppressWarnings("serial")
 @Bind( uri = "/test" )
@@ -21,7 +22,7 @@ public class TestApi extends GenericApi {
 		
 		DataAccessor dataAccessor = DataAccessorFactory.getDataAccessor();
 		
-		String appPropertyId = "Api.PratilipiProcess.ValidateData";
+/*		String appPropertyId = "Api.PratilipiProcess.ValidateData";
 		AppProperty appProperty = dataAccessor.getAppProperty( appPropertyId );
 		appProperty.setValue( new Date( 0 ) );
 		appProperty = dataAccessor.createOrUpdateAppProperty( appProperty );
@@ -34,7 +35,16 @@ public class TestApi extends GenericApi {
 		appPropertyId = "Api.UserProcess.ValidateData";
 		appProperty = dataAccessor.getAppProperty( appPropertyId );
 		appProperty.setValue( new Date( 0 ) );
-		appProperty = dataAccessor.createOrUpdateAppProperty( appProperty );
+		appProperty = dataAccessor.createOrUpdateAppProperty( appProperty ); */
+		
+		
+		Pratilipi pratilipi = dataAccessor.getPratilipi( 6214944447004672L );
+		Page page = dataAccessor.getPage( PageType.PRATILIPI, 6214944447004672L );
+		if( page == null ) {
+			page = PratilipiDataUtil._updatePratilipiPageUrl( pratilipi );
+			page = dataAccessor.createOrUpdatePage( page );
+		}
+		
 		
 		return new GenericResponse();
 		
