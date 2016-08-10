0989d9f21ee8c666c6d9a6616704257b97ea6ce9
vaisakhrg <vaisakh@pratilipi.com>
2016-08-10 14:10:07 +0530

    Update language.ml

--- src/main/java/com/pratilipi/i18n/language.ml
+++ src/main/java/com/pratilipi/i18n/language.ml
@@ -149,8 +149,8 @@ write_language				= ഭാഷ
 write_summary				= സംഗ്രഹം (നിര്‍ബന്ധമില്ല)
 write_create_new_content	= പുതിയ രചന സൃഷ്ടിക്കൂ
 write_on_desktop_only		= രചനകള്‍ പ്രസിദ്ധീകരിക്കാന്‍ താങ്കള്‍ കമ്പ്യൂട്ടര്‍ ഉപയോഗിച്ച് ലോഗിന്‍ ചെയ്യുക, അല്ലെങ്കില്‍ ഞങ്ങളെ ബന്ധപ്പെടുക malayalam@pratilipi.com/ 9036506463
-write_fill_title			= Please fill in the Title.
-write_select_type			= Please select the type.
+write_fill_title			= തലവാചകം എഴുതൂ 
+write_select_type			= തരം തിരഞ്ഞെടുക്കൂ
 
 # Common User Popups
 user_sign_in				= സൈനിന്‍
