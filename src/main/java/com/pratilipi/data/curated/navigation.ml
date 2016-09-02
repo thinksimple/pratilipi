6fb2b25907414e9bff0934e4b566724ee5f8eb26
RAGHU RAMAN <rraagghhu@gmail.com>
2016-09-02 16:46:45 +0530

    Updating Navigation files.

--- src/main/java/com/pratilipi/data/curated/navigation.ml
+++ src/main/java/com/pratilipi/data/curated/navigation.ml
@@ -1,27 +1,27 @@
 പ്രസിദ്ധമായവ 
-/most-read കൂടുതൽ  വായിക്കപ്പെട്ടവ 
-/recommended  നിര്‍ദ്ദേശിക്കപ്പെട്ടവ
-/blog     ബ്ലോഗുകള്‍ 
-/event/fr  ചങ്ങാതി
-/event/ep  എഴുത്തുപെട്ടി
-/new      പുതിയവ 
+/most-read     കൂടുതൽ  വായിക്കപ്പെട്ടവ     App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"most-read" }
+/recommended   നിര്‍ദ്ദേശിക്കപ്പെട്ടവ           App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"recommended" }
+/blog          ബ്ലോഗുകള്‍               App#BlogPostList::{ "language":"MALAYALAM", "blogId":5683739602452480 } 
+/event/fr      ചങ്ങാതി                 App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "eventId":6046056954462208 }
+/event/ep      എഴുത്തുപെട്ടി             App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "eventId":6207939288236032 }
+/new           പുതിയവ                 App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"new" } 
 
 
 
 വിഭാഗങ്ങൾ 
-/mystery ദുരൂഹം
-/memory ഓര്‍മ്മ
-/lives ജീവിതങ്ങള്‍
-/classics  ക്ലാസ്സിക്സ്
-/love പ്രണയം
-/women     സ്ത്രീ
+/mystery       ദുരൂഹം                  App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"mystery" }
+/memory        ഓര്‍മ്മ                   App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"memory" }
+/lives         ജീവിതങ്ങള്‍               App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"lives" }
+/classics      ക്ലാസ്സിക്സ്                 App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"classics" }
+/love          പ്രണയം                 App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"love" }
+/women         സ്ത്രീ                   App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"women" }
 
 
 സാഹിത്യ രൂപങ്ങൾ 
-/stories   കഥകൾ 
-/articles  ലേഖനങ്ങൾ 
-/poems     കവിതകൾ 
-/books     പുസ്തകങ്ങൾ 
+/stories       കഥകൾ                App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"stories" } 
+/articles      ലേഖനങ്ങൾ             App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"articles" }
+/poems         കവിതകൾ              App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"poems" }
+/books         പുസ്തകങ്ങൾ            App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"books" }
 
 
 
