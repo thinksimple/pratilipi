6c453e54a4ab1c7366c1f7a7f63b6af7b4838ea3
Prashant <prashant@pratilipi.com>
2016-10-03 17:37:24 +0530

    Updating GenericService.

--- src/main/java/com/pratilipi/api/GenericService.java
+++ src/main/java/com/pratilipi/api/GenericService.java
@@ -1,6 +1,7 @@
 package com.pratilipi.api;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 
 @SuppressWarnings("serial")
 public abstract class GenericService extends HttpServlet {
@@ -33,12 +35,24 @@ public abstract class GenericService extends HttpServlet {
 		if( requestUri.equals( "/" ) || requestUri.isEmpty() ) {
 			
 			String[] reqUris = request.getParameter( "req" ).split( ";" );
+			final PrintWriter pw = new PrintWriter( response.getWriter() ) {
+				public void close() {
+					// Do Nothing
+				}
+			};
+			HttpServletResponseWrapper resp = new HttpServletResponseWrapper( response ) {
+				@Override
+				public PrintWriter getWriter() throws IOException {
+					return pw;
+				}
+			};
 			for( final String reqUri : reqUris ) {
 				if( reqUri.trim().isEmpty() )
 					continue;
 				logger.log( Level.WARNING, reqUri.trim() );
-				request.getRequestDispatcher( reqUri.trim() ).forward( request, response );
+				request.getRequestDispatcher( reqUri.trim() ).forward( request, resp );
 			}
+			response.getWriter().close();
 			
 		} else {
 		
