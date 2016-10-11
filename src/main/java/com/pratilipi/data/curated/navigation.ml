10a60f4f8627b597d3cf02538cbeeb0013ffb872
vaisakhrg <vaisakh@pratilipi.com>
2016-10-11 13:21:20 +0530

    Update navigation.ml

--- src/main/java/com/pratilipi/data/curated/navigation.ml
+++ src/main/java/com/pratilipi/data/curated/navigation.ml
@@ -1,7 +1,6 @@
 പ്രസിദ്ധമായവ 
 /event/katha   കഥാപൂരണം				App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "eventId":4839232641171456 }
 /most-read     കൂടുതൽ  വായിക്കപ്പെട്ടവ     App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"most-read" }
-/recommended   നിര്‍ദ്ദേശിക്കപ്പെട്ടവ           App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"recommended" }
 /new           പുതിയവ                 App#PratilipiList::{ "language":"MALAYതുടര്‍ക്കഥകള്‍ALAM", "state":"PUBLISHED", "listName":"new" } 
 /blog          ബ്ലോഗുകള്‍               App#BlogPostList::{ "language":"MALAYALAM", "blogId":5683739602452480 } 
 /events        മത്സരങ്ങള്‍              App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"events" }
