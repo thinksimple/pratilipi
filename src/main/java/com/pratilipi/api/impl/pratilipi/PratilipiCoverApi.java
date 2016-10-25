d646b9ce8184bf3a5446040f987c762806820013
Prashant <prashant@pratilipi.com>
2016-10-25 17:18:02 +0530

    Updating PratilipiCoverApi.

--- src/main/java/com/pratilipi/api/impl/pratilipi/PratilipiCoverApi.java
+++ src/main/java/com/pratilipi/api/impl/pratilipi/PratilipiCoverApi.java
@@ -66,16 +66,10 @@ public class PratilipiCoverApi extends GenericApi {
 	public PostResponse post( PostRequest request )
 			throws InvalidArgumentException, InsufficientAccessException, UnexpectedServerException {
 
-		// This is to cover issue with jQuery FileUpload plug-in.
-		// When user hits "Ctrl+v", the plug-in uploads the data in clipboard as
-		// an image with file name "undefined".
-		// This can be removed as soon as Mark-4 is deprecated.
-		if( request.getName() != null && request.getName().equals( "undefined" ) )
-			throw new InvalidArgumentException( "File name 'undefined' is not allowed." );
-		
 		BlobEntry blobEntry = DataAccessorFactory.getBlobAccessor().newBlob( request.getName() );
 		blobEntry.setData( request.getData() );
 		blobEntry.setMimeType( request.getMimeType() );
+		blobEntry.setMetaName( request.getName() );
 		
 		String coverImageUrl = PratilipiDataUtil.savePratilipiCover(
 				request.pratilipiId,
