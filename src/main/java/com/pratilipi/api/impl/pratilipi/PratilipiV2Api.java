5252a28820b7d231bcded9eac905367f9f175186
Prashant <prashant@pratilipi.com>
2016-11-03 16:23:25 +0530

    Bug fix in Pratilipi V2 Api.

--- src/main/java/com/pratilipi/api/impl/pratilipi/PratilipiV2Api.java
+++ src/main/java/com/pratilipi/api/impl/pratilipi/PratilipiV2Api.java
@@ -1,10 +1,7 @@
 package com.pratilipi.api.impl.pratilipi;
 
-import com.pratilipi.api.GenericApi;
 import com.pratilipi.api.annotation.Bind;
 import com.pratilipi.api.annotation.Get;
-import com.pratilipi.api.impl.pratilipi.PratilipiV1Api.GetRequest;
-import com.pratilipi.api.impl.pratilipi.PratilipiV1Api.Response;
 import com.pratilipi.common.exception.UnexpectedServerException;
 import com.pratilipi.data.DataAccessor;
 import com.pratilipi.data.DataAccessorFactory;
@@ -14,7 +11,7 @@ import com.pratilipi.data.util.PratilipiDataUtil;
 
 @SuppressWarnings("serial")
 @Bind( uri = "/pratilipi", ver = "2" )
-public class PratilipiV2Api extends GenericApi {
+public class PratilipiV2Api extends PratilipiV1Api {
 	
 	@Get
 	public Response get( GetRequest request ) throws UnexpectedServerException {
