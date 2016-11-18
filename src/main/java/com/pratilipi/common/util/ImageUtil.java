3d86f005bf62b7f0ff6de42546da6d477fb38911
Prashant <prashant@pratilipi.com>
2016-11-18 11:30:41 +0530

    ImageUtil: Reverting changes done over last month.

--- src/main/java/com/pratilipi/common/util/ImageUtil.java
+++ src/main/java/com/pratilipi/common/util/ImageUtil.java
@@ -3,10 +3,9 @@ package com.pratilipi.common.util;
 import com.google.appengine.api.images.Image;
 import com.google.appengine.api.images.ImagesService;
 import com.google.appengine.api.images.ImagesService.OutputEncoding;
-import com.google.appengine.api.images.ImagesServiceFactory;
-import com.google.appengine.api.images.OutputSettings;
-import com.google.appengine.api.images.Transform;
 import com.pratilipi.data.type.BlobEntry;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.Transform;
 
 public class ImageUtil {
 
@@ -41,14 +40,17 @@ public class ImageUtil {
 	}
 	
 	public static BlobEntry resize( BlobEntry blobEntry, int width, int height ) {
+		Image image = ImagesServiceFactory.makeImage( blobEntry.getData() );
 		Transform resize = ImagesServiceFactory.makeResize(
 				width < 4000 ? width : 4000,
 				height < 4000 ? height : 4000,
 				true );
-		Image image = ImagesServiceFactory.makeImage( blobEntry.getData() );
-		OutputSettings settings = new OutputSettings( ImagesService.OutputEncoding.WEBP );
-		image = imagesService.applyTransform( resize, image, settings );
-		blobEntry.setMimeType( "image/webp" );
+		if( blobEntry.getMimeType().equalsIgnoreCase( "image/png" ) ) {
+			image = imagesService.applyTransform( resize, image, OutputEncoding.PNG );
+		} else {
+			image = imagesService.applyTransform( resize, image, OutputEncoding.JPEG );
+			blobEntry.setMimeType( "image/jpeg" );
+		}
 		blobEntry.setData( image.getImageData() );
 		return blobEntry;
 	}
