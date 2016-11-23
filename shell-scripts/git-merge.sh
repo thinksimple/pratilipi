1a5f787c18c46abe363cb264931424328904b0f4
RAGHU RAMAN <rraagghhu@gmail.com>
2016-11-23 12:13:56 +0530

    [Backend] Updating git-merge.sh

--- shell-scripts/git-merge.sh
+++ shell-scripts/git-merge.sh
@@ -1,36 +1,36 @@
 promptyn () {
-    while true; do
-        read -p "$1 " yn
-        case $yn in
-            [Yy]* ) return 0;;
-            [Nn]* ) return 1;;
-        * ) echo "Please enter Y or N ...";;
-        esac
-    done
+	while true; do
+		read -p "$1 " yn
+		case $yn in
+			[Yy]* ) return 0;;
+			[Nn]* ) return 1;;
+		* ) echo "Please enter Y or N ...";;
+		esac
+	done
 }
 
 if promptyn "Push to prod? [Y/n]"; then
-    prod=true
+	prod=true
 else
-    prod=false
+	prod=false
 fi
 
 if promptyn "Push to api? [Y/n]"; then
-    api=true
+	api=true
 else
-    api=false
+	api=false
 fi
 
 if promptyn "Push to worker? [Y/n]"; then
-    worker=true
+	worker=true
 else
-    worker=false
+	worker=false
 fi
 
 if promptyn "Push to android? [Y/n]"; then
-    android=true
+	android=true
 else
-    android=false
+	android=false
 fi
 
 echo "Initialising ...."
@@ -43,36 +43,33 @@ git fetch
 
 branch=$(date +%d-%b-%H-%M-Release)
 
-echo "Deleting existing branch ...."
-git branch -d $branch
-git push origin -d $branch
-
 echo "Creating New branch -> $branch ...."
 git checkout -b $branch origin/master
 git push origin $branch
 
 
 update_module () {
-    echo "Syncing $1 from $2 ...."
-    git tag -d $1
-    git branch -d $1
-    git push origin -d $1
-    git checkout -b $1 $2
-    git push origin $1
+	echo "Syncing $1 from $2 ...."
+	git tag --delete $1
+	git push --delete origin refs/tags/$1
+	git branch -D $1
+	git push origin -D $1
+	git checkout -b $1 $2
+	git push origin $1
 }
 
 if $prod; then
-    update_module release-prod $branch
+	update_module release-prod $branch
 fi
 
 if $api; then
-    update_module release-api $branch
+	update_module release-api $branch
 fi
 
 if $worker; then
-    update_module release-worker $branch
+	update_module release-worker $branch
 fi
 
 if $android; then
-    update_module release-android $branch
+	update_module release-android $branch
 fi
