3083c5b6af3079c2d635fc61ace94e3989e54edd
Prashant <prashant@pratilipi.com>
2016-11-30 16:35:57 +0530

    Optimizing AccessTokenCleanup api.

--- src/main/java/com/pratilipi/api/impl/accesstoken/AccessTokenCleanupApi.java
+++ src/main/java/com/pratilipi/api/impl/accesstoken/AccessTokenCleanupApi.java
@@ -1,15 +1,20 @@
 package com.pratilipi.api.impl.accesstoken;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.google.appengine.api.datastore.QueryResultIterator;
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.ObjectifyService;
 import com.pratilipi.api.GenericApi;
 import com.pratilipi.api.annotation.Bind;
 import com.pratilipi.api.annotation.Get;
 import com.pratilipi.api.shared.GenericRequest;
 import com.pratilipi.api.shared.GenericResponse;
-import com.pratilipi.data.DataAccessor;
-import com.pratilipi.data.DataAccessorFactory;
+import com.pratilipi.data.type.gae.AccessTokenEntity;
 
 @SuppressWarnings("serial")
 @Bind( uri = "/accesstoken/cleanup" )
@@ -22,13 +27,26 @@ public class AccessTokenCleanupApi extends GenericApi {
 	@Get
 	public GenericResponse get( GenericRequest request ) {
 
-		DataAccessor dataAccessor = DataAccessorFactory.getDataAccessor();
-		
+		int batchSize = 1000;
+
+		QueryResultIterator<Key<AccessTokenEntity>> itr = ObjectifyService.ofy().load()
+				.type( AccessTokenEntity.class )
+				.filter( "EXPIRY <", new Date() )
+				.chunk( batchSize )
+				.keys()
+				.iterator();
+
+		List<Key<AccessTokenEntity>> list = new ArrayList<>( batchSize );
 		while( true ) {
-			int count = dataAccessor.deleteExpiredAccessTokenList( 1000 );
-			logger.log( Level.INFO, "Deleted " + count + " AccessToken entities ..." );
-			if( count < 1000 )
-				break;
+			if( ! itr.hasNext() || list.size() == batchSize ) {
+				logger.log( Level.INFO, "Deleting " + list.size() + " AccessToken entities ..." );
+				ObjectifyService.ofy().delete().keys( list );
+				if( ! itr.hasNext() )
+					break;
+				list.clear();
+			} else {
+				list.add( itr.next() );
+			}
 		}
 		
 		return new GenericResponse();
