43018ed42f05cc843e79351a031b6ad2ddaad674
RAGHU RAMAN <rraagghhu@gmail.com>
2016-12-02 16:33:42 +0530

    [Backend] Cleaning up files.

--- src/main/java/com/pratilipi/data/util/AuthorDataUtil.java
+++ src/main/java/com/pratilipi/data/util/AuthorDataUtil.java
@@ -1,17 +1,13 @@
 package com.pratilipi.data.util;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TimeZone;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.pratilipi.api.shared.GenericRequest;
 import com.pratilipi.common.exception.InsufficientAccessException;
@@ -19,7 +15,6 @@ import com.pratilipi.common.exception.InvalidArgumentException;
 import com.pratilipi.common.exception.UnexpectedServerException;
 import com.pratilipi.common.type.AccessType;
 import com.pratilipi.common.type.AuthorState;
-import com.pratilipi.common.type.EmailType;
 import com.pratilipi.common.type.Language;
 import com.pratilipi.common.type.PageType;
 import com.pratilipi.common.type.PratilipiState;
@@ -36,7 +31,6 @@ import com.pratilipi.data.DataAccessorFactory;
 import com.pratilipi.data.DataListCursorTuple;
 import com.pratilipi.data.SearchAccessor;
 import com.pratilipi.data.client.AuthorData;
-import com.pratilipi.data.client.PratilipiData;
 import com.pratilipi.data.client.UserData;
 import com.pratilipi.data.type.AccessToken;
 import com.pratilipi.data.type.AuditLog;
@@ -45,7 +39,6 @@ import com.pratilipi.data.type.BlobEntry;
 import com.pratilipi.data.type.Page;
 import com.pratilipi.data.type.Pratilipi;
 import com.pratilipi.data.type.User;
-import com.pratilipi.email.EmailUtil;
 import com.pratilipi.filter.AccessTokenFilter;
 
 public class AuthorDataUtil {
@@ -729,42 +722,6 @@ public class AuthorDataUtil {
 			searchAccessor.deleteAuthorDataIndex( authorId );
 		}
 		
-	}
-	
-	public static void sendContentPublishedMail( Long pratilipiId, Long userId, EmailType emailType ) 
-			throws UnexpectedServerException {
-
-		DataAccessor dataAccessor = DataAccessorFactory.getDataAccessor();
-		Pratilipi pratilipi = dataAccessor.getPratilipi( pratilipiId );
-		Author author = dataAccessor.getAuthor( pratilipi.getAuthorId() );
-		PratilipiData pratilipiData = PratilipiDataUtil.createPratilipiData( pratilipi, author );
-
-		User user = dataAccessor.getUser( userId );
-
-		if( user.getEmail() == null )
-			return;
-
-		DateFormat dateFormat = new SimpleDateFormat( "dd MMM yyyy" );
-		dateFormat.setTimeZone( TimeZone.getTimeZone( "IST" ) );
-
-		Map<String, String> dataModel = new HashMap<String, String>();
-		dataModel.put( "pratilipi_title", pratilipiData.getTitle() != null ? 
-					pratilipiData.getTitle() : pratilipiData.getTitleEn() );
-		dataModel.put( "pratilipi_cover_image_url", PratilipiDataUtil.createPratilipiCoverUrl( pratilipi, 150 ) );
-		dataModel.put( "pratilipi_listing_date", dateFormat.format( pratilipiData.getListingDate() ) );
-		dataModel.put( "pratilipi_summary", HtmlUtil.truncateText( pratilipiData.getSummary(), 250 ) );
-		dataModel.put( "pratilipi_page_url", "http://" + pratilipiData.getLanguage().getHostName() + pratilipiData.getPageUrl() );
-		dataModel.put( "author_name", pratilipiData.getAuthor().getName() != null ? 
-					pratilipiData.getAuthor().getName() : pratilipiData.getAuthor().getNameEn() );
-		dataModel.put( "author_page_url", "http://" + author.getLanguage().getHostName() + pratilipiData.getAuthor().getPageUrl() );
-
-
-		EmailUtil.sendMail( user.getEmail(),
-							UserDataUtil.createUserData( user ).getDisplayName(),
-							emailType,
-							pratilipi.getLanguage(), 
-							dataModel );
-
 	}
 
 }
