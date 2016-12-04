97bfa2987d87f2d6e7788fb3ea70a3b884ff9851
Prashant <prashant@pratilipi.com>
2016-12-04 21:01:25 +0530

    Updating blobstore shell scripts

---
+++ shell-scripts/blobstore-cleanup.sh
@@ -0,0 +1,1 @@
+gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/2015-11-17
--- shell-scripts/startup-blobstore.sh
+++ shell-scripts/startup-blobstore.sh
@@ -3,12 +3,15 @@ do
 
 	logger -t ping "_BLOBSTORE_SCRIPT_RUNNING_"
 	
-	cd ~
-	sudo bash ~/test/pratilipi/shell-scripts/blobstore-backup.sh 2>&1 | grep -v "^Copying "            | logger -t blobstore_script
+	# cd ~
+	# sudo bash ~/test/pratilipi/shell-scripts/blobstore-backup.sh 2>&1 | grep -v "^Copying "            | logger -t blobstore_script
+	
+	# cd ~
+	# sudo bash ~/test/pratilipi/shell-scripts/blobstore-meta.sh   2>&1 | grep -v "^Setting metadata on" | logger -t blobstore_script
 	
 	cd ~
-	sudo bash ~/test/pratilipi/shell-scripts/blobstore-meta.sh   2>&1 | grep -v "^Setting metadata on" | logger -t blobstore_script
+	sudo bash ~/test/pratilipi/shell-scripts/blobstore-cleanup.sh   2>&1 | logger -t blobstore_script
 	
-	sleep 30
+	sleep 60
 
 done
