8a53bb9dbfccc67d1bb0b161affff9b686a06f29
RAGHU RAMAN <rraagghhu@gmail.com>
2016-12-07 16:19:19 +0530

    [Backend] Updating EmailType.

--- src/main/java/com/pratilipi/common/type/EmailType.java
+++ src/main/java/com/pratilipi/common/type/EmailType.java
@@ -5,13 +5,20 @@ public enum EmailType {
 	PRATILIPI_PUBLISHED_AUTHOR		( "PratilipiPublishedAuthorTemplate.ftl" ),		// Email sent to Author whenever s/he publishes a content.
 	PRATILIPI_PUBLISHED_FOLLOWER	( "PratilipiPublishedFollowerTemplate.ftl" ),	// Email sent to Author's followers whenever s/he publishes a content.
 
-	AUTHOR_FOLLOW					( "AuthorFollowTemplate.ftl" ),					// Email sent to Author whenever someone follows him/her.
-	
-	USER_PRATILIPI_REVIEW			( "UserPratilipiReviewTemplate.ftl" ),			// Email sent to Author when someone reviews his/her content.
-	
-	COMMENT_REVIEW_REVIEWER		( "CommentReviewReviewerTemplate.ftl" ),			// Email sent to Reviewer when someone comments on his/her review.
-	COMMENT_REVIEW_AUTHOR		( "CommentReviewAuthorTemplate.ftl" ),			// Email sent to Author when someone comments on any review.
-	
+	AUTHOR_FOLLOW					( "AuthorFollowTemplate.ftl" ),			// Email sent to Author whenever someone follows him/her.
+
+	USER_PRATILIPI_REVIEW			( "UserPratilipiReviewTemplate.ftl" ),	// Email sent to Author when someone reviews his/her content.
+
+	COMMENT_REVIEW_REVIEWER		( "CommentReviewReviewerTemplate.ftl" ),	// Email sent to Reviewer when someone comments on his/her review.
+	COMMENT_REVIEW_AUTHOR		( "CommentReviewAuthorTemplate.ftl" ),		// Email sent to Author when someone comments on any review.
+
+	VOTE_REVIEW_REVIEWER 		( "VoteReviewReviewerTemplate.ftl" ),		// Email sent to the reviewer when someone likes his/her comment.
+	VOTE_REVIEW_AUTHOR 			( "VoteReviewAuthorTemplate.ftl" ),			// Email sent to the author when someone likes on review of his/her content.
+
+	VOTE_COMMENT_COMMENTOR 		( "VoteCommentCommentorTemplate.ftl" ),		// Email sent to the commentor when someone likes his/her comment.
+	VOTE_COMMENT_REVIEWER 		( "VoteCommentReviewerTemplate.ftl" ),		// Email sent to the reviewer when someone likes any of the comments under the review. 
+	VOTE_COMMENT_AUTHOR 		( "VoteCommentAuthorTemplate.ftl" ),		// Email sent to the author when someone likes any of the comments.
+
 	;
 
 	
