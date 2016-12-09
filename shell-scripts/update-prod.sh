211f96ed605e92e05a723037b57f78148a83512a
Prashant <prashant@pratilipi.com>
2016-12-10 00:56:52 +0530

    Updating maven version in update-prod.sh

--- shell-scripts/update-prod.sh
+++ shell-scripts/update-prod.sh
@@ -1,4 +1,4 @@
-export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.3
+export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.9
 export M2=$M2_HOME/bin
 export MAVEN_OPTS="-Xms256m -Xmx512m"
 export PATH=$M2:$PATH
