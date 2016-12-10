e25bab6651665e4e99e9d69fae9855da517ef534
Prashant <prashant@pratilipi.com>
2016-12-10 16:21:28 +0530

    Minor update in update-test.sh

--- shell-scripts/update-test.sh
+++ shell-scripts/update-test.sh
@@ -19,7 +19,7 @@ then
 	cp src/main/webapp/WEB-INF/gamma-appengine-web.xml src/main/webapp/WEB-INF/appengine-web.xml
 	
 	# Update prod-pratilipi/gamma
-	mvn gcloud:deploy -Dapp.id=prod-pratilipi -Dapp.module=gamma
+	mvn gcloud:deploy -Dgcloud.gcloud_project=prod-pratilipi -Dgcloud.service=gamma -Dgcloud.version=mark-6
 	
 	
 	# Cleaning up .git directory
