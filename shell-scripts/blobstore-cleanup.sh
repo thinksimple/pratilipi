76c954696023ca2fa11446bd5c0a1877945f8182
Prashant <prashant@pratilipi.com>
2016-12-12 12:21:05 +0530

    Updating blobstore cleanup script.

--- shell-scripts/blobstore-cleanup.sh
+++ shell-scripts/blobstore-cleanup.sh
@@ -1,9 +1,12 @@
-gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-05
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-06
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-07
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-08
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-09
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-10
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-11
+gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-12
+gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-13
+gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-14
+gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-15
 
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-$(date +%d)
