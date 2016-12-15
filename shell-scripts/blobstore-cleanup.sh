5e961da4b412eeff5cb6dff12fb1937f7a276f5e
Prashant <prashant@pratilipi.com>
2016-12-15 21:36:02 +0530

    Updating blobstore-cleanup.sh

--- shell-scripts/blobstore-cleanup.sh
+++ shell-scripts/blobstore-cleanup.sh
@@ -1,6 +1,5 @@
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-06
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-07
-gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-08
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-09
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-10
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-11
@@ -8,5 +7,10 @@ gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-12
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-13
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-14
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-15
+gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-16
+gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-17
+gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-18
+gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-19
+gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-20
 
 gsutil -m rm -r gs://backup.pratilipi.com/static.pratilipi.com/yyyy-mm-$(date +%d)
