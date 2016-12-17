a9870f466b2fccc122d5da194feda18d6c68134d
Prashant <prashant@pratilipi.com>
2016-12-17 14:47:06 +0530

    Bug fix in continuous deployment script.

--- shell-scripts/continuous-deployment.sh
+++ shell-scripts/continuous-deployment.sh
@@ -9,61 +9,61 @@ do
 
 	if [ -d "/root/scripts" ]; then
 		cd /root/scripts/pratilipi
-		bash $scripts/continuous-deployment-git-merge.sh $1               2>&1 | logger -t git-merge
+		bash $scripts/continuous-deployment-git-merge.sh $1                     2>&1 | logger -t git-merge
 	else
 		mkdir -p /root/scripts
 		cd /root/scripts
-		git clone -b master https://github.com/Pratilipi/pratilipi.git    2>&1 | logger -t git-merge
+		git clone -b master https://github.com/Pratilipi/pratilipi.git          2>&1 | logger -t git-merge
 	fi
 
 
 	if [ -d "/root/prod" ]; then
 		cd /root/prod/pratilipi
-		bash $scripts/continuous-deployment-update-prod.sh                2>&1 | logger -t update-prod
+		bash $scripts/continuous-deployment-update-prod.sh                      2>&1 | logger -t update-prod
 	else
 		mkdir -p /root/prod
 		cd /root/prod
-		git clone -b master https://github.com/Pratilipi/pratilipi.git    2>&1 | logger -t update-prod
+		git clone -b release-prod https://github.com/Pratilipi/pratilipi.git    2>&1 | logger -t update-prod
 	fi
 
 	
 	if [ -d "/root/api" ]; then
 		cd /root/api/pratilipi
-		bash $scripts/continuous-deployment-update-api.sh                 2>&1 | logger -t update-api
+		bash $scripts/continuous-deployment-update-api.sh                       2>&1 | logger -t update-api
 	else
 		mkdir -p /root/api
 		cd /root/api
-		git clone -b master https://github.com/Pratilipi/pratilipi.git    2>&1 | logger -t update-api
+		git clone -b release-api https://github.com/Pratilipi/pratilipi.git     2>&1 | logger -t update-api
 	fi
 
 
 	if [ -d "/root/android" ]; then
 		cd /root/android/pratilipi
-		bash $scripts/continuous-deployment-update-android.sh             2>&1 | logger -t update-android
+		bash $scripts/continuous-deployment-update-android.sh                   2>&1 | logger -t update-android
 	else
 		mkdir -p /root/android
 		cd /root/android
-		git clone -b master https://github.com/Pratilipi/pratilipi.git    2>&1 | logger -t update-android
+		git clone -b release-android https://github.com/Pratilipi/pratilipi.git 2>&1 | logger -t update-android
 	fi
 
 
 	if [ -d "/root/worker" ]; then
 		cd /root/worker/pratilipi
-		bash $scripts/continuous-deployment-update-worker.sh              2>&1 | logger -t update-worker
+		bash $scripts/continuous-deployment-update-worker.sh                    2>&1 | logger -t update-worker
 	else
 		mkdir -p /root/worker
 		cd /root/worker
-		git clone -b master https://github.com/Pratilipi/pratilipi.git    2>&1 | logger -t update-worker
+		git clone -b release-worker https://github.com/Pratilipi/pratilipi.git  2>&1 | logger -t update-worker
 	fi
 
 
 	if [ -d "/root/gamma" ]; then
 		cd /root/gamma/pratilipi
-		bash $scripts/continuous-deployment-update-gamma.sh               2>&1 | logger -t update-gamma
+		bash $scripts/continuous-deployment-update-gamma.sh                     2>&1 | logger -t update-gamma
 	else
 		mkdir -p /root/gamma
 		cd /root/gamma
-		git clone -b master https://github.com/Pratilipi/pratilipi.git    2>&1 | logger -t update-gamma
+		git clone -b master https://github.com/Pratilipi/pratilipi.git          2>&1 | logger -t update-gamma
 	fi
 
 	
