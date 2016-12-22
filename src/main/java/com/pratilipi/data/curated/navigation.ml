16d97229f957999aa10e3df1353d50dc4a0f80a5
siddu23 <sankar@pratilipi.com>
2016-12-22 18:33:18 +0530

    Update navigation.ml

--- src/main/java/com/pratilipi/data/curated/navigation.ml
+++ src/main/java/com/pratilipi/data/curated/navigation.ml
@@ -1,26 +1,26 @@
 പ്രസിദ്ധമായവ 
-/event/tellastory  ഒരു കഥ പറയാം		  App#imageName::event_children.jpg        App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "eventId":4557681508483072 }
-/most-read     നിര്‍ദ്ദേശിക്കപ്പെട്ടവ        App#imageName::mostpopular.jpg       App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"most-read" }
-/fiveminute    5 മിനിറ്റ് കഥകള്‍         App#imageName::5minread.jpg        App#PratilipiList::{ "language":"MALAYതുടര്‍ക്കഥകള്‍ALAM", "state":"PUBLISHED", "listName":"fiveminute" } 
-/new           പുതിയവ                App#imageName::newpublished.jpg       App#PratilipiList::{ "language":"MALAYതുടര്‍ക്കഥകള്‍ALAM", "state":"PUBLISHED", "listName":"new" } 
-/blog          ബ്ലോഗുകള്‍              App#imageName::blog.jpg              App#BlogPostList::{ "language":"MALAYALAM", "blogId":5683739602452480 } 
-/events        മത്സരങ്ങള്‍              App#imageName::festival.jpg           App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"events" }
-/two-minute    കുഞ്ഞിക്കഥകള്‍        App#imageName::events.jpg           App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"two-minute " }
-/classics      ക്ലാസ്സിക്സ്                 App#imageName::classic.jpg        App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"classics" }
+/event/tellastory  ഒരു കഥ പറയാം		  Analytics#categoryName::event-tell-a-story        App#imageName::event_children.jpg        App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "eventId":4557681508483072 }
+/most-read     നിര്‍ദ്ദേശിക്കപ്പെട്ടവ        Analytics#categoryName::most-read        App#imageName::mostpopular.jpg       App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"most-read" }
+/fiveminute    5 മിനിറ്റ് കഥകള്‍         Analytics#categoryName::short-stories        App#imageName::5minread.jpg        App#PratilipiList::{ "language":"MALAYതുടര്‍ക്കഥകള്‍ALAM", "state":"PUBLISHED", "listName":"fiveminute" } 
+/new           പുതിയവ                Analytics#categoryName::newly-published        App#imageName::newpublished.jpg       App#PratilipiList::{ "language":"MALAYതുടര്‍ക്കഥകള്‍ALAM", "state":"PUBLISHED", "listName":"new" } 
+/blog          ബ്ലോഗുകള്‍              Analytics#categoryName::blogs        App#imageName::blog.jpg              App#BlogPostList::{ "language":"MALAYALAM", "blogId":5683739602452480 } 
+/events        മത്സരങ്ങള്‍              Analytics#categoryName::online-events        App#imageName::festival.jpg           App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"events" }
+/two-minute    കുഞ്ഞിക്കഥകള്‍        Analytics#categoryName::two-min        App#imageName::events.jpg           App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"two-minute " }
+/classics      ക്ലാസ്സിക്സ്                 Analytics#categoryName::classics        App#imageName::classic.jpg        App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"classics" }
 
 വിഭാഗങ്ങൾ 
-/series        തുടര്‍ക്കഥകള്‍           App#imageName::series.jpg         App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"series" }
-/lives         ജീവിതങ്ങള്‍              App#imageName::life.jpg           App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"lives" }
-/satire        ആക്ഷേപഹാസ്യം           App#imageName::satire.jpg          App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"satire" }  
-/memory        ഓര്‍മ്മ                   App#imageName::experiences.jpg     App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"memory" }
-/mystery       ദുരൂഹം                   App#imageName::suspence.jpg         App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"mystery" }
-/women         സ്ത്രീ                      App#imageName::women.jpg             App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"women" }
-/love          പ്രണയം                   App#imageName::romance.jpg          App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"love" }
-/society       സമൂഹം                   App#imageName::social.jpg          App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"society" }
-/non-fiction   നോണ്‍ ഫിക്ഷന്‍           App#imageName::shortstories.jpg     App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"non-fiction" }
-/letters       കത്തുകള്‍                App#imageName::letters.jpg            App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"letters" }
-/stories       കഥകൾ           App#imageName::event_generic3.jpg      App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"stories" } 
-/poems         കവിതകൾ         App#imageName::poems.jpg             App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"poems" }
+/love          പ്രണയം                  Analytics#categoryName::romance         App#imageName::romance.jpg          App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"love" }
+/series        തുടര്‍ക്കഥകള്‍           Analytics#categoryName::series        App#imageName::series.jpg         App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"series" }
+/lives         ജീവിതങ്ങള്‍              Analytics#categoryName::life        App#imageName::life.jpg           App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"lives" }
+/satire        ആക്ഷേപഹാസ്യം        Analytics#categoryName::satire        App#imageName::satire.jpg          App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"satire" }  
+/memory        ഓര്‍മ്മ                  Analytics#categoryName::memory          App#imageName::experiences.jpg     App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"memory" }
+/mystery       ദുരൂഹം                 Analytics#categoryName::mystery   App#imageName::suspence.jpg         App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"mystery" }
+/women         സ്ത്രീ                   Analytics#categoryName::women       App#imageName::women.jpg             App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"women" }
+/society       സമൂഹം                Analytics#categoryName::social      App#imageName::social.jpg          App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"society" }
+/non-fiction   നോണ്‍ ഫിക്ഷന്‍      Analytics#categoryName::articles     App#imageName::shortstories.jpg     App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"non-fiction" }
+/letters       കത്തുകള്‍             Analytics#categoryName::letters         App#imageName::letters.jpg            App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"letters" }
+/stories       കഥകൾ              Analytics#categoryName::other-stories         App#imageName::event_generic3.jpg      App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"stories" } 
+/poems         കവിതകൾ         Analytics#categoryName::poetry            App#imageName::poems.jpg             App#PratilipiList::{ "language":"MALAYALAM", "state":"PUBLISHED", "listName":"poems" }
 
 
 
