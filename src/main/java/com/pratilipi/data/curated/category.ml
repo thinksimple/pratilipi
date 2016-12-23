1d69288183bd435b29031da1202bef5b8f3fef34
Prashant <prashant@pratilipi.com>
2016-12-23 15:19:40 +0530

    Removing category.xx files.

--- src/main/java/com/pratilipi/data/curated/category.ml
+++
@@ -1,7 +0,0 @@
-പുതിയവ         { "language":"MALAYALAM", "listName":"new", "state":"PUBLISHED" }
-പുസ്തകങ്ങൾ        { "language":"MALAYALAM", "listName":"books", "state":"PUBLISHED" }
-കഥകൾ        { "language":"MALAYALAM", "listName":"stories", "state":"PUBLISHED" }
-കവിതകൾ       { "language":"MALAYALAM", "listName":"poems", "state":"PUBLISHED" }
-ലേഖനങ്ങൾ       { "language":"MALAYALAM", "listName":"articles", "state":"PUBLISHED" }
-ക്ലാസ്സിക്സ്       { "language":"MALAYALAM", "listName":"classics", "state":"PUBLISHED" }
-സ്ത്രീ      { "language":"MALAYALAM", "listName":"women", "state":"PUBLISHED" }
