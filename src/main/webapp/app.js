a3ceec37899d39b7a975a79c66512fa8151ccb2f
rdhk <rdhk94@gmail.com>
2016-12-26 19:55:37 +0530

    FRONTEND: Changes in PWA POC.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -9,6 +9,7 @@ var pratilipiSummaryController = function() {
 
 pratilipiSummaryController.prototype.init = function() {
   this.getData();
+  this.registerServiceWorker();
   // var dummy = {"pratilipiId":5187823242051584,"title":"देवांश","titleEn":"devansh","language":"HINDI","author":{"authorId":5714053011865600,"name":"वीणा वत्सल सिंह","pageUrl":"/veena-vatsal-singh"},"pageUrl":"/veena-vatsal-singh/devansh","coverImageUrl":"http://4.ptlp.co/pratilipi/cover?pratilipiId\u003d5187823242051584\u0026version\u003d1462536818515","readPageUrl":"/read?id\u003d5187823242051584","writePageUrl":"/write?id\u003d5187823242051584","oldContent":true,"type":"STORY","state":"PUBLISHED","listingDateMillis":1439032842890,"reviewCount":13,"ratingCount":16,"averageRating":4.4375,"readCount":17052,"fbLikeShareCount":1729,"hasAccessToUpdate":false};
   // this.pushToViewModel( dummy );
 };
@@ -38,4 +39,12 @@ pratilipiSummaryController.prototype.pushToViewModel = function( data ) {
     self.ViewModel[ key ] = ko.observable( value );
   });
   ko.applyBindings( self.ViewModel );
+};
+
+pratilipiSummaryController.prototype.registerServiceWorker = function() {
+  if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+             .register('service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }    
 };
