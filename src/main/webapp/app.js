77625da3fcb120d616b2dd329da566339bb52303
rdhk <rdhk94@gmail.com>
2016-12-29 12:31:54 +0530

    FRONTEND: Changes in pwa poc.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -20,7 +20,7 @@ var pratilipiSummaryController = function() {
 
 pratilipiSummaryController.prototype.init = function() {
   this.getData();
-  this.registerServiceWorker();
+  //this.registerServiceWorker();
   // var dummy = {"pratilipiId":5187823242051584,"title":"देवांश","titleEn":"devansh","language":"HINDI","author":{"authorId":5714053011865600,"name":"वीणा वत्सल सिंह","pageUrl":"/veena-vatsal-singh"},"pageUrl":"/veena-vatsal-singh/devansh","coverImageUrl":"http://4.ptlp.co/pratilipi/cover?pratilipiId\u003d5187823242051584\u0026version\u003d1462536818515","readPageUrl":"/read?id\u003d5187823242051584","writePageUrl":"/write?id\u003d5187823242051584","oldContent":true,"type":"STORY","state":"PUBLISHED","listingDateMillis":1439032842890,"reviewCount":13,"ratingCount":16,"averageRating":4.4375,"readCount":17052,"fbLikeShareCount":1729,"hasAccessToUpdate":false};
   // this.pushToViewModel( dummy );
 };
