79f175885f81b2c631fc78a8fc9ee7d46299b0b5
RAGHU RAMAN <rraagghhu@gmail.com>
2017-01-03 11:36:44 +0530

    [Backend] Updating RequestCookie.

--- src/main/java/com/pratilipi/common/type/RequestCookie.java
+++ src/main/java/com/pratilipi/common/type/RequestCookie.java
@@ -5,7 +5,10 @@ public enum RequestCookie {
 	ACCESS_TOKEN ( "access_token" ),
 	FONT_SIZE ( "fontSize" ),
 	IMAGE_SIZE ( "imageSize" ),
-	PAGE_NO ( "pageNo" )
+	PAGE_NO ( "pageNo" ),
+	USER_NOTIFIED_APP_LAUNCHED ( "USER_NOTIFIED_APP_LAUNCHED" ),
+	APP_LAUNCHED_CLICK_COUNT ( "APP_LAUNCHED_CLICKED" ),
+	APP_LAUNCHED_CROSS_COUNT ( "APP_LAUNCHED_CROSSED" )
 	;
 	
 	
