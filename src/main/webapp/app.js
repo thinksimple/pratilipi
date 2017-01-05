a71a6a6751f45b46502715d3c0a92c1122ab79e3
rdhk <rdhk94@gmail.com>
2017-01-05 18:26:55 +0530

    FRONTEND: Changes in pwa poc.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -72,7 +72,7 @@ pratilipiSummaryController.prototype.pushToViewModel = function( data ) {
 pratilipiSummaryController.prototype.registerServiceWorker = function() {
   if ('serviceWorker' in navigator) {
       navigator.serviceWorker
-             .register('service-worker.js?4')
+             .register('service-worker.js?5')
              .then(function() { console.log('Service Worker Registered'); });
   }    
 }; 
--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -1,8 +1,8 @@
 var dataCacheName = 'data-v1';
 var cacheName = 'appshell-v1';
 var filesToCache = [
-                    '/poc',
-                    '/app.js?6',
+                    '/poc?action=sixteen',
+                    '/app.js?7',
                     'https://code.jquery.com/jquery-3.1.1.min.js',
                     'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.1/knockout-min.js',
                     'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
