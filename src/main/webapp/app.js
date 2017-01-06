49ac7f93f4320ed4a4991ad46d84f03b0661989c
rdhk <rdhk94@gmail.com>
2017-01-06 15:02:17 +0530

    FRONTEND: Minor bug fix in knockout poc.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -72,7 +72,7 @@ pratilipiSummaryController.prototype.pushToViewModel = function( data ) {
 pratilipiSummaryController.prototype.registerServiceWorker = function() {
   if ('serviceWorker' in navigator) {
       navigator.serviceWorker
-             .register('service-worker.js?8')
+             .register('service-worker.js?9')
              .then(function() { console.log('Service Worker Registered'); });
   }    
 }; 
