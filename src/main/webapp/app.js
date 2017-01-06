f4c914c086aa1507e19411058a4f1a231de882e7
rdhk <rdhk94@gmail.com>
2017-01-06 14:46:11 +0530

    FRONTEND: Changes for knockout poc.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -72,7 +72,7 @@ pratilipiSummaryController.prototype.pushToViewModel = function( data ) {
 pratilipiSummaryController.prototype.registerServiceWorker = function() {
   if ('serviceWorker' in navigator) {
       navigator.serviceWorker
-             .register('service-worker.js?6')
+             .register('service-worker.js?7')
              .then(function() { console.log('Service Worker Registered'); });
   }    
 }; 
