ca14dd4d17c10763e9f8c56769ebe903d128b8d5
Prashant <prashant@pratilipi.com>
2017-01-19 16:06:44 +0530

    Updating EmailState type.

--- src/main/java/com/pratilipi/common/type/EmailState.java
+++ src/main/java/com/pratilipi/common/type/EmailState.java
@@ -3,9 +3,11 @@ package com.pratilipi.common.type;
 public enum EmailState {
 
 	PENDING,
+	@Deprecated
 	IN_PROGRESS,
-	DEFERRED,
+	DEFERRED,	// Email not valid anymore (at the time of sending)
+	@Deprecated
 	INVALID_EMAIL,
-	SENT
+	SENT,		// Email successfully sent
 
 }
