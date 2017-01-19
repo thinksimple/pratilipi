b8c36a3a956d9cb69d45ea1fe346cd8795f0057f
rdhk <rdhk94@gmail.com>
2017-01-19 17:33:40 +0530

    FRONTEND: Adding components for header and footer in summary page.

---
+++ src/main/webapp/summary-components.ftl
@@ -0,0 +1,13 @@
+<#macro add_backslashes>
+<#local captured><#nested></#local>
+${ "'" + captured?replace("\\n", "", 'r') + "'" }
+</#macro>
+
+
+ko.components.register('pratilipi-header', {
+    template: <@add_backslashes><#include "pratilipi-header.html"></@add_backslashes>
+});
+
+ko.components.register('pratilipi-footer', {
+    template: <@add_backslashes><#include "pratilipi-footer.html"></@add_backslashes>
+});
--- src/main/webapp/summary.js
+++ src/main/webapp/summary.js
@@ -21,17 +21,20 @@ var pratilipiSummaryController = function() {
   ko.applyBindings( this.ViewModel );
 };
 
+pratilipiSummaryController.prototype.initializeHeaderComponent = function() {
+
+};
+
 pratilipiSummaryController.prototype.init = function() {
+  this.initializeHeaderComponent();
   this.getData();
-  // var dummy = {"pratilipiId":5187823242051584,"title":"देवांश","titleEn":"devansh","language":"HINDI","author":{"authorId":5714053011865600,"name":"वीणा वत्सल सिंह","pageUrl":"/veena-vatsal-singh"},"pageUrl":"/veena-vatsal-singh/devansh","coverImageUrl":"http://4.ptlp.co/pratilipi/cover?pratilipiId\u003d5187823242051584\u0026version\u003d1462536818515","readPageUrl":"/read?id\u003d5187823242051584","writePageUrl":"/write?id\u003d5187823242051584","oldContent":true,"type":"STORY","state":"PUBLISHED","listingDateMillis":1439032842890,"reviewCount":13,"ratingCount":16,"averageRating":4.4375,"readCount":17052,"fbLikeShareCount":1729,"hasAccessToUpdate":false};
-  // this.pushToViewModel( dummy );
 };
 
 pratilipiSummaryController.prototype.getData = function() {
   var self = this;
   $.ajax({
       type: 'get',
-      url: '/api/pratilipi?_apiVer=2&pratilipiId=5713444722442240',
+      url: '/api/pratilipi?_apiVer=2&pratilipiId=1',
       data: { 
           // 'language': "${ language }"
       },
