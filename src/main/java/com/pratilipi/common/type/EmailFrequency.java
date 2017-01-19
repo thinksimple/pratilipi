d392e4ca01c69d4044c5d4969a900e645aef3212
Prashant <prashant@pratilipi.com>
2017-01-19 15:49:52 +0530

    Enhancements in EmailFrequency type.

--- src/main/java/com/pratilipi/common/type/EmailFrequency.java
+++ src/main/java/com/pratilipi/common/type/EmailFrequency.java
@@ -1,11 +1,35 @@
 package com.pratilipi.common.type;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public enum EmailFrequency {
 
-	IMMEDIATELY,
-	DAILY,
-	WEEKLY,
-	MONTHLY,
-	NEVER,
+	IMMEDIATELY	( 0 ),
+	DAILY		( TimeUnit.DAYS.toMillis( 1 ) ),
+	WEEKLY		( TimeUnit.DAYS.toMillis( 7 ) ),
+	MONTHLY		( TimeUnit.DAYS.toMillis( 30 ) ),
+	NEVER		( -1 ),
+	;
+	
+	
+	private long delayMillis;
+	
+	private EmailFrequency( long delayMillis ) {
+		this.delayMillis = delayMillis;
+	}
 
+	
+	public Date getNextSchedule( Date lastMailDate ) {
+		
+		if( this == NEVER )
+			return null;
+
+		while( lastMailDate.before( new Date() ) )
+			lastMailDate = new Date( lastMailDate.getTime() + delayMillis );
+		
+		return lastMailDate;
+	
+	}
+	
 }
