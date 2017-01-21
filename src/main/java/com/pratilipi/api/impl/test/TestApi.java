3849b5baed1df311d64130a1c1830a4e3efafa54
Raghu Raman <rraagghhu@gmail.com>
2017-01-21 15:02:22 +0530

    Update TestApi.java

--- src/main/java/com/pratilipi/api/impl/test/TestApi.java
+++ src/main/java/com/pratilipi/api/impl/test/TestApi.java
@@ -3,44 +3,36 @@ package com.pratilipi.api.impl.test;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.google.appengine.api.datastore.QueryResultIterator;
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.ObjectifyService;
 import com.pratilipi.api.GenericApi;
 import com.pratilipi.api.annotation.Bind;
 import com.pratilipi.api.annotation.Get;
-import com.pratilipi.api.annotation.Validate;
 import com.pratilipi.api.shared.GenericRequest;
 import com.pratilipi.api.shared.GenericResponse;
 import com.pratilipi.common.exception.UnexpectedServerException;
-import com.pratilipi.common.type.NotificationType;
-import com.pratilipi.common.util.HttpUtil;
-import com.pratilipi.data.DataAccessorFactory;
-import com.pratilipi.data.RtdbAccessor;
-import com.pratilipi.data.type.UserPreferenceRtdb;
+import com.pratilipi.data.type.gae.PageEntity;
 
 @SuppressWarnings( "serial" )
 @Bind( uri = "/test" )
 public class TestApi extends GenericApi {
-	
+
 	private static final Logger logger = Logger.getLogger( TestApi.class.getName() );
 
-	public static class GetRequest extends GenericRequest {
-
-		@Validate( required = true )
-		private Long userId;
-
-	}
-
 	@Get
-	public GenericResponse get( GetRequest request ) throws UnexpectedServerException {
+	public GenericResponse get( GenericRequest request ) throws UnexpectedServerException {
 
-		RtdbAccessor rtDbAccessor = DataAccessorFactory.getRtdbAccessor();
-		UserPreferenceRtdb preference = rtDbAccessor.getUserPreference( request.userId );
+		QueryResultIterator<Key<PageEntity>> it = ObjectifyService.ofy().load()
+				.type( PageEntity.class )
+				.filterKey( ">=", 4503702338535424L )
+				.filterKey( "<=", 4503741840490496L )
+				.keys()
+				.iterator();
 
-		logger.log( Level.INFO, "UserId: " + request.userId );
-		logger.log( Level.INFO, "EmailFrequency: " + preference.getEmailFrequency() );
-		logger.log( Level.INFO, "EmailFrequency: " + preference.isNotificationSubscribed( NotificationType.AUTHOR_FOLLOW ) );
+		while( it.hasNext() )
+			logger.log( Level.INFO, it.next().getId() + "" );
 
-		logger.log( Level.INFO, new String( HttpUtil.doGet( "https://prod-pratilipi.firebaseio.com/PREFERENCE/5629499534213120.json" ).getData() ) );
-		
 		return new GenericResponse();
 
 	}
