c09c0eb890de4e9700e1d235b32b4bcdf5981d90
RAGHU RAMAN <rraagghhu@gmail.com>
2017-01-21 18:18:32 +0530

    [Backend] Updating TestApi.

--- src/main/java/com/pratilipi/api/impl/test/TestApi.java
+++ src/main/java/com/pratilipi/api/impl/test/TestApi.java
@@ -1,10 +1,7 @@
 package com.pratilipi.api.impl.test;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.google.appengine.api.datastore.QueryResultIterator;
-import com.googlecode.objectify.Key;
 import com.googlecode.objectify.ObjectifyService;
 import com.pratilipi.api.GenericApi;
 import com.pratilipi.api.annotation.Bind;
@@ -23,15 +20,20 @@ public class TestApi extends GenericApi {
 	@Get
 	public GenericResponse get( GenericRequest request ) throws UnexpectedServerException {
 
-		QueryResultIterator<Key<PageEntity>> it = ObjectifyService.ofy().load()
+		System.out.println( ObjectifyService.ofy().load()
 				.type( PageEntity.class )
-				.filterKey( ">=", 4503702338535424L )
-				.filterKey( "<=", 4503741840490496L )
 				.keys()
-				.iterator();
+				.first()
+				.now()
+				.getId() );
 
-		while( it.hasNext() )
-			logger.log( Level.INFO, it.next().getId() + "" );
+		System.out.println( ObjectifyService.ofy().load()
+				.type( PageEntity.class )
+				.order( "-__key__" )
+				.keys()
+				.first()
+				.now()
+				.getId() );
 
 		return new GenericResponse();
 
