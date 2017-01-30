6aa9f8404670115ab7bd8f79d94ac4facb329849
RAGHU RAMAN <rraagghhu@gmail.com>
2017-01-30 19:20:37 +0530

    [Backend] Updating TestApi.

--- src/main/java/com/pratilipi/api/impl/test/TestApi.java
+++ src/main/java/com/pratilipi/api/impl/test/TestApi.java
@@ -1,5 +1,10 @@
 package com.pratilipi.api.impl.test;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.pratilipi.api.GenericApi;
@@ -9,26 +14,36 @@ import com.pratilipi.api.shared.GenericRequest;
 import com.pratilipi.api.shared.GenericResponse;
 import com.pratilipi.common.exception.UnexpectedServerException;
 import com.pratilipi.data.DataAccessorFactory;
+import com.pratilipi.data.RtdbAccessor;
+import com.pratilipi.data.type.UserPreferenceRtdb;
 
 @SuppressWarnings( "serial" )
 @Bind( uri = "/test" )
 public class TestApi extends GenericApi {
 
-	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger( TestApi.class.getName() );
 
 	public static class GetRequest extends GenericRequest {
 
-		private String method;
-
 		private Integer limitTo;
 
 	}
-
 	@Get
 	public static GenericResponse get( GetRequest request ) throws UnexpectedServerException {
 
-		DataAccessorFactory.getRtdbAccessor().getUserPreferences( request.method, request.limitTo );
+		RtdbAccessor rtdbAccessor = DataAccessorFactory.getRtdbAccessor();
+		List<Long> userIds = new ArrayList<>( rtdbAccessor.getUserPreferences( 25 ).keySet() );
+
+		if( request.limitTo != null )
+			userIds = userIds.subList( 0, request.limitTo );
+
+		Long x = new Date().getTime();
+		Map<Long, UserPreferenceRtdb> preferences = rtdbAccessor.getUserPreferences( userIds );
+		Long y = new Date().getTime();
+		
+		System.out.println( "Size: " + preferences.size() );
+
+		logger.log( Level.INFO, "Total Time : " + (y-x) + " ms." );
 		return new GenericResponse();
 
 	}
