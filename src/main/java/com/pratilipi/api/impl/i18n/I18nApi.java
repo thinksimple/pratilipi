20709b4e5a51830999b5bb3d29a6d72b095137e8
RAGHU RAMAN <rraagghhu@gmail.com>
2017-02-03 13:59:24 +0530

    [Backend] Updating I18nApi.

--- src/main/java/com/pratilipi/api/impl/i18n/I18nApi.java
+++ src/main/java/com/pratilipi/api/impl/i18n/I18nApi.java
@@ -7,7 +7,9 @@ import java.util.Map.Entry;
 
 import com.pratilipi.api.GenericApi;
 import com.pratilipi.api.annotation.Bind;
+import com.pratilipi.api.annotation.Get;
 import com.pratilipi.api.annotation.Post;
+import com.pratilipi.api.annotation.Validate;
 import com.pratilipi.api.shared.GenericRequest;
 import com.pratilipi.api.shared.GenericResponse;
 import com.pratilipi.common.exception.InsufficientAccessException;
@@ -25,6 +27,29 @@ import com.pratilipi.filter.AccessTokenFilter;
 @Bind( uri = "/i18n" )
 public class I18nApi extends GenericApi {
 
+	public static class GetRequest extends GenericRequest {
+
+		@Validate( required = true )
+		private Language language;
+
+		private String i18nId;
+
+		private I18nGroup group;
+
+		public void setLanguage( Language language ) {
+			this.language = language;
+		}
+
+		public void setId( String i18nId ) {
+			this.i18nId = i18nId;
+		}
+
+		public void setGroup( I18nGroup group ) {
+			this.group = group;
+		}
+
+	}
+
 	public static class PostRequest extends GenericRequest {
 
 		private I18nGroup group;
@@ -35,8 +60,51 @@ public class I18nApi extends GenericApi {
 
 	}
 
+	public static class GetResponse extends GenericResponse {
+
+		private String value;
+
+		private Map<String, String> keyValues;
+
+		public GetResponse( String value ) {
+			this.value = value;
+		}
+
+		public GetResponse( Map<String, String> keyValues ) {
+			this.keyValues = keyValues;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public Map<String, String> getKeyValues() {
+			return keyValues;
+		}
+
+	}
+
+
+
+	@Get
+	public static GetResponse get( GetRequest request ) {
+
+		DataAccessor dataAccessor = DataAccessorFactory.getDataAccessor();
+
+		if( request.i18nId != null ) {
+			I18n i18n = dataAccessor.getI18n( request.i18nId );
+			return new GetResponse( i18n != null ? i18n.getI18nString( request.language ) : (String) null );
+		}
+
+		if( request.group != null )
+			return new GetResponse( dataAccessor.getI18nStrings( request.group, request.language ) );
+
+		return new GetResponse( (String) null );
+
+	}
+
 	@Post
-	public GenericResponse post( PostRequest request ) 
+	public static GenericResponse post( PostRequest request ) 
 			throws InsufficientAccessException, UnexpectedServerException {
 
 		if( ! UserAccessUtil.hasUserAccess( AccessTokenFilter.getAccessToken().getUserId(), request.language, AccessType.I18N_UPDATE ) )
