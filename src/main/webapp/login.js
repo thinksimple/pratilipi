07a482c52ca4ddaa62d23040350dd25d745fdafb
rdhk <rdhk94@gmail.com>
2017-02-13 18:54:05 +0530

    FRONTEND: Changes for login page.

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -29,9 +29,10 @@ function getQueryVariable(variable) {
 }
 
 function ViewModel() {
-    this.userEmail = "";
-    this.userPassword = "";
-    this.login = function() {
+    this.userEmail = ko.observable("");
+    this.userPassword = ko.observable("");
+    var self = this;
+    this.login_from_fb = function() {
         FB.login( function( response ) {
           if( response == null || response.authResponse == null ) {
 //            facebookLoginOnFlight = false;
@@ -68,6 +69,27 @@ function ViewModel() {
           });
         }, { scope: 'public_profile,email,user_birthday' } );      
     };
+    
+    this.login = function() {
+      $.ajax({
+        
+        type: 'post',
+        url: '/api/user/login',
+    
+        data: { 
+           email: self.userEmail(),
+           password: self.userPassword()
+        },
+        
+        success: function( response ) {
+          console.log("success");
+        },
+        
+        error: function( response ) {
+            alert( "Invalid Credentials" );
+        }
+      });            
+    };   
 }
 
 ko.applyBindings( new ViewModel() );
