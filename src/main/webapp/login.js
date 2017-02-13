68c106f2f061fe0fe830250d14b244ba1884d618
rdhk <rdhk94@gmail.com>
2017-02-13 12:07:27 +0530

    FRONTEND: Linking fb app id for fb login.

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -1,18 +1,22 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '293990794105516',
-    cookie     : true,
-    xfbml      : true,
-    version    : 'v2.6' 
-  });
-};
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '413366985722072',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
 
 function getQueryVariable(variable) {
   var query = window.location.search.substring(1);
