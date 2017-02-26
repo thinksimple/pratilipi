f6c59de3982bb68e93862360499ee37f449326f2
RAGHU RAMAN <rraagghhu@gmail.com>
2017-02-26 08:55:44 +0530

    [Frontend] Transliteration on input field.

---
+++ src/main/webapp/pwa-scripts/transliteration-app.js
@@ -0,0 +1,252 @@
+(function () {
+
+var properties = [
+  'direction',  // RTL support
+  'boxSizing',
+  'width',  // on Chrome and IE, exclude the scrollbar, so the mirror div wraps exactly as the textarea does
+  'height',
+  'overflowX',
+  'overflowY',  // copy the scrollbar for IE
+
+  'borderTopWidth',
+  'borderRightWidth',
+  'borderBottomWidth',
+  'borderLeftWidth',
+  'borderStyle',
+
+  'paddingTop',
+  'paddingRight',
+  'paddingBottom',
+  'paddingLeft',
+
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/font
+  'fontStyle',
+  'fontVariant',
+  'fontWeight',
+  'fontStretch',
+  'fontSize',
+  'fontSizeAdjust',
+  'lineHeight',
+  'fontFamily',
+
+  'textAlign',
+  'textTransform',
+  'textIndent',
+  'textDecoration',  // might not make a difference, but better be safe
+
+  'letterSpacing',
+  'wordSpacing',
+
+  'tabSize',
+  'MozTabSize'
+
+];
+
+var isBrowser = (typeof window !== 'undefined');
+var isFirefox = (isBrowser && window.mozInnerScreenX != null);
+
+function getCaretCoordinates(element, position, options) {
+  if(!isBrowser) {
+    throw new Error('textarea-caret-position#getCaretCoordinates should only be called in a browser');
+  }
+
+  var debug = options && options.debug || false;
+  if (debug) {
+    var el = document.querySelector('#input-textarea-caret-position-mirror-div');
+    if ( el ) { el.parentNode.removeChild(el); }
+  }
+
+  // mirrored div
+  var div = document.createElement('div');
+  div.id = 'input-textarea-caret-position-mirror-div';
+  document.body.appendChild(div);
+
+  var style = div.style;
+  var computed = window.getComputedStyle? getComputedStyle(element) : element.currentStyle;  // currentStyle for IE < 9
+
+  // default textarea styles
+  style.whiteSpace = 'pre-wrap';
+  if (element.nodeName !== 'INPUT')
+    style.wordWrap = 'break-word';  // only for textarea-s
+
+  // position off-screen
+  style.position = 'absolute';  // required to return coordinates properly
+  if (!debug)
+    style.visibility = 'hidden';  // not 'display: none' because we want rendering
+
+  // transfer the element's properties to the div
+  properties.forEach(function (prop) {
+    style[prop] = computed[prop];
+  });
+
+  if (isFirefox) {
+    // Firefox lies about the overflow property for textareas: https://bugzilla.mozilla.org/show_bug.cgi?id=984275
+    if (element.scrollHeight > parseInt(computed.height))
+      style.overflowY = 'scroll';
+  } else {
+    style.overflow = 'hidden';  // for Chrome to not render a scrollbar; IE keeps overflowY = 'scroll'
+  }
+
+  div.textContent = element.value.substring(0, position);
+  // the second special handling for input type="text" vs textarea: spaces need to be replaced with non-breaking spaces - http://stackoverflow.com/a/13402035/1269037
+  if (element.nodeName === 'INPUT')
+    div.textContent = div.textContent.replace(/\s/g, '\u00a0');
+
+  var span = document.createElement('span');
+
+  span.textContent = element.value.substring(position) || '.';  // || because a completely empty faux span doesn't render at all
+  div.appendChild(span);
+
+  var coordinates = {
+    top: span.offsetTop + parseInt(computed['borderTopWidth']),
+    left: span.offsetLeft + parseInt(computed['borderLeftWidth'])
+  };
+
+  if (debug) {
+    span.style.backgroundColor = '#aaa';
+  } else {
+    document.body.removeChild(div);
+  }
+
+  return coordinates;
+}
+
+if (typeof module != 'undefined' && typeof module.exports != 'undefined') {
+  module.exports = getCaretCoordinates;
+} else if(isBrowser){
+  window.getCaretCoordinates = getCaretCoordinates;
+}
+
+}());
+
+var transliterationApp = function( $transliterable_elem, lang ) {
+  this.$transliterable_elem = $transliterable_elem; 
+  this.lang = lang;
+};
+
+transliterationApp.prototype.init = function() {
+  var _this = this;
+  this.setTransliterationElementType();
+  this.setSuggestionResolveFunction();
+  this.suggester = new Suggester('.word-suggester', _this.onSuggestionPicked, _this.$transliterable_elem, _this.isTransliterationInputType(), _this.lang );
+  this.suggester.init();
+  this.$transliterable_elem.on('keypress', _this.suppressKeypress.bind( _this ));
+  this.$transliterable_elem.on('keydown', _this.suppressKeydown.bind( _this ));
+  this.$transliterable_elem.on("click", function(event) {
+    if( _this.suggester.getMode() && ( !$(event.target).closest('.word-suggester').length ) ) {
+      _this.suggester.clear();
+    }
+  });   
+};
+
+function getFirstRange() {
+  var sel = rangy.getSelection();
+  return sel.rangeCount ? sel.getRangeAt(0) : null;
+};
+
+transliterationApp.prototype.onSuggestionPickedInsideDiv = function( word, eng_word ) {
+  if (window.getSelection) {
+    var text = ( word ? word : eng_word ) + "\u00A0";
+    sel = rangy.getSelection();
+    var range = getFirstRange();
+    if (range) {
+      var text_node = document.createTextNode(text);
+      range.insertNode( text_node );
+      range.setStartAfter( text_node );
+      // sel.collapseToEnd();
+      sel.setSingleRange(range);
+    }
+  }
+};
+
+transliterationApp.prototype.onSuggestionPickedInsideInput = function(word, eng_word) {
+  // console.log(':::::::Resolved with word ' + word + '::::::::')
+  var text = ( word ? word : eng_word ) + " "; 
+  var startPos = this.content_holder.get(0).selectionStart;
+  var endPos = this.content_holder.get(0).selectionEnd;
+  var positon_after_selection = startPos + text.length ;
+  var word = this.content_holder.get(0).value.substring(0, startPos)
+  + text + this.content_holder.get(0).value.substring(endPos, this.content_holder.get(0).value.length);
+  $( this.content_holder.get(0)).val( word ).change();
+  this.content_holder.get(0).selectionStart = this.content_holder.get(0).selectionEnd = positon_after_selection;
+};
+
+function insertNodeAtRange() {
+  var range = getFirstRange();
+  if (range) {
+    var el = document.createElement("span");
+    el.className = "curWord";
+    range.insertNode(el);
+    rangy.getSelection().setSingleRange(range);
+    return el;
+  }
+};
+
+transliterationApp.prototype.setTransliterationElementType = function() {
+  this.isInputTypeElement = this.$transliterable_elem.is("input,textarea") ? true : false;
+};
+
+transliterationApp.prototype.setSuggestionResolveFunction = function() {
+  this.onSuggestionPicked = this.isTransliterationInputType() ? this.onSuggestionPickedInsideInput : this.onSuggestionPickedInsideDiv;
+};
+
+transliterationApp.prototype.isTransliterationInputType = function() {
+  return this.isInputTypeElement;
+};
+
+transliterationApp.prototype.suppressKeypress = function(e) {
+  var keycode = e.keycode || e.which;
+  var isShiftKey = e.shiftKey;
+  var translation = new Translation( keycode, isShiftKey );
+  if( this.shouldPreventKeypress( translation ) ) {
+    e.preventDefault();
+    this.sendKeyToSuggester( translation );
+  }
+};
+
+transliterationApp.prototype.sendKeyToSuggester = function( translation ) {
+  this.suggester.type( translation );
+};
+
+transliterationApp.prototype.shouldPreventKeypress = function( translation ) {
+  // var translation = new Translation( keycode );
+  return ( this.isNotBackspaceKey(translation) && !this.isKeydownActionKey(translation) && ( this.isNotKeypressActionKey(translation) || this.suggester.getMode() ) );
+};
+
+transliterationApp.prototype.isNotKeypressActionKey = function( translation ) {
+  return ( translation.action != 'space' && translation.action != 'special_char' );
+};
+
+transliterationApp.prototype.isNotBackspaceKey = function( translation ) {
+  return translation.action != 'backspace';
+};
+
+transliterationApp.prototype.suppressKeydown = function(e) {
+  var keycode = e.keycode || e.which;
+  var isShiftKey = e.shiftKey;
+  var translation = new Translation( keycode, isShiftKey );
+  if( this.shouldPreventKeydown( translation ) ) {
+    e.preventDefault();
+    this.sendKeyToSuggester( translation );
+  }
+};
+
+transliterationApp.prototype.shouldPreventKeydown = function( translation ) {
+  // var translation = new Translation( keycode );
+  return ( this.isKeydownActionKey(translation) && this.suggester.getMode() );
+};
+
+transliterationApp.prototype.isKeydownActionKey = function( translation ) {
+  if( this.isTransliterationInputType() ) {
+    return ( translation.action == 'up' || translation.action == 'down' || translation.action == 'left' || translation.action == 'right' || translation.action == 'escape' || translation.action == 'tab' || translation.action == 'backspace');
+  } else {
+    return ( translation.action == 'up' || translation.action == 'down' || translation.action == 'left' || translation.action == 'right' || translation.action == 'escape' );
+  }
+};
+
+
+window.onload = function() {
+  if(screen.width > 480) {
+    rangy.init();
+  } 
+};
---
+++ src/main/webapp/pwa-scripts/transliteration-suggester.js
@@ -0,0 +1,304 @@
+var Suggester = function(selectorOrElem, onResolve, content_holder, isInputElement, lang) {
+  if(selectorOrElem) {
+    this.elem = $(selectorOrElem);
+    this.setMode( false );
+  } else {
+    throw 'Coding karni nahi aati.'
+  }
+
+  if(typeof onResolve == 'function') {
+    this.resolveCallback = onResolve;
+  } else {
+    throw 'Coding karni nahi aati.'
+  }
+  
+  if( content_holder ) {
+    this.content_holder = content_holder;
+    this.isInputTypeElement = isInputElement;
+  } else {
+    throw 'Coding karni nahi aati.'
+  }
+  this.lang = lang;
+  this.url = "https://www.google.com/inputtools/request?ime=transliteration_en_" + this.lang + "&num=5&cp=0&cs=0&ie=utf-8&oe=utf-8&app=jsapi";
+  this.text = '';
+  this.savedSelection = null;
+  this.words_map = {};
+};
+
+var suggesterMethods = {
+  init: function() {
+    var self = this;
+    this.content_holder.on("blur", function() {
+      if( self.getMode() ) {
+        if (this.savedSelection) {
+          rangy.removeMarkers(this.savedSelection);
+        }
+        this.savedSelection = rangy.saveSelection();
+      }
+    });
+
+    this.elem.on('click',  'div.suggestion', function(e) {
+      self.elem.find(".highlight-suggestion").removeClass("highlight-suggestion");
+      $(this).addClass("highlight-suggestion");
+      if (this.savedSelection) {
+        rangy.restoreSelection(savedSel, true);
+        this.savedSelection = null;
+      }  
+      self.handleWordSelection();
+    } );
+  },
+
+  type: function( translation ) {
+    // var translation = new Translation(keycode);
+    if( translation.action == 'typing' ) {
+      if( !this.getMode() ) {
+        //add a span tag there and set the relative position of the dropdown
+        if( this.isInputTypeElement == false ) {
+          this.curSpan = insertNodeAtRange();
+        }
+        this.setSuggesterPosition();
+        this.showSuggester();
+        this.setMode( true );
+      }
+      this.setText(this.text + translation.text);
+    } else if(translation.action == 'space' || translation.action == 'enter') {
+      this.handleWordSelection();
+    } else if(  translation.action == 'special_char' ) {
+      this.text += translation.text;
+      this.handleWordSelection( translation.text );
+    } else if(translation.action == 'backspace') {
+      this.backspace();
+    } else if(translation.action == 'up') {
+      this.goUp();
+    } else if( translation.action == 'down') {
+      this.goDown();
+    } else if( translation.action == 'escape' || translation.action == 'tab' ) {
+      this.clear();
+    }
+  },
+
+  backspace: function() {
+    if( this.text.length == 1 ) {
+      this.clear();
+    } else {
+      this.setText(this.text.substr(0, this.text.length -1));
+    }
+  },
+
+  goUp: function() {
+    var $suggestions = this.elem.find('.suggestions');
+    var $currently_highlighted_suggestion = $suggestions.find('.highlight-suggestion');
+    var $prev_suggestion = $currently_highlighted_suggestion.prev();
+    $currently_highlighted_suggestion.removeClass("highlight-suggestion");
+    if( $prev_suggestion.length ) {
+      $prev_suggestion.addClass("highlight-suggestion");
+    } else {
+       $suggestions.find(".suggestion:last").addClass("highlight-suggestion");
+    }
+  },  
+
+  goDown: function() {
+    var $suggestions = this.elem.find('.suggestions');
+    var $currently_highlighted_suggestion = $suggestions.find('.highlight-suggestion');
+    var $next_suggestion = $currently_highlighted_suggestion.next();
+    $currently_highlighted_suggestion.removeClass("highlight-suggestion");
+    if( $next_suggestion.length ) {
+      $next_suggestion.addClass("highlight-suggestion");
+    } else {
+       $suggestions.find(".suggestion:first").addClass("highlight-suggestion");
+    }
+  },
+
+  getSuggestions: function() {
+
+    if( this.words_map[this.text] ) {
+      this.suggestFromWordsMap();
+    } else {
+      this.suggestFromGoogleApi();
+    }
+
+  },
+
+  suggestFromWordsMap: function() {
+    this.suggestions = this.words_map[this.text];
+    this.suggest();
+  },
+
+  suggestFromGoogleApi: function() {
+    var self = this;
+    $.ajax({
+      url: self.url,
+      data: {
+        text: self.text
+      },
+      type: "GET",
+   
+      dataType : "json",
+    })
+    .done(function( json ) {
+      self.suggestions = json[1][0][1];
+      self.suggestions.push( self.text );
+      self.suggest();
+      self.words_map[ self.text ] = self.suggestions;
+    });
+  },
+
+  clearSuggestions: function() {
+    this.suggestions = [];
+    this.suggest();
+  },
+
+  suggest: function() {
+    var $suggestions = this.elem.find('.suggestions');
+    $suggestions.empty();
+    
+    this.suggestions.forEach(function(suggestion) {
+      var $suggestion = $("<div class='suggestion'></div>");
+      $suggestion.text(suggestion);
+      $suggestions.append($suggestion);
+    });
+    $suggestions.find(".suggestion:first").addClass("highlight-suggestion");    
+  },
+
+  clear: function() {
+    this.setText('');
+    this.hideSuggester();
+    this.clearSpanTags();
+    this.setMode(false);
+  },
+
+  setText: function(text) {
+    // this.addSpanElement();
+    this.text = text;
+    this.getInputElem().text(this.text);
+    if(text) {
+      this.getSuggestions();
+    } else {
+      this.clearSuggestions();
+    }
+  },
+
+  getInputElem: function() {
+    if (!this.inputElem) {
+      this.inputElem = this.elem.find('.word-input');
+    }
+    return this.inputElem;
+  },
+
+  resolve: function(withSuggestion) {
+    this.resolveCallback && this.resolveCallback(withSuggestion, this.text);
+    this.clear();
+  },
+
+  setMode: function( val ) {
+    this.mode = val;
+  },
+
+  getMode: function() {
+    return this.mode;
+  },
+
+  getCaretPosition: function() {
+
+    var coordinates = {};
+    if( this.isInputTypeElement ) {
+      var caret_coordinates = getCaretCoordinates( this.content_holder.get(0), this.content_holder.get(0).selectionEnd );
+      coordinates["top"] = caret_coordinates.top + this.content_holder.offset().top;
+      coordinates["left"] = caret_coordinates.left + this.content_holder.offset().left;
+    }
+    else {
+      var doc_height = $(document).height();
+      // var client_height = document.body.clientHeight;
+      coordinates["top"] = this.curSpan.offsetTop + this.curSpan.offsetHeight + this.content_holder.get(0).offsetTop;
+      coordinates["left"] = this.curSpan.offsetLeft + this.content_holder.get(0).offsetLeft;
+
+      // var visibleTop = coordinates["top"] - $(window).scrollTop();
+      // console.log( doc_height, coordinates["top"], doc_height - coordinates["top"] );
+      // console.log( client_height, visibleTop, client_height - visibleTop );
+      var last_usable_value = doc_height - coordinates["top"];
+      if( doc_height - coordinates["top"] < 200 ) {
+        // this.content_holder.height( this.content_holder.height() + 200 );
+        coordinates["top"] -= 180;
+        window.scrollTo( 0, document.body.scrollHeight + 200 );
+      }
+    }
+    return coordinates;
+  },
+
+  setSuggesterPosition: function() {
+    var coordinates = this.getCaretPosition();
+    var self = this;
+
+    this.elem.css({
+      top: coordinates.top,
+      left: coordinates.left,
+    });
+
+    // this.elem.css({
+    //   top: self.curSpan.offsetTop + self.curSpan.offsetHeight + self.content_holder.get(0).offsetTop,
+    //   left: self.curSpan.offsetLeft + self.content_holder.get(0).offsetLeft,
+    // });
+  },
+
+  showSuggester: function() {
+    this.elem.show();
+  },
+
+  hideSuggester: function() {
+    this.elem.hide();
+  },
+
+  clearSpanTags: function() {
+    $("span.curWord").remove();
+  },
+
+  handleWordSelection: function( special_char ) {
+    var selected_word;
+    var $highlighted_word = this.elem.find(".highlight-suggestion");
+    selected_word = $highlighted_word.length ? $highlighted_word.text() : this.text;
+    if( special_char ) {
+      selected_word += special_char;
+    }
+    this.resolve( selected_word );
+  }
+    
+};
+
+for(var method in suggesterMethods) {
+  Suggester.prototype[method] = suggesterMethods[method];
+}
+
+var Translation = function(keycode, isShiftKey) {
+  if( keycode == 13 ) {
+    this.action = 'enter'
+  } else if( keycode == 32 ) {
+    this.action = 'space';
+  } else if( keycode == 8 ) {
+    this.action = 'backspace';
+  } else if( keycode == 9 ) {
+    this.action = 'tab';
+  } else if( keycode == 38 && !isShiftKey ) {
+    this.action = 'up';
+  } else if( keycode == 40 && !isShiftKey ) {
+    this.action = 'down';
+  } else if( keycode == 37 && !isShiftKey ) {
+    this.action = 'left';
+  } else if( keycode == 39 && !isShiftKey ) {
+    this.action = 'right';
+  } else if( keycode == '27') {
+    this.action = 'escape';
+  }
+
+  else if(keycode > 32 && keycode < 127) {
+    if( keycode.between(33, 47) || keycode.between(58, 64) || keycode.between(91, 96) || keycode.between(123, 126) ) {
+      this.action = 'special_char';
+    } else {
+      this.action = 'typing';
+    }
+    this.text = String.fromCharCode(keycode).toLowerCase();
+  }
+};
+
+Number.prototype.between = function (min, max) {
+  return this >= min && this <= max;
+}
