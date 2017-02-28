4ced6836162382a4c70053e824a84d5611a29e14
Rahul Ranjan <genirahul@users.noreply.github.com>
2017-02-28 20:40:17 +0530

    Support Mail Test code

--- src/main/java/com/pratilipi/data/util/ConversationDataUtil.java
+++ src/main/java/com/pratilipi/data/util/ConversationDataUtil.java
@@ -68,6 +68,101 @@ public class ConversationDataUtil {
 		
 		conversation = dataAccessor.createOrUpdateConversation( conversation, conversationMessage );
 		
+		String language = author != null ? author.getLanguage().getNameEn().toLowerCase() : null;
+		Logger.getLogger(ConversationDataUtil.class.getSimpleName())
+					.log(Level.SEVERE, "Language : " + language);
+		try {
+			ArrayList<InternetAddress> receiverList = createReceiversId(team.name().toLowerCase(), language);
+			InternetAddress[] receivers = new InternetAddress[receiverList.size()];
+			receiverList.toArray(receivers);
+			createSupportMailTask(receivers, userId.toString(), name, email, phone, message, data, team.name());
+		} catch (UnsupportedEncodingException | UnexpectedServerException e) {
+			// TODO Auto-generated catch block
+			Logger.getLogger(ConversationDataUtil.class.getSimpleName())
+					.log(Level.SEVERE, "Exception while sending Support mail");
+			Logger.getLogger(ConversationDataUtil.class.getSimpleName())
+					.log(Level.SEVERE, "User ID : " + userId);
+			Logger.getLogger(ConversationDataUtil.class.getSimpleName())
+					.log(Level.SEVERE, "Conversation Id : " + conversation.getId());
+			e.printStackTrace();
+		}
+	}
+	
+	private static void createSupportMailTask(
+			InternetAddress[] receiversList, String userId, String name, 
+			String email, String phone, String message, JsonObject data, String team) 
+					throws UnsupportedEncodingException, UnexpectedServerException {
+		
+		// Preparing report data
+		String reportString = "", subject = "", type;
+		if (data != null) {
+			type = data.get(TYPE).getAsString();
+			if (type.toLowerCase().equals("pratilipi")) {
+				subject = "REPORTED PRATILIPI";
+				reportString = "Reported Pratilipi Id : " + data.get(ID).getAsString() + "\n";
+			} else {
+				subject = "REPORTED AUTHOR";
+				reportString = "Reported Author Id : " + data.get(ID).getAsString() + "\n";
+			}
+		}
+		
+		if (subject.isEmpty())
+			subject = team;
+		
+		Logger.getLogger(ConversationDataUtil.class.getSimpleName())
+				.log(Level.SEVERE, "Subject : " + subject);
+		
+		String body = "User Name : " + name + "\n"
+				+ "Phone Number : " + phone + "\n"
+				+ "User Id : " + userId + "\n"
+				+ reportString
+				+ "Message : " + MimeUtility.encodeText( message, "UTF-8", "B" ) + "\n";
+		
+		Logger.getLogger(ConversationDataUtil.class.getSimpleName())
+				.log(Level.SEVERE, "Body : " + body);
+		
+		InternetAddress[] cc = new InternetAddress[]{new InternetAddress("ranjeet@pratilipi.com", "Ranjeet Pratap Singh")};
+		
+		Logger.getLogger(ConversationDataUtil.class.getSimpleName())
+				.log(Level.SEVERE, "CC : " + Arrays.toString(cc));
+
+		Logger.getLogger(ConversationDataUtil.class.getSimpleName())
+				.log(Level.SEVERE, "CC : " + Arrays.toString(receiversList));
+		
+		// THIS IS FOR TESTING EMAIL. TASK QUEUE WILL BE ADDED IN PRODUCTION
+		
+		EmailUtil.sendMail(
+				name, 
+				email, 
+				receiversList, 
+				new InternetAddress[]{new InternetAddress("rahul@pratilipi.com", "Rahul Ranjan")}, 
+				subject, 
+				body
+		);
+		
+	}
+	
+	private static ArrayList<InternetAddress> createReceiversId(String teamName, String language) 
+				throws UnsupportedEncodingException {
+		ArrayList<InternetAddress> emailList = new ArrayList<>();
+		emailList.add(new InternetAddress( "abhishek@pratilipi.com", "Abhishek Sharma" ));
+		emailList.add(new InternetAddress( "shreyansh@pratilipi.com", "Shreyansh Maini" ));
+		emailList.add(new InternetAddress( "rahul@pratilipi.com", "Rahul Ranjan" ));
+		if (teamName.contains(AEE) || teamName.contains(ISSUES)) {	
+			// when contact team name is AEE_* or ANDROID_APP_ISSUES
+			if (language != null)
+// 				emailList.add(new InternetAddress( language + DOMAIN, language + DOMAIN ));
+			Logger.getLogger(ConversationDataUtil.class.getSimpleName())
+				.log(Level.SEVERE, "Language Team : " + language + DOMAIN);
+		} else if (teamName.contains(TECH_SUPPORT)) {		
+			// when contact team name is ANDROID_APP_TECH_SUPPORT 
+// 			emailList.add(new InternetAddress( "prashant@pratilipi.com", "Prashant" ));
+// 			emailList.add(new InternetAddress( "kshitij@pratilipi.com", "Kshitij Sharma" ));
+			Logger.getLogger(ConversationDataUtil.class.getSimpleName())
+				.log(Level.SEVERE, "Tech support issue");
+		}
+		
+		return emailList;
 	}
 
 }
