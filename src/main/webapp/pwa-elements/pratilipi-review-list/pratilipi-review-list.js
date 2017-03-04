d1c152355d2459bf6db041c5f70a04811dcf44b6
rdhk <rdhk94@gmail.com>
2017-03-04 11:22:35 +0530

    FRONTEND: Changes in review-list section.

--- src/main/webapp/pwa-elements/pratilipi-review-list/pratilipi-review-list.js
+++ src/main/webapp/pwa-elements/pratilipi-review-list/pratilipi-review-list.js
@@ -1,79 +1,44 @@
 function( params ) {
     var self = this;
-//    console.log( "params", params );
-//    console.log( "pobj", params.pratilipiObj );
-//    console.log( "upobj", params.userPratilipiObj );
-    this.pratilipiObj = params.pratilipi;
-    this.userPratilipiObj = params.userPratilipi;
-    console.log( this.userPratilipiObj, this.pratilipiObj );
-    
-    this.hasAccessToReview = ko.computed( function() { /* test this for changes in review access */
-        if( appViewModel.user.isGuest() )
-            return  true;
-        else if( !isEmpty( this.userPratilipiObj ) ) /* if user is not guest, this will never be empty */
-            return this.userPratilipiObj.hasAccessToReview();
-    }, this);
-    
-    this.isGuest = ko.observable( appViewModel.user.isGuest() );
-    this.dataAccessor = new DataAccessor();
-    // this.pratilipiId = "5093925535612928";
+    this.pratilipi = params.pratilipi;
+    this.userpratilipi = params.userPratilipi;
+    this.updatePratilipi = params.updatePratilipi;
+    this.updateUserPratilipi = params.updateUserPratilipi;
+    // console.log( this.userpratilipi, this.pratilipi );
+
+    /* Constants */
     this.maxRating = 5;    
     this.firstLoadReviewCount = 5;
-    this.subsequentLoadReviewCount = 10;  
-    
+    this.subsequentLoadReviewCount = 10; 
+
+
     this.reviewList = ko.observableArray( [] );
+
+    this.dataAccessor = new DataAccessor();
 
     this.isSaveInProgress = ko.observable( false );
     this.totalReviewsPresent = ko.observable( 0 );
+    this.selectedReviewRating = ko.observable( 0 ); /* init the initially until we get userpratiipi obj */
+    this.newReviewContent = ko.observable( "" ); /* init the initially until we get userpratiipi obj */
+   
 
-    this.initializeReviewRating = function() {
-        if( appViewModel.user.isGuest() ) {
-            return 0;
-        } else {
-            if( this.userPratilipiObj.rating )
-                return this.userPratilipiObj.rating();
-            else 
-                return 0;
-        }
-    };
-
-    this.initializeReviewContent = function() {
-        if( appViewModel.user.isGuest() ) {
-            return "";
-        } else {
-            if( this.userPratilipiObj.review )
-                return this.userPratilipiObj.review();
-            else 
-                return "";
-        }
-    };    
-
-    this.selectedReviewRating = ko.observable( this.initializeReviewRating() );
-    this.newReviewContent = ko.observable( this.initializeReviewContent() );
+    
+    this.isGuest = ko.computed( function() {
+        return appViewModel.user.isGuest();
+    }, this);
 
     this.totalReviewsShown = ko.computed(function() {
         return this.reviewList().length;
     }, this);
-    
+
     this.areMoreReviews = ko.computed(function() {
-        return this.totalReviewsShown() < this.totalReviewsPresent();
+        return this.totalReviewsShown() < this.totalReviewsPresent(); /* can change this to directly have reviewlist.length() */
     }, this);
-    
+
+    /* helper function */
     this.isNewReviewRatingZero = function() {
       return this.selectedReviewRating() == 0;
-    };   
-    
-    this.emptyReviewRating = ko.computed(function() {
-        return ( this.maxRating - this.selectedReviewRating() );
-    }, this);
-    
-    this.isSaveDisabled = ko.computed( function() {
-        return this.isNewReviewRatingZero() ||  this.isSaveInProgress();
-    }, this ); 
-    
-
-    
-//    this.userIsGuest = ko.observable();
+    }; 
     this.pushToReviewList = function( revList ) {
         for( var i=0; i< revList.length; i++ ) {
             this.reviewList.push( revList[i] );
@@ -83,8 +48,7 @@ function( params ) {
     this.addToReviewList = function( review ) {
         this.reviewList.unshift( review );
         this.settotalReviewsPresent( this.totalReviewsPresent() + 1 );
-    };  
-
+    }; 
     this.settotalReviewsPresent = function( count ) {
         this.totalReviewsPresent( count );
     };
@@ -108,29 +72,8 @@ function( params ) {
     
     this.loadMoreReviews = function() {
         this.dataAccessor.getReviewList( this.pratilipiId, this.totalReviewsShown(), null, this.subsequentLoadReviewCount, this.loadMoreReviewsCallback.bind( this ) );
-    };    
-//    this.getUser();
-    
-    var dialog = document.querySelector( '#pratilipi-review-dialog' );
-    if ( !dialog.showModal ) {
-        dialogPolyfill.registerDialog( dialog );
-    }
-    
-    this.openReviewModal = function() {        
-        if( self.isGuest() ) {
-            goToLoginPage();
-            return;
-        }
-        else {
-            componentHandler.upgradeDom();
-            dialog.showModal();
-        }
-    };
-   
-    this.hideReviewModal = function() {
-        dialog.close();
-    };
-    
+    };  
+
     this.setSelectedReviewRating = function ( rating ) {
         this.selectedReviewRating( rating );
     };   
@@ -190,25 +133,104 @@ function( params ) {
             }
         });     
       
-    }; 
+    };           
+
+    this.emptyReviewRating = ko.computed(function() {  /* TODO change its name to remptyReviewSTars*/
+        return ( this.maxRating - this.selectedReviewRating() );
+    }, this);
     
-//     if( !isEmpty( this.pratilipiObj ) ) {
-//        this.pratilipiId = this.pratilipiObj.pratilipiId();
-//        this.getReviewList();
-//     }  
-      this.init = ko.computed( function() {
+    this.isSaveDisabled = ko.computed( function() {
+        return this.isNewReviewRatingZero() ||  this.isSaveInProgress();
+    }, this );
+
+    var dialog = document.querySelector( '#pratilipi-review-dialog' );
+    if ( !dialog.showModal ) {
+        dialogPolyfill.registerDialog( dialog );
+    }
+    
+    this.openReviewModal = function() {        
+        if( self.isGuest() ) {
+            goToLoginPage();
+            return;
+        }
+        else {
+            componentHandler.upgradeDom();
+            dialog.showModal();
+        }
+    };
+   
+    this.hideReviewModal = function() {
+        dialog.close();
+    };    
+
+
+
+
+
+
+
+    
+
+
+
+/*    this.initializeReviewRating = function() {
+        if( appViewModel.user.isGuest() ) {
+            return 0;
+        } else {
+            if( this.userpratilipi.rating )
+                return this.userpratilipi.rating();
+            else 
+                return 0;
+        }
+    };
+
+    this.initializeReviewContent = function() {
+        if( appViewModel.user.isGuest() ) {
+            return "";
+        } else {
+            if( this.userpratilipi.review )
+                return this.userpratilipi.review();
+            else 
+                return "";
+        }
+    };   */ 
+
+
+    this.hasAccessToReview = ko.computed( function() { /* can be true if person has already reviewed */
+        if( appViewModel.user.isGuest() )
+            return  true;
+        // else if( !isEmpty( this.userpratilipi ) ) /* if user is not guest, this will never be empty */
+        else
+            return this.userpratilipi.hasAccessToReview(); /* whenever userPratilipi has accessToReview Changes ths will change */
+    }, this);
+
+     
+    this.pratilipiIdObserver = ko.computed( function() {
         console.log("inside reviewws");
-        if( this.pratilipiObj.pratilipiId() ) {
-          this.pratilipiId = this.pratilipiObj.pratilipiId();
+        if( this.pratilipi.pratilipiId() ) {
+          this.pratilipiId = this.pratilipi.pratilipiId();
           this.getReviewList();
         }
-  //      return this.pratilipi.title();
-      }, this );
+    }, this );
+
+    this.userPratilipiRatingObserver = ko.computed( function() {
+        console.log( "inside up observer" );
+        if( this.userpratilipi.rating ) {
+            this.selectedReviewRating( this.userPratilipi.rating() );
+        }
+    }, this);
+
+    this.userPratilipiReviewContentObserver = ko.computed( function() {
+        console.log( "inside up observer" );
+        if( this.userpratilipi.review ) {
+            this.newReviewContent( this.userPratilipi.review() );
+        }
+    }, this);      
      
      // if( appViewModel.user.isGuest() ) {
      //     this.hasAccessToReview( true );
-     // } else if ( !isEmpty( this.userPratilipiObj ) ) {
-     //      this.hasAccessToReview( this.userPratilipiObj.hasAccessToReview() );  
+     // } else if ( !isEmpty( this.userpratilipi ) ) {
+     //      this.hasAccessToReview( this.userpratilipi.hasAccessToReview() );  
      // } 
      this.deleteReview = function( review ) {
          self.reviewList.remove( review );
