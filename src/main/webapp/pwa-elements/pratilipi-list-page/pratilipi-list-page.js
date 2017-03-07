b86d7bbc74b8c13fa176da565d204b425705ad97
RAGHU RAMAN <rraagghhu@gmail.com>
2017-03-08 02:25:50 +0530

    [Frontend] Updating pratilipi-list-page.

--- src/main/webapp/pwa-elements/pratilipi-list-page/pratilipi-list-page.js
+++ src/main/webapp/pwa-elements/pratilipi-list-page/pratilipi-list-page.js
@@ -2,25 +2,29 @@ function() {
 	var self = this;
 	var cursor = null;
 	var resultCount = 20;
+	var dataAccessor = new DataAccessor();
 
-	this.pratilipiList = ko.mapping.fromJS( [], {}, self.pratilipi );
+	this.pratilipiList = ko.observableArray();
 	this.hasMoreContents = ko.observable( true );
+	this.isLoading = ko.observable( false );
 
 
 	this.updatePratilipiList = function( pratilipiList ) {
-		ko.mapping.fromJS( pratilipiList, {}, self.pratilipiList );
+		for( var i = 0; i < pratilipiList.length; i++ )
+			self.pratilipiList.push( ko.mapping.fromJS( pratilipiList[i] ) );
 	};
 
 	this.initialDataLoaded = ko.observable( false );
 
 	this.fetchPratilipiList = function() {
-		var dataAccessor = new DataAccessor();
+		self.isLoading( true );
 		dataAccessor.getPratilipiListByListName( window.location.pathname.substring(1), cursor, null, resultCount,
 				function( pratilipiListResponse ) {
 					var pratilipiList = pratilipiListResponse.pratilipiList;
 					self.updatePratilipiList( pratilipiList );
 					cursor = pratilipiListResponse.cursor;
 					self.initialDataLoaded( true );
+					self.isLoading( false );
 					self.hasMoreContents( pratilipiList.length == resultCount );
 		});
 	};
