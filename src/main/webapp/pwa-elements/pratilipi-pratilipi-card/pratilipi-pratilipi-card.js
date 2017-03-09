0224ce2039dc96d44e5c672496c9b1c5c25da7d5
RAGHU RAMAN <rraagghhu@gmail.com>
2017-03-09 15:55:55 +0530

    [Frontend] Updating pratilipi-pratilipi-card.

--- src/main/webapp/pwa-elements/pratilipi-pratilipi-card/pratilipi-pratilipi-card.js
+++ src/main/webapp/pwa-elements/pratilipi-pratilipi-card/pratilipi-pratilipi-card.js
@@ -3,8 +3,33 @@ function( params ) {
 
 	var dataAccessor = new DataAccessor();
 	this.pratilipi = params.pratilipi;
+	this.libraryRequestOnFlight = ko.observable( false );
+
+	this.updatePratilipi = function( pratilipi ) {
+		ko.mapping.fromJS( pratilipi, {}, self.pratilipi );
+	};
+
+	this.addToLibrary = function( vm, evt ) {
+		evt.stopPropagation();
+		self.libraryRequestOnFlight( true );
+		var addedToLib = self.pratilipi.addedToLib();
+		self.updatePratilipi( { "addedToLib": ! addedToLib } );
+		dataAccessor.addOrRemoveFromLibrary( self.pratilipi.pratilipiId(), ! addedToLib, 
+			function( pratilipi ) {
+				self.updatePratilipi( pratilipi );
+				self.libraryRequestOnFlight( false );
+			}, function( error ) {
+				self.libraryRequestOnFlight( false );
+				self.updatePratilipi( { "addedToLib": addedToLib } );
+				var message = "${ _strings.server_error_message }";
+				if( error[ "message" ] != null )
+					message = error[ "message" ];
+				ToastUtil.toast( message, 3000, true );
+		});
+	};
 
 	this.test = function( vm, evt ) {
 		evt.stopPropagation();
-	}
+	};
+
 }
