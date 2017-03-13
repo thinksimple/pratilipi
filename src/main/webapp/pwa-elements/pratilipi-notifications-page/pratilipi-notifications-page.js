815c155c8ef8a025fd354985c3f0b25e6691b4ab
RAGHU RAMAN <rraagghhu@gmail.com>
2017-03-13 12:54:41 +0530

    [Frontend] Updating notifications page.

--- src/main/webapp/pwa-elements/pratilipi-notifications-page/pratilipi-notifications-page.js
+++ src/main/webapp/pwa-elements/pratilipi-notifications-page/pratilipi-notifications-page.js
@@ -1,1 +1,10 @@
-function( params ) {}
+function( params ) {
+	var self = this;
+
+	this.userObserver = ko.computed( function() {
+		if( appViewModel.user.isGuest() && appViewModel.user.userId() == 0 ) {
+			ToastUtil.toastUp( "${ _strings.user_login_to_view_notifications }" );
+		}
+	}, this );
+
+}
