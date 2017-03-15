c624add4c5c334f7f0f3fd452dc8a9bc452deaf6
RAGHU RAMAN <rraagghhu@gmail.com>
2017-03-15 15:26:34 +0530

    [Frontend] Updating MetaTagUtil.

--- src/main/webapp/pwa-scripts/utils/MetaTagUtil.js
+++ src/main/webapp/pwa-scripts/utils/MetaTagUtil.js
@@ -14,10 +14,10 @@ var MetaTagUtil = (function() {
 			metaTagArray.push( new metaTag( "og:type", "books.book" ) );
 			metaTagArray.push( new metaTag( "og:description", pratilipi.summary != null ? pratilipi.summary : "${ _strings.home_page_title }" ) );
 			metaTagArray.push( new metaTag( "og:locale", "${ lang }_IN" ) );
-			metaTagArray.push( new metaTag( "og:url", "http://" + pratilipi.language.toLowerCase() + ".pratilipi.com" + pratilipi.pageUrl ) );
+			metaTagArray.push( new metaTag( "og:url", "http://" + ( pratilipi.language != null ? pratilipi.language.toLowerCase() : "${ language?lower_case }" ) + ".pratilipi.com" + pratilipi.pageUrl ) );
 			metaTagArray.push( new metaTag( "og:image", getImageUrl( pratilipi.coverImageUrl, 256 ) ) );
 			metaTagArray.push( new metaTag( "books:isbn", pratilipi.pratilipiId ) );
-			metaTagArray.push( new metaTag( "books:author", "http://" + pratilipi.language.toLowerCase() + ".pratilipi.com" + pratilipi.author.pageUrl ) );
+			metaTagArray.push( new metaTag( "books:author", "http://" + ( pratilipi.language != null ? pratilipi.language.toLowerCase() : "${ language?lower_case }" ) + ".pratilipi.com" + pratilipi.author.pageUrl ) );
 			appViewModel.metaTags( metaTagArray );
 		}
 	};
