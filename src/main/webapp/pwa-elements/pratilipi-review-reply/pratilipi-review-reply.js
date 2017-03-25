74c6139b28b631b283be95e22cee21cfc5bb97fc
RAGHU RAMAN <rraagghhu@gmail.com>
2017-03-25 18:57:31 +0530

    [Frontend] Bug fix in review-reply.

--- src/main/webapp/pwa-elements/pratilipi-review-reply/pratilipi-review-reply.js
+++ src/main/webapp/pwa-elements/pratilipi-review-reply/pratilipi-review-reply.js
@@ -9,22 +9,5 @@ function( params ) {
     this.isSaveDisabled = ko.computed( function() {
         return this.reply().trim().length == 0 ||  this.saveInProgress();
     }, this );    
-    
-    var textarea = document.querySelector('textarea.js-autoresize-textarea');
 
-
-    textarea.addEventListener('keydown', autosize);
-                 
-    function autosize(){
-      var el = this;
-      setTimeout(function(){
-        el.style.cssText = 'height:auto; padding:0';
-        // for box-sizing other than "content-box" use:
-        // el.style.cssText = '-moz-box-sizing:content-box';
-        el.style.cssText = 'height:' + el.scrollHeight + 'px';
-      },0);
-    } 
-    // var $content_object = $( "#review" );
-    // this.content_transliteration_object = new transliterationApp( $content_object, "${ lang }" );
-    // this.content_transliteration_object.init();  
 }
