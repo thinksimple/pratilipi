f211bd46d02a07d80aa523334dfaf0b9473629a2
RAGHU RAMAN <rraagghhu@gmail.com>
2017-03-27 14:34:20 +0530

    [Frontend] Updating pratilipi-card.

--- src/main/webapp/pwa-elements/pratilipi-pratilipi-card/pratilipi-pratilipi-card.js
+++ src/main/webapp/pwa-elements/pratilipi-pratilipi-card/pratilipi-pratilipi-card.js
@@ -1,8 +1,13 @@
 function( params ) { 
 	var self = this;
+	var dataAccessor = new DataAccessor();
 
-	this.libraryPageBehaviour = params.libraryPageBehaviour != null ? params.libraryPageBehaviour : false;
 	this.pratilipi = params.pratilipi;
+	this.canRemoveFromLibrary = params.canRemoveFromLibrary != null ? params.canRemoveFromLibrary : false;
+	this.redirectToReaderOnClick = params.redirectToReaderOnClick != null ? params.redirectToReaderOnClick : false;
+
+	this.libraryButtonVisible = ko.observable( false );
+	this.shareButtonVisible = ko.observable( false );
 	this.libraryRequestOnFlight = ko.observable( false );
 
 	this.updatePratilipi = function( pratilipi ) {
@@ -10,17 +15,22 @@ function( params ) {
 	};
 
 	this.switchLibraryState = function() {
-		var dataAccessor = new DataAccessor();
+
+		if( ! self.canRemoveFromLibrary() && self.pratilipi.addedToLib() ) 
+			return;
+
 		self.libraryRequestOnFlight( true );
+
+		if( self.pratilipi.addedToLib() ) /* Toast only for remove action */
+			ToastUtil.toastCallBack( "${ _strings.removed_from_library }", 3000, "UNDO", self.switchLibraryState );
+		else 
+			ToastUtil.toastUp( "${ _strings.added_to_library }", 2000 );
+
 		var addedToLib = self.pratilipi.addedToLib();
 		self.updatePratilipi( { "addedToLib": ! addedToLib } );
-		if( self.libraryPageBehaviour && addedToLib ) /* Toast only for remove action */
-			ToastUtil.toastCallBack( "${ _strings.removed_from_library }", 5000, "UNDO", self.switchLibraryState );
-		if( self.libraryPageBehaviour && ! addedToLib ) /* Toast Down only for UNDO action */
-			ToastUtil.toastDown();
 		dataAccessor.addOrRemoveFromLibrary( self.pratilipi.pratilipiId(), ! addedToLib, 
 			function( pratilipi ) {
-				self.updatePratilipi( pratilipi );
+				/* self.updatePratilipi( pratilipi ); */
 				self.libraryRequestOnFlight( false );
 			}, function( error ) {
 				self.libraryRequestOnFlight( false );
@@ -45,5 +55,13 @@ function( params ) {
 		evt.stopPropagation();
 		ShareUtil.sharePratilipi( ko.mapping.toJS( self.pratilipi ) );
 	};
+
+	this.userObserver = ko.computed( function() {
+		self.libraryButtonVisible( appViewModel.user.author.authorId() != self.pratilipi.author.authorId() );
+	}, this );
+
+	this.pratilipiMetaObserver = ko.computed( function() {
+		self.shareButtonVisible( self.pratilipi.state == null || self.pratilipi.state() == "PUBLISHED" );
+	}, this );
 
 }
