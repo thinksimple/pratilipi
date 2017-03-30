e76e4ea4af482fbc48009425bb472c2cbc0c916e
RAGHU RAMAN <rraagghhu@gmail.com>
2017-03-30 22:44:12 +0530

    [Frontend] Updating author-info.

--- src/main/webapp/pwa-elements/pratilipi-author-info/pratilipi-author-info.js
+++ src/main/webapp/pwa-elements/pratilipi-author-info/pratilipi-author-info.js
@@ -8,4 +8,34 @@ function( params ) {
 	this.updateAuthor = params.updateAuthor;
 	this.updateUserAuthor = params.updateUserAuthor;
 
+	/* Follow/Unfollow Author */
+	this.followRequestOnFlight = ko.observable( false );
+	this.followOrUnfollowAuthor = function() {
+		if( self.followRequestOnFlight() ) 
+			return;
+		self.followRequestOnFlight( true );
+
+		ToastUtil.toast( "${ _strings.success_generic_message }" );
+
+		var switchState = function() {
+			self.updateAuthor( { "followCount": self.userAuthor.following() ? self.author.followCount() - 1 : self.author.followCount() + 1 } );
+			self.updateUserAuthor( { "following": ! self.userAuthor.following() } );
+		};
+
+		switchState();
+		dataAccessor.followOrUnfollowAuthor( self.author.authorId(), self.userAuthor.following(), 
+			function( userAuthor ) {
+				self.followRequestOnFlight( false );
+			}, function( error ) {
+				self.followRequestOnFlight( false );
+				switchState();
+				ToastUtil.toast( error[ "message" ] != null ? error[ "message" ] : "${ _strings.server_error_message }", 3000, true );
+		});
+	};
+
+	/* Share Author */
+	this.shareAuthor = function() {
+		/* */
+	};
+
 }
