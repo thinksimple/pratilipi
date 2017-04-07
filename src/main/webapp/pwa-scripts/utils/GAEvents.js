8a84f04dba173d8dea65d665a5932a9134107030
rraagghhu <rraagghhu@gmail.com>
2017-04-07 19:19:25 +0530

    [Frontend] Updating GAEvents.js

---
+++ src/main/webapp/pwa-scripts/utils/GAEvents.js
@@ -0,0 +1,113 @@
+/* Google Analytics */
+/* 
+ * GA has 4 parameters to record:
+ * Category, Action, Label, Value
+ * 
+ * https://drive.google.com/drive/u/1/folders/0B1GuDK5-Y90aVE0zVldHRm95RU0
+ * Event Category => The entity being tracked - pratilipi, author, notification, review etc.
+ * Event Action => The action the user is taking.
+ * Event Label => The page / location from where it is taken.
+ * Event Value => The difference in time between last action taken and current action.
+ * 
+ * 
+ * Function Names and Definition:
+ * ga_CALV => category, action, location, value
+ * ga_CAL => category, action, location
+ * ga_CA => category, action
+ * 
+ */
+
+var ga_value = new Date().getTime();
+
+
+var LOCATION = {
+
+	HOME: { value: "Home Page", urlPattern: "/\/$/s" },
+	LIST: { value: "List Page", urlPattern: "/^\/[^\/]*.*$/s" },
+	SEARCH : { value: "Search Page", urlPattern: "/\/search$/s" },
+	LIBRARY: { value: "Library Page", urlPattern: "/\/library$/s" },
+
+	AUTHOR: { value: "Author Page", urlPattern: "/^\/[^\/]*.*$/s" },
+	PRATILIPI: { value: "Pratilipi Page", urlPattern: "/^\/[^\/]*\/[^\/]*/s" },
+
+	LOGIN: { value: "Login Page", urlPattern: "/\/login$/s" },
+	REGISTER: { value: "Register Page", urlPattern: "/\/register$/s" },
+	FORGOT_PASSWORD: { value: "Forgot Password Page", urlPattern: "/\/forgot-password$/s" },
+
+	NOTIFICATIONS: { value: "Notifications Page", urlPattern: "/\/notifications$/s" },
+	SETTINGS: { value: "Settings Page", urlPattern: "/\/settings$/s" }
+
+};
+
+
+function ga_CALV( category, action, location, value ) {
+	if( category == null || action == null || location == null || value == null ) {
+		console.log( category, action, location, value );
+		return;
+	}
+	ga( 'send', 'event', category, action, location, value );
+	console.log( category, action, location, value );
+
+	ga_value = new Date().getTime(); /* reset timer */ 
+}
+
+function ga_CAL( category, action, location ) {
+	if( category == null || action == null || location == null ) {
+		console.log( category, action, location );
+		return;
+	}
+	ga_CALV( category, action, location, new Date().getTime() - ga_value );
+}
+
+function ga_CA( category, action ) {
+	if( category == null || action == null ) {
+		console.log( category, action );
+		return;
+	}
+
+	/* Logic For Location */
+	var location = null;
+
+	var currentLocation = window.location.pathname;
+	if( currentLocation == "/" )
+		location = LOCATION.HOME.value;
+	else if( currentLocation == "/search" )
+		location = LOCATION.SEARCH.value;
+	else if( currentLocation == "/library" )
+		location = LOCATION.LIBRARY.value;
+
+	else if( currentLocation == "/login" )
+		location = LOCATION.LOGIN.value;
+	else if( currentLocation == "/register" )
+		location = LOCATION.REGISTER.value;
+	else if( currentLocation == "/forgot-password" )
+		location = LOCATION.FORGOT_PASSWORD.value;
+
+	else if( currentLocation == "/notifications" )
+		location = LOCATION.NOTIFICATIONS.value;
+	else if( currentLocation == "/settings" )
+		location = LOCATION.SETTINGS.value;
+
+	<#list navigationList as navigation>
+		<#list navigation.linkList as link>
+
+	else if( currentLocation == "${ link.url }" && ! "${ link.url }".startsWith( "/search?" ) )
+		location = LOCATION.LIST.value;	
+
+		</#list>
+	</#list>
+
+
+	if( location == null ) { /* Author or Pratilipi */
+		String.prototype.count = function( s1 ) { 
+			return ( this.length - this.replace( new RegExp(s1,"g"), '' ).length ) / s1.length;
+		};
+		if( currentLocation.count( '/' ) == 1 )
+			location = LOCATION.AUTHOR.value;
+		else if( currentLocation.count( '/' ) == 2 )
+			location = LOCATION.PRATILIPI.value;
+	}
+
+	ga_CAL( category, action, location );
+
+}
