beb8b8b2e46759251894abfd817a6351d6cce4e8
rraagghhu <rraagghhu@gmail.com>
2017-04-08 19:17:35 +0530

    [PWA] Setting user-list.

---
+++ src/main/java/com/pratilipi/site/data/pwa-user-list.ml
@@ -0,0 +1,10 @@
+reshmas.devan@gmail.com
+sivin7791@gmail.com
+abdulbasith.etpa@gmail.com
+sruthirajan998@gmail.com
+saravanakumarks143@gmail.com
+vipi.1097@gmail.com
+civicjohn93@gmail.com
+vineethaanilvineetha@gmail.com
+ajulal222@gmail.com
+anuroopns@gmail.com
