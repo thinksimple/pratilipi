141b1a9cb46b38415a0b5bfbb293da8f38451b3a
rraagghhu <rraagghhu@gmail.com>
2017-04-08 21:04:48 +0530

    [Frontend] Updating notifications page.

--- src/main/webapp/pwa-elements/pratilipi-notifications-page/pratilipi-notifications-page.js
+++ src/main/webapp/pwa-elements/pratilipi-notifications-page/pratilipi-notifications-page.js
@@ -4,7 +4,10 @@ function( params ) {
 	this.userObserver = ko.computed( function() {
 		if( appViewModel.user.isGuest() && appViewModel.user.userId() == 0 ) {
 			goToLoginPage( null, { "message": "NOTIFICATIONS" } );
+			return;
 		}
+		if( getUrlParameter( 'action' ) == "settings" )
+			$( "#pratilipiNotificationSettings" ).modal();
 	}, this );
 
 }
