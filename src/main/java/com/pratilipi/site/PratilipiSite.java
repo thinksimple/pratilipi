26a4c54bc7d42b23d8dc51a89977cc091071fda5
Raghu Raman <rraagghhu@gmail.com>
2017-04-09 20:30:01 +0530

    [Backend] Updating PratilipiSite.

--- src/main/java/com/pratilipi/site/PratilipiSite.java
+++ src/main/java/com/pratilipi/site/PratilipiSite.java
@@ -151,8 +151,7 @@ public class PratilipiSite extends HttpServlet {
 		// Load PWA
 		Long uId = AccessTokenFilter.getAccessToken().getUserId();
 //		boolean loadPWA = UserAccessUtil.hasUserAccess( uId, null, AccessType.USER_ADD );
-		boolean loadPWA = ! SystemProperty.STAGE.equals( SystemProperty.STAGE_PROD ) ||
-					isEligibleForPWA( userData.getEmail(), filterLanguage );
+		boolean loadPWA = isEligibleForPWA( userData.getEmail(), filterLanguage );
 
 		// Data Model for FreeMarker
 		Map<String, Object> dataModel = null;
