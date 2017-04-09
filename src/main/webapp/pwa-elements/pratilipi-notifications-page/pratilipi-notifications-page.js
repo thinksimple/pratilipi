b7fc358b11d4e42c5152c339aba1e1ea58564f87
Raghu Raman <rraagghhu@gmail.com>
2017-04-09 12:43:53 +0530

    [Frontend] Bug fix in notifications page.

--- src/main/webapp/pwa-elements/pratilipi-notifications-page/pratilipi-notifications-page.js
+++ src/main/webapp/pwa-elements/pratilipi-notifications-page/pratilipi-notifications-page.js
@@ -3,11 +3,14 @@ function( params ) {
 
 	this.userObserver = ko.computed( function() {
 		if( appViewModel.user.isGuest() && appViewModel.user.userId() == 0 ) {
-			goToLoginPage( null, { "message": "NOTIFICATIONS" } );
+			goToLoginPage( getUrlParameter( 'action' ) == "settings" ? { "action": "settings" } : null, { "message": "NOTIFICATIONS" } );
 			return;
 		}
-		if( getUrlParameter( 'action' ) == "settings" )
-			$( "#pratilipiNotificationSettings" ).modal();
+		if( getUrlParameter( 'action' ) == "settings" ) {
+			setTimeout( function() {
+				$( "#pratilipiNotificationSettings" ).modal();
+			}, 0 );
+		}
 	}, this );
 
 }
